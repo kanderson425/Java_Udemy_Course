db2930de51673bfb8e9a7430927b1fa08cab83f5
kanderson425 <kyleanderson.dev@gmail.com>
2019-05-06 14:55:31 -0500

    Adds whitespace and other lessons

---
+++ KeywordsAndExpressions/src/com/kyleanderson/Main.java
@@ -0,0 +1,17 @@
+package com.kyleanderson;
+
+public class Main {
+
+    public static void main(String[] args) {
+        //  a mile is equal to 1.609344 kilometres
+        double kilometres = (100 * 1.609344);
+        int highScore = 50;
+        if(highScore == 50) {
+            System.out.println("This is an expression");
+        }
+
+
+
+
+    }
+}
---
+++ StatementsWhiteSpaceAndIndenting/src/com/kyleanderson/Main.java
@@ -0,0 +1,21 @@
+package com.kyleanderson;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int myVariable = 50;
+        myVariable++;
+        myVariable--;
+        System.out.println("This is a test");
+
+        System.out.println("This is" + " " +
+                "another" + " " +
+                "still more.");
+
+        int anotherVariable = 50;
+        if (anotherVariable == 50) {
+            System.out.println("Another Variable is equal to 50");
+        }
+
+    }
+}
