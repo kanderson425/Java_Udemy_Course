dbbe59a1133c1d3734ab4ebad932d0e43844c96a
kanderson425 <kyleanderson.dev@gmail.com>
2019-06-03 21:18:34 -0500

    Adds coding exercise 8: Area Calculator

---
+++ MethodOverloading Exercises/src/Main.java
@@ -0,0 +1,26 @@
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println(area(5.0));
+        System.out.println(area(5.0, 4.0));
+    }
+
+
+    public static double area(double radius) {
+        if( radius < 0) {
+            return -1.0;
+        }
+
+        double circleArea = radius * radius * Math.PI;
+        return circleArea;
+    }
+
+    public static double area(double x, double y) {
+        if ((x < 0) || (y < 0)) {
+            return -1.;
+        }
+
+        double rectangleArea = x * y;
+        return rectangleArea;
+    }
+}
