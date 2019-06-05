9847ba6d49a94960bcbc00a221c84a3eb4c9c46f
kanderson425 <kyleanderson.dev@gmail.com>
2019-06-04 22:34:24 -0500

    Adds DayOfTheWeekChallenge

---
+++ DayOfTheWeekChallenge/src/Main.java
@@ -0,0 +1,52 @@
+public class Main {
+
+    public static void main(String[] args) {
+        printDayOfTheWeek(6);
+    }
+
+    public static void printDayOfTheWeek(int day) {
+        switch(day) {
+            case 0:
+                System.out.println("Sunday");
+                break;
+            case 1:
+                System.out.println("Monday");
+                break;
+            case 2:
+                System.out.println("Tuesday");
+                break;
+            case 3:
+                System.out.println("Wednesday");
+                break;
+            case 4:
+                System.out.println("Thursday");
+                break;
+            case 5:
+                System.out.println("Friday");
+                break;
+            case 6:
+                System.out.println("Saturday");
+                break;
+            default:
+                System.out.println("Invalid day");
+                break;
+        }
+//        if(day == 0) {
+//            System.out.println("Sunday");
+//        } else if(day == 1) {
+//            System.out.println("Monday");
+//        } else if(day ==2) {
+//            System.out.println("Tuesday");
+//        } else if(day == 3) {
+//            System.out.println("Wednesday");
+//        } else if(day ==4) {
+//            System.out.println("Thursday");
+//        } else if(day == 5) {
+//            System.out.println("Friday");
+//        } else if(day == 6) {
+//            System.out.println("Saturday");
+//        } else {
+//            System.out.println("Invalid Day");
+//        }
+    }
+}
