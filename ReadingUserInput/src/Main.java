a9a69d93e5bcd296a168a9b07a8dfa4e523c7b4e
kanderson425 <kyleanderson.dev@gmail.com>
2019-06-27 20:24:04 -0500

    Adds interaction and validation logic to ReadingUserInput lesson from Java Udemy course

--- ReadingUserInput/src/Main.java
+++ ReadingUserInput/src/Main.java
@@ -6,14 +6,26 @@ public class Main {
         Scanner scanner = new Scanner(System.in);
 
         System.out.println("Enter your year of birth: ");
-        int yearOfBirth = scanner.nextInt();
-        scanner.nextLine(); // handle next line character (enter key)
 
-        System.out.println("Enter your name: " );
-        String name = scanner.nextLine();
-        int age = 2019 - yearOfBirth;
+        boolean hasNextInt = scanner.hasNextInt();
 
-        System.out.println("Your name is " + name + ", and you are " + age + " years old.");
+        if(hasNextInt) {
+            int yearOfBirth = scanner.nextInt();
+            scanner.nextLine(); // handle next line character (enter key)
+
+            System.out.println("Enter your name: " );
+            String name = scanner.nextLine();
+            int age = 2019 - yearOfBirth;
+
+            if(age >= 0 && age <= 100) {
+                System.out.println("Your name is " + name + ", and you are " + age + " years old.");
+            } else {
+                System.out.println("Invalid year of birth");
+            }
+        } else {
+            System.out.println("Unable to parse year of birth.");
+        }
+
 
         scanner.close();
     }
