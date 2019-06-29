c5088bbaddb04e7d3c4db3c7a39ad525dfb9e9c7
kanderson425 <kyleanderson.dev@gmail.com>
2019-06-29 17:51:26 -0500

    Adds code from Classes lesson - part 2, specifically setters/getters and fields

--- Classes/src/com/kyleanderson/Car.java
+++ Classes/src/com/kyleanderson/Car.java
@@ -12,6 +12,15 @@ public class Car {
     private String engine;
     private String color;
 
+    public void setModel(String model) {
+        this.model = model;
+
+    }
+
+    public String getModel() {
+        return this.model;
+    }
+
 
 
 
--- Classes/src/com/kyleanderson/Main.java
+++ Classes/src/com/kyleanderson/Main.java
@@ -5,8 +5,8 @@ public class Main {
     public static void main(String[] args) {
 	    Car porsche = new Car();
 	    Car holden = new Car();
-	    porsche.model = "Carrera";
+	    porsche.setModel("Carrera");
 
-
+		System.out.println("Model is " + porsche.getModel());
     }
 }
