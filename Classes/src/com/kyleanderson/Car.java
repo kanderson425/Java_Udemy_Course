5afd969d910b959fbc176a952d3a3923d7ca9a70
kanderson425 <kyleanderson.dev@gmail.com>
2019-06-29 17:44:20 -0500

    Includes code from part 1 of Classes lesson

---
+++ Classes/src/com/kyleanderson/Car.java
@@ -0,0 +1,18 @@
+package com.kyleanderson;
+
+/**
+ * created by a dev on 6/29/2019
+ */
+
+public class Car {
+
+    private int doors;
+    private int wheels;
+    private String model;
+    private String engine;
+    private String color;
+
+
+
+
+}
---
+++ Classes/src/com/kyleanderson/Main.java
@@ -0,0 +1,12 @@
+package com.kyleanderson;
+
+public class Main {
+
+    public static void main(String[] args) {
+	    Car porsche = new Car();
+	    Car holden = new Car();
+	    porsche.model = "Carrera";
+
+
+    }
+}
