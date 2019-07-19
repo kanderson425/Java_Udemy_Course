e16540109eb8c38d4442e9fe6da09726a4598762
kanderson425 <kyleanderson.dev@gmail.com>
2019-07-19 17:42:23 -0500

    Adds 45 degrees to the direction parameter

--- InheritanceChallenge/src/com/kyleanderson/Main.java
+++ InheritanceChallenge/src/com/kyleanderson/Main.java
@@ -5,6 +5,7 @@ public class Main {
     public static void main(String[] args) {
 	// write your code here
     BMW bmw = new BMW(36);
+    bmw.steer(45);
     bmw.accelerate(30);
     bmw.accelerate(20);
 
