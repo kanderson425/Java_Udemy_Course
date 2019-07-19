4e51e16c7a484eed03a3cad589399597b6803c3e
kanderson425 <kyleanderson.dev@gmail.com>
2019-07-18 23:54:36 -0500

    Adds the underlying logic to the accelerate method that controls the gear change

--- InheritanceChallenge/src/com/kyleanderson/BMW.java
+++ InheritanceChallenge/src/com/kyleanderson/BMW.java
@@ -11,5 +11,21 @@ public class BMW extends Car {
     public void accelerate(int rate) {
 
         int newVelocity = getCurrentVelocity() + rate;
+        if(newVelocity ==0) {
+            stop()
+        } else if(newVelocity > 0 && newVelocity <=10) {
+            changeGear(1);
+        } else if(newVelocity > 10 && newVelocity <=20) {
+            changeGear(2);
+
+        } else if (newVelocity > 20 && newVelocity <=30) {
+            changeGear(3);
+        } else {
+            changeGear(4);
+        }
+
+        if(newVelocity > 0) {
+            changeVelocity(newVelocity, getCurrentDirection());
+        }
     }
 }
