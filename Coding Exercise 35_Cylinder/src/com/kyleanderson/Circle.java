b0dfce8d2224de77cc21a3ddbe5dfd89e20f3217
kanderson425 <kyleanderson.dev@gmail.com>
2019-07-21 10:30:44 -0500

    Adds the getRadius method

--- Coding Exercise 35_Cylinder/src/com/kyleanderson/Circle.java
+++ Coding Exercise 35_Cylinder/src/com/kyleanderson/Circle.java
@@ -4,12 +4,19 @@ public class Circle {
 
     double radius;
 
+
+
     public Circle(double radius) {
         if(radius < 0) {
             this.radius = 0;
         } else {
             this.radius = radius;
         }
-
     }
+
+    public double getRadius() {
+        return radius;
+    }
+
+
 }
