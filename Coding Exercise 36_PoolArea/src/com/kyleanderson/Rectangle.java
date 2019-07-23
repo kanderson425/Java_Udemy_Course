30de75a54d21fd15d21820ffd7a4e5f44433e587
kanderson425 <kyleanderson.dev@gmail.com>
2019-07-22 19:03:43 -0500

    Adds the constructor for the Rectangle class

--- Coding Exercise 36_PoolArea/src/com/kyleanderson/Rectangle.java
+++ Coding Exercise 36_PoolArea/src/com/kyleanderson/Rectangle.java
@@ -4,4 +4,11 @@ public class Rectangle {
 
     private double width;
     private double length;
+
+    public Rectangle(double width, double length) {
+        this.width = width;
+        this.length = length;
+    }
+
+
 }
