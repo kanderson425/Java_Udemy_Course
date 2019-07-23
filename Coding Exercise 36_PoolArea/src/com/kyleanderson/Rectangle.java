a3d4f2dde5b44f9787949d7a37d02594aff92a9f
kanderson425 <kyleanderson.dev@gmail.com>
2019-07-22 19:05:32 -0500

    Adds the getWidth method

--- Coding Exercise 36_PoolArea/src/com/kyleanderson/Rectangle.java
+++ Coding Exercise 36_PoolArea/src/com/kyleanderson/Rectangle.java
@@ -6,9 +6,20 @@ public class Rectangle {
     private double length;
 
     public Rectangle(double width, double length) {
-        this.width = width;
-        this.length = length;
+        if(width < 0) {
+            this.width = 0;
+        } else {
+            this.width = width;
+        }
+
+        if(length < 0) {
+            this.length = 0;
+        } else {
+            this.length = length;
+        }
     }
 
-
+    public double getWidth() {
+        return width;
+    }
 }
