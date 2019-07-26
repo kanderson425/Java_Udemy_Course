5a57e06f57b831cee154c9cd47aeea59f47e7d2d
kanderson425 <kyleanderson.dev@gmail.com>
2019-07-26 10:08:23 -0500

    Creates the PC class with theCase, theMonitor and theMotherboard included

--- Composition/src/com/kyleanderson/Main.java
+++ Composition/src/com/kyleanderson/Main.java
@@ -9,6 +9,9 @@ public class Main {
 
         Monitor theMonitor = new Monitor("27inch Beast","Acer", 27, new Resolution(2540,1440));
 
+        Motherboard theMotherboard = new Motherboard("BJ-200","Asus",4,6,"v2.44");
+
+        PC thePC = new PC(theCase, theMonitor, theMotherboard);
 
     }
 }
