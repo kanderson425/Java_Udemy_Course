f53cd98cba75c392167e3b50c5bf4002e5fcf20b
kanderson425 <kyleanderson.dev@gmail.com>
2019-07-26 09:47:59 -0500

    Adds the motherboard class

--- Composition/src/com/kyleanderson/Car.java
+++ Composition/src/com/kyleanderson/Car.java
@@ -10,4 +10,6 @@ public class Car extends Vehicle {
         this.doors = doors;
         this.engingeCapacity = engingeCapacity;
     }
+
+
 }
---
+++ Composition/src/com/kyleanderson/Motherboard.java
@@ -0,0 +1,10 @@
+package com.kyleanderson;
+
+public class Motherboard {
+
+    private String model;
+    private String manufacturer;
+    private int ramSlots;
+    private int cardSlots;
+    private String bios;
+}
