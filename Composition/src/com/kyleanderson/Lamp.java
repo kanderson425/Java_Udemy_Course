99f1b48b376e1e45563d63429f16d2faae1812b9
kanderson425 <kyleanderson.dev@gmail.com>
2019-07-27 16:41:08 -0500

    Adds the lamp class

---
+++ Composition/src/com/kyleanderson/Lamp.java
@@ -0,0 +1,26 @@
+package com.kyleanderson;
+
+public class Lamp {
+
+    private String style;
+    private boolean battery;
+    private int globeRating;
+
+    public Lamp(String style, boolean battery, int globeRating) {
+        this.style = style;
+        this.battery = battery;
+        this.globeRating = globeRating;
+    }
+
+    public String getStyle() {
+        return style;
+    }
+
+    public boolean isBattery() {
+        return battery;
+    }
+
+    public int getGlobeRating() {
+        return globeRating;
+    }
+}
