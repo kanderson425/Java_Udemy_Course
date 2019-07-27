6af17b47535b73c74570f12f97e0d6e4cd074647
kanderson425 <kyleanderson.dev@gmail.com>
2019-07-27 11:12:25 -0500

    Adds the ceiling and wall classes

---
+++ Composition/src/com/kyleanderson/Wall.java
@@ -0,0 +1,16 @@
+package com.kyleanderson;
+
+public class Wall {
+
+    private String direction;
+
+    public Wall(String direction) {
+        this.direction = direction;
+    }
+
+    public String getDirection() {
+        return direction;
+    }
+
+
+}
