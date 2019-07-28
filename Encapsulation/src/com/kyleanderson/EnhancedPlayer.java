c26200d344755cea955f9ae1523ac169e1d07ec2
kanderson425 <kyleanderson.dev@gmail.com>
2019-07-28 17:14:54 -0500

    Add the loseHealth method and the getHealth getter to the enhancedPlayer class

--- Encapsulation/src/com/kyleanderson/EnhancedPlayer.java
+++ Encapsulation/src/com/kyleanderson/EnhancedPlayer.java
@@ -8,9 +8,21 @@ public class EnhancedPlayer {
 
     public EnhancedPlayer(String name, int health, String weapon) {
         this.name = name;
-        if(health >0 && health <= 200) {
+        if(health >0 && health <= 100) {
             this.health = health;
         }
         this.weapon = weapon;
+    }
+
+    public void loseHealth(int damage) {
+        this.health = this.health - damage;
+        if(this.health <=0) {
+            System.out.println("Player knocked out");
+            //Reduce the number of lives remaining for the player
+        }
+    }
+
+    public int getHealth() {
+        return health;
     }
 }
