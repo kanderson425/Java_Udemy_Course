08f8746a3a4788afac269419a15f5d5909678dc5
kanderson425 <kyleanderson.dev@gmail.com>
2019-07-28 17:41:23 -0500

    Adds the fillUpToner and printPage methods to the printer class

--- Encapsulation/src/com/kyleanderson/Printer.java
+++ Encapsulation/src/com/kyleanderson/Printer.java
@@ -2,10 +2,24 @@ package com.kyleanderson;
 
 public class Printer {
 
-    private int tonerLevel;
+    private int tonerLevelPercent = 100;
     private int pagesPrinted;
     private boolean duplexPrinter;
 
+    public Printer(int tonerLevelPercent, int pagesPrinted, boolean duplexPrinter) {
+        this.tonerLevelPercent = tonerLevelPercent;
+        this.pagesPrinted = pagesPrinted;
+        this.duplexPrinter = duplexPrinter;
+    }
 
+    public void fillUpToner() {
+        tonerLevelPercent = 100;
 
+    }
+
+    public int printPage() {
+        int newPagesPrinted = pagesPrinted + 1;
+        pagesPrinted = newPagesPrinted;
+        return pagesPrinted;
+    }
 }
