efb4a92898b75ef395b91d09d4123ba71066effe
kanderson425 <kyleanderson.dev@gmail.com>
2019-07-28 16:43:18 -0500

    Adds the player constructor and set the splayer fields manually in the Main class

--- Encapsulation/src/com/kyleanderson/Main.java
+++ Encapsulation/src/com/kyleanderson/Main.java
@@ -3,6 +3,9 @@ package com.kyleanderson;
 public class Main {
 
     public static void main(String[] args) {
-
+    Player player = new Player();
+    player.name = "Kyle";
+    player.health = 20;
+    player.weapon = "Sword";
     }
 }
