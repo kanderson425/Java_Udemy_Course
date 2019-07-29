f6aeb98bc407c8b8ca03fcfcdecdc16bc189dc46
kanderson425 <kyleanderson.dev@gmail.com>
2019-07-29 13:03:36 -0500

    Adds the startEngine, the accelerate and the brake method to the main Car class

--- Polymorphism Challenge/src/com/kyleanderson/Main.java
+++ Polymorphism Challenge/src/com/kyleanderson/Main.java
@@ -14,6 +14,37 @@ public class Main {
             this.cylinders = cylinders;
             this.wheels = 4;
         }
+
+        public String getName() {
+            return name;
+        }
+
+        public boolean isEngine() {
+            return engine;
+        }
+
+        public int getCylinders() {
+            return cylinders;
+        }
+
+        public int getWheels() {
+            return wheels;
+        }
+
+        public void startEngine() {
+            System.out.println("The car engine is starting");
+        }
+
+        public void accelerate() {
+            System.out.println("The car is now accelerating");
+
+        }
+
+        public void brake() {
+            System.out.println("The car is now braking");
+
+        }
+
     }
 
     public static void main(String[] args) {
