654db24884686a3a4413c7e0e1c8a8ca69995494
kanderson425 <kyleanderson.dev@gmail.com>
2019-07-29 12:08:40 -0500

    Adds the Jaws class

--- Polymorphism/src/com/kyleanderson/Main.java
+++ Polymorphism/src/com/kyleanderson/Main.java
@@ -13,6 +13,16 @@ class Movie {
     }
 }
 
+class Jaws extends Movie {
+    public Jaws() {
+        super("Jaws");
+    }
+
+    public String plot() {
+        return "A shark eats lots of people."
+    }
+}
+
 
 public class Main {
 
