e69a3e1d210a6c7378d5d7cd4f11626017487180
kanderson425 <kyleanderson.dev@gmail.com>
2019-07-29 12:22:52 -0500

    Adds the randomMovie method into the main method

--- Polymorphism/src/com/kyleanderson/Main.java
+++ Polymorphism/src/com/kyleanderson/Main.java
@@ -1,7 +1,5 @@
 package com.kyleanderson;
 
-import org.graalvm.compiler.lir.sparc.SPARCMove;
-
 class Movie {
     private String name;
 
@@ -21,7 +19,7 @@ class Jaws extends Movie {
     }
 
     public String plot() {
-        return "A shark eats lots of people."
+        return "A shark eats lots of people.";
     }
 }
 
@@ -72,5 +70,23 @@ public class Main {
 
     public static void main(String[] args) {
 
+    }
+
+    public static Movie randomMovie() {
+        int randomNumber = (int) (Math.random() * 5) + 1;
+        System.out.println("Random number generated was " + randomNumber);
+        switch (randomNumber) {
+            case 1:
+                return new Jaws();
+            case 2:
+                return new IndependenceDay();
+            case 3:
+                return new MazeRunner();
+            case 4:
+                return new StarWars();
+            case 5:
+                return new Forgetable();
+        }
+        return null;
     }
 }
