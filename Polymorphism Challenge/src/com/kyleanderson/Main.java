19c9629b0c8d85ee57b6a5d25ae77dccace9ef71
kanderson425 <kyleanderson.dev@gmail.com>
2019-07-29 13:20:18 -0500

    Adds the functionality of the getName methods and utilizes polymorphism

--- Polymorphism Challenge/src/com/kyleanderson/Main.java
+++ Polymorphism Challenge/src/com/kyleanderson/Main.java
@@ -1,86 +1,105 @@
 package com.kyleanderson;
 
+class Car {
+    private String name;
+    private boolean engine;
+    private int cylinders;
+    private int wheels;
+
+    public Car(String name, int cylinders) {
+        this.name = name;
+        this.engine = true;
+        this.cylinders = cylinders;
+        this.wheels = 4;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public boolean isEngine() {
+        return engine;
+    }
+
+    public int getCylinders() {
+        return cylinders;
+    }
+
+    public int getWheels() {
+        return wheels;
+    }
+
+    public void startEngine() {
+        System.out.println("The car engine is starting");
+    }
+
+    public void accelerate() {
+        System.out.println("The car is now accelerating");
+
+    }
+
+    public void brake() {
+        System.out.println("The car is now braking");
+
+    }
+
+}
+
+class BMW extends Car {
+    public BMW() {
+        super("BMW", 6);
+    }
+
+    @Override
+    public String getName() {
+        return "This here car is a BEEEEMER";
+    }
+}
+
+class Subaru extends Car {
+    public Subaru() {
+        super("Subaru", 4);
+    }
+
+    @Override
+    public String getName() {
+        return "This Car is a Subaru - very nice!";
+    }
+}
+
+class Jeep extends Car {
+    public Jeep() {
+        super("Jeep", 6);
+    }
+
+    @Override
+    public String getName() {
+        return "This tool of warfare defeated the damn Nazis!";
+    }
+}
+
 public class Main {
 
-    class Car {
-        private String name;
-        private boolean engine;
-        private int cylinders;
-        private int wheels;
-
-        public Car(String name, int cylinders) {
-            this.name = name;
-            this.engine = true;
-            this.cylinders = cylinders;
-            this.wheels = 4;
-        }
-
-        public String getName() {
-            return name;
-        }
-
-        public boolean isEngine() {
-            return engine;
-        }
-
-        public int getCylinders() {
-            return cylinders;
-        }
-
-        public int getWheels() {
-            return wheels;
-        }
-
-        public void startEngine() {
-            System.out.println("The car engine is starting");
-        }
-
-        public void accelerate() {
-            System.out.println("The car is now accelerating");
-
-        }
-
-        public void brake() {
-            System.out.println("The car is now braking");
-
-        }
-
-    }
-
-    class BMW extends Car {
-        public BMW() {
-            super("BMW", 6);
-        }
-
-        @Override
-        public String getName() {
-            return "This here car is a BEEEEMER";
-        }
-    }
-
-    class Subaru extends Car {
-        public Subaru() {
-            super("Subaru", 4);
-        }
-
-        @Override
-        public String getName() {
-            return "This Car is a Subaru - very nice!";
-        }
-    }
-
-    class Jeep extends Car {
-        public Jeep() {
-            super("Jeep", 6);
-        }
-
-        @Override
-        public String getName() {
-            return "This tool of warfare defeated the damn Nazis!";
-        }
-    }
-
     public static void main(String[] args) {
-	// write your code here
+	    for(int i = 1; i < 11; i++) {
+	        Car car = randomCar();
+            System.out.println("Car #" + i +
+                    " : " + car.getName() + "\n" +
+                    "Cylinders: " + car.getCylinders() + "\n");
+        }
+    }
+
+    public static Car randomCar() {
+        int randomNumber = (int) (Math.random() * 3) + 1;
+        System.out.println("Random number generated was " + randomNumber);
+        switch (randomNumber) {
+            case 1:
+                return new BMW();
+            case 2:
+                return new Subaru();
+            case 3:
+                return new Jeep();
+        }
+        return null;
     }
 }
