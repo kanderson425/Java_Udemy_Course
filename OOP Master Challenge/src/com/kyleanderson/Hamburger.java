e01521d9c16865a93dc86927643d5df382c22e78
kanderson425 <kyleanderson.dev@gmail.com>
2019-07-30 22:09:35 -0500

    Adds the fields to the main Hamburger class

--- OOP Master Challenge/src/com/kyleanderson/Hamburger.java
+++ OOP Master Challenge/src/com/kyleanderson/Hamburger.java
@@ -1,6 +1,10 @@
 package com.kyleanderson;
 
 public class Hamburger {
-
+    private String rollType;
+    private String meathType;
+    private boolean lettuce;
+    private boolean tomato;
+    private boolean carrot;
 
 }
