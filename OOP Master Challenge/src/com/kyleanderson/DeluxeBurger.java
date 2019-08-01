44ea2d692173d56fa63ac66eaa2454a3a34ce2e2
kanderson425 <kyleanderson.dev@gmail.com>
2019-07-31 23:53:18 -0500

    Adds the HealthyBurger constructor

--- OOP Master Challenge/src/com/kyleanderson/DeluxeBurger.java
+++ OOP Master Challenge/src/com/kyleanderson/DeluxeBurger.java
@@ -1,3 +1,11 @@
 package com.kyleanderson;
 
+public class DeluxeBurger extends Hamburger {
+    private String additionChipsName;
+    private double additionChipsPrice;
 
+    private String additionDrinkName;
+    private double additionDrinkPrice;
+
+
+}
--- OOP Master Challenge/src/com/kyleanderson/HealthyBurger.java
+++ OOP Master Challenge/src/com/kyleanderson/HealthyBurger.java
@@ -1,3 +1,13 @@
 package com.kyleanderson;
 
+public class HealthyBurger extends Hamburger {
+    private String healthyExtra1Name;
+    private double healthyExtra1Price;
 
+    private String healthyExtra2Name;
+    private double healthyExtra2Price;
+
+    public HealthyBurger( String meat, double price) {
+        super("Healthy","Brown Rye", meat, price);
+    }
+}
