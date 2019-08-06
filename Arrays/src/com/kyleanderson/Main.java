c59fb5a8488fce8f5a56f17c2db13c3159215634
kanderson425 <kyleanderson.dev@gmail.com>
2019-08-05 22:57:26 -0500

    Uses a for loop to initialize an array

--- Arrays/src/com/kyleanderson/Main.java
+++ Arrays/src/com/kyleanderson/Main.java
@@ -5,12 +5,15 @@ public class Main {
     public static void main(String[] args) {
 
     //This is how you can initialize an array with all of the array elements
-	int[] myIntArray = { 1,2,3,4,5,6,7,8,9,10};
+	int[] myIntArray = new int[10];
 //	myIntArray[0] = 45;
 //	myIntArray[5] = 50;
-
-
-
+        for(int i=0; i<10; i++) {
+            myIntArray[i] = i*10;
+        }
+        for(int i=0; i<10; i++) {
+            System.out.println("Element " + i + ", value is " + myIntArray[i]);
+        }
 
 
     }
