db9933d8b419706bac995905a10755dfef0cce08
kanderson425 <kyleanderson.dev@gmail.com>
2019-08-06 23:58:48 -0500

    Adds the getIntegers and printArray methods to the ArrayChallenge

--- ArrayChallenge/src/com/kyleanderson/Main.java
+++ ArrayChallenge/src/com/kyleanderson/Main.java
@@ -5,7 +5,26 @@ import java.util.Scanner;
 public class Main {
     private static Scanner scanner = new Scanner(System.in);
     public static void main(String[] args) {
+        int[] myIntegers = getIntegers(5);
+        for(int i=0; i<myIntegers.length; i++) {
+            System.out.println("Element " + i + ", typed value was " + myIntegers[i]);
+        }
 
+    }
 
+    public static int[] getIntegers(int number) {
+        System.out.println("Enter " + number + " integer values.\r");
+        int[] values = new int[number];
+
+        for(int i=0; i<values.length; i++) {
+            values[i] = scanner.nextInt();
+        }
+        return values;
+    }
+
+    public static void printArray(int[] array) {
+        for(int i=0; i<array.length; i++) {
+            System.out.println("Element " + i + ", value is " + array[i]);
+        }
     }
 }
