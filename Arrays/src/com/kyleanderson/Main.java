8eed38efb2e89e02071f64550f244aca0b0c71af
kanderson425 <kyleanderson.dev@gmail.com>
2019-08-06 23:42:14 -0500

    Passes the array through the printArray method

--- Arrays/src/com/kyleanderson/Main.java
+++ Arrays/src/com/kyleanderson/Main.java
@@ -5,16 +5,18 @@ public class Main {
     public static void main(String[] args) {
 
     //This is how you can initialize an array with all of the array elements
-	int[] myIntArray = new int[10];
+	int[] myIntArray = new int[25];
 //	myIntArray[0] = 45;
 //	myIntArray[5] = 50;
         for(int i=0; i<myIntArray.length; i++) {
             myIntArray[i] = i*10;
         }
-        for(int i=0; i<myIntArray.length; i++) {
-            System.out.println("Element " + i + ", value is " + myIntArray[i]);
+        printArray(myIntArray);
+    }
+
+    public static void printArray(int[] array) {
+        for(int i=0; i<array.length; i++) {
+            System.out.println("Element " + i + ", value is " + array[i]);
         }
-
-
     }
 }
