e5f528ab7d75428129a74f7ee220b7e0a02c3b45
kanderson425 <kyleanderson.dev@gmail.com>
2019-08-06 23:49:17 -0500

    Adds the getAverage method to return the average of the array

--- Arrays/src/com/kyleanderson/Main.java
+++ Arrays/src/com/kyleanderson/Main.java
@@ -1,22 +1,47 @@
 package com.kyleanderson;
 
+import java.util.Scanner;
 public class Main {
-
+    //Adds the scanner functionality
+    private static Scanner scanner = new Scanner(System.in);
     public static void main(String[] args) {
-
-    //This is how you can initialize an array with all of the array elements
-	int[] myIntArray = new int[25];
-//	myIntArray[0] = 45;
-//	myIntArray[5] = 50;
-        for(int i=0; i<myIntArray.length; i++) {
-            myIntArray[i] = i*10;
+        int[] myIntegers = getIntegers(5);
+        for(int i=0; i<myIntegers.length; i++) {
+            System.out.println("Element " + i +", typed value was " + myIntegers[i]);
         }
-        printArray(myIntArray);
+
+//    //This is how you can initialize an array with all of the array elements
+//	int[] myIntArray = new int[25];
+////	myIntArray[0] = 45;
+////	myIntArray[5] = 50;
+//        for(int i=0; i<myIntArray.length; i++) {
+//            myIntArray[i] = i*10;
+//        }
+//        printArray(myIntArray);
+//    }
+//
+//    public static void printArray(int[] array) {
+//        for(int i=0; i<array.length; i++) {
+//            System.out.println("Element " + i + ", value is " + array[i]);
+//        }
+    }
+    //Adds the scanner functionality
+    public static int[] getIntegers(int number) {
+        System.out.println("Enter " + number + " integer values.\r");
+        int[] values = new int[number];
+
+        for(int i=0; i<values.length; i++) {
+            values[i] = scanner.nextInt();
+        }
+
+        return values;
     }
 
-    public static void printArray(int[] array) {
+    public static double getAverage(int[] array) {
+        int sum = 0;
         for(int i=0; i<array.length; i++) {
-            System.out.println("Element " + i + ", value is " + array[i]);
+            sum += array[i];
         }
+        return (double) sum / array.length;
     }
 }
