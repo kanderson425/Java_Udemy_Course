8a6afd17050e39bed92e4797ec30927d9e914c32
kanderson425 <kyleanderson.dev@gmail.com>
2019-08-09 17:34:23 -0500

    Adds the walkthrough of array reference variables

--- ReferenceTypesVsValueTypes/src/com/kyleanderson/Main.java
+++ ReferenceTypesVsValueTypes/src/com/kyleanderson/Main.java
@@ -1,5 +1,7 @@
 package com.kyleanderson;
 
+import java.util.Arrays;
+
 public class Main {
 
     public static void main(String[] args) {
@@ -14,9 +16,15 @@ public class Main {
         System.out.println("myIntValue = " + myIntValue);
         System.out.println("anotherIntValue = " + anotherIntValue);
 
-        myIntValue++;
+        int[] myIntArray = new int[5];
+        int[] anotherIntArray = myIntArray;
 
-        System.out.println("myIntValue = " + myIntValue);
-        System.out.println("anotherIntValue = " + anotherIntValue);
+        System.out.println("myIntArray= " + Arrays.toString(myIntArray));
+        System.out.println("anotherIntArray= " + Arrays.toString(anotherIntArray));
+
+        anotherIntArray[0] = 1;
+
+        System.out.println("after change myIntArray= " + Arrays.toString(myIntArray));
+        System.out.println("after change anotherIntArray= " + Arrays.toString(anotherIntArray));
     }
 }
