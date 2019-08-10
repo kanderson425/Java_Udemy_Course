04c40b9bbc4ccdb4c2531aa540975ad6b6819abf
kanderson425 <kyleanderson.dev@gmail.com>
2019-08-10 16:26:59 -0500

    Adds the reverse method for ReverseArrayChallenge

---
+++ ReverseArrayChallenge/src/com/kyleanderson/Main.java
@@ -0,0 +1,22 @@
+package com.kyleanderson;
+
+import java.util.Arrays;
+
+public class Main {
+
+
+    //My Attempt
+
+    public static void main(String[] args) {
+
+        int[] array = {1,2,3,4,5};
+        System.out.println("array = " + Arrays.toString(array));
+
+    }
+
+
+    public static void reverse(int[] array) {
+
+    }
+
+}
