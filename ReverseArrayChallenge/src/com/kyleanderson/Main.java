fe0e7f2d758af410fd20bd65091c3390e0130280
kanderson425 <kyleanderson.dev@gmail.com>
2019-08-10 16:36:45 -0500

    Adds the reversing functionality to the ReverseArrayChallenge

--- ReverseArrayChallenge/src/com/kyleanderson/Main.java
+++ ReverseArrayChallenge/src/com/kyleanderson/Main.java
@@ -1,6 +1,7 @@
 package com.kyleanderson;
 
 import java.util.Arrays;
+import java.util.Collections;
 
 public class Main {
 
@@ -12,11 +13,19 @@ public class Main {
         int[] array = {1,2,3,4,5};
         System.out.println("array = " + Arrays.toString(array));
 
+        reverse(array);
+
     }
 
 
     public static void reverse(int[] array) {
-
+        int i, k, t;
+        for (i = 0; i < array.length / 2; i++) {
+            t = array[i];
+            array[i] = array[array.length - i - 1];
+            array[array.length - i - 1] = t;
+        }
+        System.out.println("Reversed Array is: " + Arrays.toString(array));
     }
 
 }
