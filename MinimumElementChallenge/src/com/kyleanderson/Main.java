dd1576f5cce253713fe513d4fb07da03737f2f0a
kanderson425 <kyleanderson.dev@gmail.com>
2019-08-10 16:14:48 -0500

    Adds remaining functionality of the findMin method

--- MinimumElementChallenge/src/com/kyleanderson/Main.java
+++ MinimumElementChallenge/src/com/kyleanderson/Main.java
@@ -5,41 +5,80 @@ import java.util.Arrays;
 
 public class Main {
 
+    //My Attempt
+//    private static Scanner scanner = new Scanner(System.in);
+//
+//    public static void main(String[] args) {
+//	int[] myIntegers = readIntegers(5);
+//     System.out.println("myIntegers = " + Arrays.toString(myIntegers));
+//    findMin(myIntegers);
+//    }
+//
+//    public static int[] readIntegers(int count) {
+//        int[] array = new int[count];
+//        System.out.println("Enter " + count + " integer values:\r");
+//        for(int i=0; i<array.length; i++) {
+//            array[i] = scanner.nextInt();
+//        }
+//        return array;
+//    }
+//
+//    public static int findMin(int[] myIntegers) {
+//        int min;
+//        int temp;
+//        boolean flag = true;
+//        while(flag) {
+//            flag = false;
+//            for(int i=0; i<myIntegers.length-1; i++) {
+//                if(myIntegers[i] > myIntegers[i+1]) {
+//                    temp = myIntegers[i];
+//                    myIntegers[i] = myIntegers[i + 1];
+//                    myIntegers[i+1] = temp;
+//                    flag = true;
+//
+//                }
+//            }
+//        }
+//        min = myIntegers[0];
+//        System.out.println("The min value is " + min);
+//        return min;
+//    }
+
+    //Solution
     private static Scanner scanner = new Scanner(System.in);
 
     public static void main(String[] args) {
-	int[] myIntegers = readIntegers(5);
-     System.out.println("myIntegers = " + Arrays.toString(myIntegers));
-    findMin(myIntegers);
+        System.out.println("Enter count:");
+        int count = scanner.nextInt();
+        scanner.nextLine();
+
+        int[] returnedArray = readIntegers(count);
+        int returnedMin = findMin(returnedArray);
     }
 
-    public static int[] readIntegers(int count) {
+    private static int[] readIntegers(int count) {
+
         int[] array = new int[count];
-        System.out.println("Enter " + count + " integer values:\r");
         for(int i=0; i<array.length; i++) {
-            array[i] = scanner.nextInt();
+            System.out.println("Enter a number: ");
+            int number = scanner.nextInt();
+            scanner.nextLine();
+            array[i] = number;
         }
         return array;
     }
 
-    public static int findMin(int[] myIntegers) {
-        int min;
-        int temp;
-        boolean flag = true;
-        while(flag) {
-            flag = false;
-            for(int i=0; i<myIntegers.length-1; i++) {
-                if(myIntegers[i] > myIntegers[i+1]) {
-                    temp = myIntegers[i];
-                    myIntegers[i] = myIntegers[i + 1];
-                    myIntegers[i+1] = temp;
-                    flag = true;
+    private static int findMin(int[] array) {
+        int min = Integer.MAX_VALUE;
 
-                }
+        for(int i=0; i<array.length; i++) {
+            int value = array[i];
+
+            if(value < min) {
+                min = value;
             }
         }
-        min = myIntegers[0];
-        System.out.println("The min value is " + min);
         return min;
     }
+
 }
