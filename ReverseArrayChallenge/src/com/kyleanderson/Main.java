cbdc39e55f720ae732247340001b6c272907481c
kanderson425 <kyleanderson.dev@gmail.com>
2019-08-12 20:13:43 -0500

    Adds the print statement for printing out the array

--- ReverseArrayChallenge/src/com/kyleanderson/Main.java
+++ ReverseArrayChallenge/src/com/kyleanderson/Main.java
@@ -21,7 +21,15 @@ public class Main {
 //        System.out.println("Reversed Array is: " + Arrays.toString(array));
 //    }
 
+
     //Solution
+    public static void main(String[] args) {
+        int[] array = {1,5,3,7,11,9,15};
+        System.out.println("Array = " + Arrays.toString(array));
+        reverse(array);
+        System.out.println("Reversed array = " + Arrays.toString(array));
+
+    }
     public static void reverse(int[] array) {
         int maxIndex = array.length - 1;
         int halfLength = array.length / 2;
