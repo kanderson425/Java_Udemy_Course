c0c09c8d3390db832e88a88e04baedbdae67b516
kanderson425 <kyleanderson.dev@gmail.com>
2019-08-18 15:24:07 -0500

    Adds the addItem and modifyItem methods to the main class

--- ListAndArrayList/src/com/kyleanderson/Main.java
+++ ListAndArrayList/src/com/kyleanderson/Main.java
@@ -1,8 +1,69 @@
 package com.kyleanderson;
+import java.util.Scanner;
 
 
 public class Main {
+    private static Scasnner scanner = new Scanner(System.in);
+    private static GroceryList groceryList = new GroceryList();
 
+    public static void main(String[] args) {
+        boolean quit = false;
+        int choice = 0;
+        printInstruction();
+        while(!quit) {
+            System.out.println("Enter your choice: " );
+            choice = scanner.nextIn();
+            scanner.nextLine();
 
+            switch(choice) {
+                case 0:
+                    printInstructions();
+                    break;
+                case 1:
+                    groceryList.printGroceryList();
+                    break;
+                case 2:
+                    addItem();
+                    break;
+                case 3:
+                    modifyItem();
+                    break;
+                case 4:
+                    removeItem();
+                    break;
+                case 5:
+                    searchForItem();
+                    break;
+                case 6:
+                    quit = true;
+                    break;
+            }
+        }
+    }
+
+    public static printInstructions() {
+        System.out.println("\nPress ");
+        System.out.println("\t 0 - To print choice options.");
+        System.out.println("\t 1 - To print the list of grocery items.");
+        System.out.println("\t 2 - To add an item to the list.");
+        System.out.println("\t 3 - To modify an item in the list.");
+        System.out.println("\t 4 - To remove an item from the list.");
+        System.out.println("\t 5 - To search for an item in the list.");
+        System.out.println("\t 6 - To quit the application.");
+    }
+
+    public static void addItem() {
+        System.out.print("Please enter the grocery item: ");
+        groceryList.addGroceryItem(scanner.nextLin());
+    }
+
+    public static void modifyItem() {
+        System.out.print("Enter item number: " );
+        int itemNo = scanner.nextInt();
+        scanner.nextLine();
+        System.out.println("Enter replacement item: " );
+        String newItem = scanner.nextLine();
+        groceryList.modifyGroceryItem(itemNo-1, newItem);
+    }
 }
 
