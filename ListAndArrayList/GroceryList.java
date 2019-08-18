3256671e3e57680a45f7725fb56f20d869afacf8
kanderson425 <kyleanderson.dev@gmail.com>
2019-08-18 15:09:05 -0500

    Adds the modifyGroceryItem and removeGroceryItem methods to the LIstAndArrayist lesson

--- ListAndArrayList/GroceryList.java
+++ ListAndArrayList/GroceryList.java
@@ -15,4 +15,16 @@ public class GroceryList {
             System.out.println((i+1) + ". " + groceryList.get(i));
         }
     }
+
+    public void modifyGroceryItem(int position, String newItem) {
+        groceryList.set(position, newItem);
+        System.out.println("Grocery item " + (position+1) + " has been modified.");
+    }
+
+    public void removeGroceryItem(int position) {
+        String theItem = groceryList.get(position);
+        groceryList.remove(position);
+
+
+    }
 }
