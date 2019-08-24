c4354bedcc7aeb07490cbeddf0f986b4d5959d7c
kanderson425 <kyleanderson.dev@gmail.com>
2019-08-23 21:48:58 -0500

    Adds the printContacts method to the MobilePhone class

--- ArrayListChallenge_Solution/src/com/kyleanderson/Main.java
+++ ArrayListChallenge_Solution/src/com/kyleanderson/Main.java
@@ -11,10 +11,48 @@ public class Main {
 	    startPhone();
 	    printActions();
 	    while(!quit) {
+            System.out.println("\nEnter action: (6 to show available actions)");
+            int action = scanner.nextInt();
+            scanner.nextLine();
+
+            switch(action) {
+                case 0:
+                    System.out.println("\nShutting down...");
+                    quit = true;
+                    break;
+
+                case 1:
+                    printContacts();
+                    break;
+            }
 
         }
     }
+
+    private static void printContacts() {
+        mobilePhone.printContact
+        System.out.println("Contact List");
+        for(int i=0; i<mobilePhone.get)
+
+    }
+
     private static void startPhone() {
         System.out.println("Starting phone...");
     }
+
+    private static void printActions() {
+        System.out.println("\nAvailable actions:\npress");
+        System.out.println("0 - to shutdown\n" +
+                "1 - to print contacts\n" +
+                "2 - to add a new contact\n" +
+                "3 - to update existing an existing contacts\n" +
+                "4 - to remove an existing contact\n" +
+                "5 - query if an existing contact exists\n" +
+                "6 - to print a list of available actions.");
+        System.out.println("Choose your action: ");
+
+
+    }
+
+
 }
--- ArrayListChallenge_Solution/src/com/kyleanderson/MobilePhone.java
+++ ArrayListChallenge_Solution/src/com/kyleanderson/MobilePhone.java
@@ -67,5 +67,14 @@ public class MobilePhone {
         return null;
     }
 
+    public void printContacts() {
+        System.out.println("Contact List");
+        for(int i=0;i<this.myContacts.size(); i++) {
+            System.out.println((i+1) + ","+
+                    this.myContacts.get(i).getName() + " -> " +
+                    this.myContacts.get(i).getPhoneNumber());
+        }
+    }
+
 
 }
