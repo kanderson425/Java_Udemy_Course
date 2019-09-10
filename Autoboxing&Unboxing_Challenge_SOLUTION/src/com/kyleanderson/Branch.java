94225581b4519838a84b57f56b33de300b0e6e44
kanderson425 <kyleanderson.dev@gmail.com>
2019-09-09 20:14:34 -0500

    Adds the newCustomer and the addCustomerTransaction method to the Branch class

--- Autoboxing&Unboxing_Challenge_SOLUTION/src/com/kyleanderson/Branch.java
+++ Autoboxing&Unboxing_Challenge_SOLUTION/src/com/kyleanderson/Branch.java
@@ -1,4 +1,40 @@
 package com.kyleanderson;
 
+import java.util.ArrayList;
+
 public class Branch {
+
+    private String name;
+    private ArrayList<Customer> customers;
+
+    public String getName() {
+        return name;
+    }
+
+    public Branch(String name) {
+        this.name = name;
+        this.customers = new ArrayList<Customer>(); //here we are initializing the customer list for the first time
+
+    }
+
+    public boolean newCustomer(String customerName, double initialAmount) {
+        if(findCustomer(customerName) == null) {
+            this.customers.add(new Customer(customerName, initialAmount));
+            return true;
+        }
+        return false;
+    }
+
+    public boolean addCustomerTransaction(String customerName, double amount) {
+        Customer existingCustomer = findCustomer(customerName);
+        if(existingCustomer != null) {
+            existingCustomer.addTransaction(amount);
+            return true;
+        }
+        return false;
+    }
+
+    private Customer findCustomer(String customerName) {
+
+    }
 }
