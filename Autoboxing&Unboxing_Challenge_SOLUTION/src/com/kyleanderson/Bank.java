a565415fe3a57b01000c7610cdd1aaef63875880
kanderson425 <kyleanderson.dev@gmail.com>
2019-09-10 21:52:32 -0500

    Adds the addBranch method to the newly created Bank class as well as my attempt at the findBranch method to the bank class

---
+++ Autoboxing&Unboxing_Challenge_SOLUTION/src/com/kyleanderson/Bank.java
@@ -0,0 +1,36 @@
+package com.kyleanderson;
+
+import java.util.ArrayList;
+
+public class Bank {
+
+    private String name;
+    private ArrayList<Branch> branches;
+
+    public Bank(String name) {
+        this.name = name;
+        this.branches = new ArrayList<Branch>();
+    }
+
+    public boolean addBranch(String branchName) {
+        if(findBranch(branchName) == null) {
+            this.branches.add(new Branch(branchName));
+            return true;
+        }
+        return false;
+    }
+
+
+
+
+    private Branch findBranch(String branchName) {
+        for(int i = 0; i<branches.size(); i++) {
+            Branch checkedBranch = this.branches.get(i);
+            if (checkedBranch.getName().equals(branchName)) {
+                return checkedBranch;
+            }
+        }
+        return null;
+    }
+
+}
