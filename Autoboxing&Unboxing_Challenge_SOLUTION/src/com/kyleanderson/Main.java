41ef57c6e9bfc5f27cb97b3c60e42a2a560839e2
kanderson425 <kyleanderson.dev@gmail.com>
2019-09-10 22:30:15 -0500

    Adds code to the main method for testing purposes

--- Autoboxing&Unboxing_Challenge_SOLUTION/src/com/kyleanderson/Main.java
+++ Autoboxing&Unboxing_Challenge_SOLUTION/src/com/kyleanderson/Main.java
@@ -3,6 +3,23 @@ package com.kyleanderson;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+	Bank bank = new Bank("Wells Fargo Bank");
+
+	bank.addBranch("Austin");
+
+	bank.addCustomer("Austin", "Kyle", 50.05);
+	bank.addCustomer("Austin", "Sarah", 100.25);
+	bank.addCustomer("Austin", "Curtis", 200.35);
+
+	bank.addBranch("Houston");
+
+	bank.addCustomer("Houston", "Cheryl", 100.10);
+
+	bank.addCustomerTransaction("Austin", "Kyle", 44.22);
+	bank.addCustomerTransaction("Austin","Kyle", 50.98);
+	bank.addCustomerTransaction("Houston", "Cheryl", 25.89);
+
+	bank.listCustomers("Austin", true);
+
     }
 }
