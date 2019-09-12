a5f68699f2b65386891054fbc1cb31ab4e746d00
kanderson425 <kyleanderson.dev@gmail.com>
2019-09-12 17:28:49 -0500

    Adds the constructor, getter and setter methods to the Song class

--- LinkedList_Challenge/src/com/kyleanderson/Song.java
+++ LinkedList_Challenge/src/com/kyleanderson/Song.java
@@ -5,4 +5,25 @@ public class Song {
     private String name;
     private double duration;
 
+
+    public Song(String name, double duration) {
+        this.name = name;
+        this.duration = duration;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public double getDuration() {
+        return duration;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setDuration(double duration) {
+        this.duration = duration;
+    }
 }
