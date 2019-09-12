075e73f70721dd8e361672025e1bc1762319fcee
kanderson425 <kyleanderson.dev@gmail.com>
2019-09-11 21:56:00 -0500

    Adds the addInOrder method to the Demo class which will place entries into alphabetical order

--- LinkedList/src/com/kyleanderson/Demo.java
+++ LinkedList/src/com/kyleanderson/Demo.java
@@ -2,6 +2,7 @@ package com.kyleanderson;
 
 import java.util.Iterator;
 import java.util.LinkedList;
+import java.util.ListIterator;
 
 public class Demo {
     public static void main(String[] args) {
@@ -22,6 +23,8 @@ public class Demo {
         placesToVisit.remove(4);
         printList(placesToVisit);
 
+
+
     }
 
     private static void printList(LinkedList<String> linkedList) {
@@ -31,5 +34,30 @@ public class Demo {
 
         }
         System.out.println("=======================");
+    }
+
+    private static boolean addInOrder(LinkedList<String> linkedList,String newCity) {
+        ListIterator<String> stringListIterator = linkedList.listIterator();
+        while (stringListIterator.hasNext()) {
+            int comparison = stringListIterator.next().compareTo(newCity);
+            if (comparison == 0) {
+                // equals, do not add the city again
+                System.out.println(newCity + " is already included as a destination");
+                return false;
+            } else if (comparison > 0) {
+                // new city should appear before this one
+                //Looking at Brisbane, but Adelaide was passed through...we'd return a number greater than 0
+                stringListIterator.previous();
+                stringListIterator.add(newCity);
+                return true;
+            } else if (comparison < 0) {
+                // move on to next city
+                // Brisbane is passed as a parameter and we are already at Adelaide
+                // B/c we've done the .next() up above we've already passed along and don't need to do anything
+
+            }
+        }
+        stringListIterator.add(newCity);
+        return true;
     }
 }
