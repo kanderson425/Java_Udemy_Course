7eb1ec705958e275594d7b9cd682f43f5d70abba
kanderson425 <kyleanderson.dev@gmail.com>
2019-09-12 17:48:54 -0500

    Adds the properties and constructor for the Playlist method

--- LinkedList_Challenge/src/com/kyleanderson/Playlist.java
+++ LinkedList_Challenge/src/com/kyleanderson/Playlist.java
@@ -1,4 +1,11 @@
 package com.kyleanderson;
 
+import java.util.ArrayList;
+
 public class Playlist {
+    private ArrayList<Album> playlist;
+
+    public Playlist(ArrayList<Album> playlist) {
+        this.playlist = playlist;
+    }
 }
