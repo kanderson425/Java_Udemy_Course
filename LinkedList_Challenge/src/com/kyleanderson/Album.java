78bf9cf7fcf1bf9bda8b795bb9a4af192c9f4469
kanderson425 <kyleanderson.dev@gmail.com>
2019-09-12 17:41:15 -0500

    Adds the Album properties and getters to the Album class

--- LinkedList_Challenge/src/com/kyleanderson/Album.java
+++ LinkedList_Challenge/src/com/kyleanderson/Album.java
@@ -1,4 +1,16 @@
 package com.kyleanderson;
 
+import java.util.ArrayList;
+
 public class Album {
+    private String name;
+    private ArrayList<Song> albumSongList;
+
+    public String getName() {
+        return name;
+    }
+
+    public ArrayList<Song> getAlbumSongList() {
+        return albumSongList;
+    }
 }
