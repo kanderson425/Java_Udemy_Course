650f58f646a1cec118bd3cea4152fc0c947f885f
kanderson425 <kyleanderson.dev@gmail.com>
2019-09-11 22:21:03 -0500

    Updates the code to account for catch infinite loops on hitting previous and next actions

--- LinkedList/src/com/kyleanderson/Demo.java
+++ LinkedList/src/com/kyleanderson/Demo.java
@@ -61,6 +61,7 @@ public class Demo {
     private static void visit(LinkedList cities) {
         Scanner scanner = new Scanner(System.in);
         boolean quit = false;
+        boolean goingForward = true;
         ListIterator<String> listIterator = cities.listIterator();
 
         if(cities.isEmpty()) {
@@ -80,17 +81,31 @@ public class Demo {
                     quit = true;
                     break;
                 case 1:
+                    if(!goingForward) {
+                        if(listIterator.hasNext()) {
+                            listIterator.next();
+                        }
+                        goingForward = true;
+                    }
                     if(listIterator.hasNext()) {
                         System.out.println("Now visiting " + listIterator.next());
                     } else {
                         System.out.println("Reached the end of the list");
+                        goingForward = false;
                     }
                     break;
                 case 2:
+                    if(goingForward) {
+                        if(listIterator.hasPrevious()) {
+                            listIterator.previous();
+                        }
+                        goingForward = false;
+                    }
                     if(listIterator.hasPrevious()) {
                         System.out.println("Now visiting " + listIterator.previous());
                     } else {
                         System.out.println("We are at the start of the list");
+                        goingForward = true;
                     }
                     break;
                 case 3:
