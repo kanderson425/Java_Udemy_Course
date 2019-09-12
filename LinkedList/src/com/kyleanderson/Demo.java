9bf21e5e3f5ded506cffe3f6858e7827666acc49
kanderson425 <kyleanderson.dev@gmail.com>
2019-09-11 22:00:03 -0500

    Validates that the addInOrder is working

--- LinkedList/src/com/kyleanderson/Demo.java
+++ LinkedList/src/com/kyleanderson/Demo.java
@@ -7,23 +7,14 @@ import java.util.ListIterator;
 public class Demo {
     public static void main(String[] args) {
         LinkedList<String> placesToVisit = new LinkedList<String>();
-        placesToVisit.add("Sydney");
-        placesToVisit.add("Melbourne");
-        placesToVisit.add("Brisbane");
-        placesToVisit.add("Perth");
-        placesToVisit.add("Canberra");
-        placesToVisit.add("Adelaide");
-        placesToVisit.add("Darwin");
-
+        addInOrder(placesToVisit, "Sydney");
+        addInOrder(placesToVisit, "Melbourne");
+        addInOrder(placesToVisit, "Brisbane");
+        addInOrder(placesToVisit, "Perth");
+        addInOrder(placesToVisit, "Canberra");
+        addInOrder(placesToVisit, "Adelaide");
+        addInOrder(placesToVisit, "Darwin");
         printList(placesToVisit);
-
-        placesToVisit.add(1,"Alice Springs");
-        printList(placesToVisit);
-
-        placesToVisit.remove(4);
-        printList(placesToVisit);
-
-
 
     }
 
