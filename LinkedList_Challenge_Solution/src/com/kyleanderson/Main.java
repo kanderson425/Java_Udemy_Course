d133fbaf26bbb12fc220ba7acaf0a8ea4762f7f1
kanderson425 <kyleanderson.dev@gmail.com>
2019-09-13 07:01:58 -0500

    Adds albums and songs into our main method

--- LinkedList_Challenge_Solution/src/com/kyleanderson/Main.java
+++ LinkedList_Challenge_Solution/src/com/kyleanderson/Main.java
@@ -1,8 +1,21 @@
 package com.kyleanderson;
 
+import java.util.ArrayList;
+
 public class Main {
+    private static ArrayList<Album> albums = new ArrayList<Album>();
 
     public static void main(String[] args) {
-	// write your code here
+	Album album = new Album("Stormbringer", "Deep Purple");
+	album.addSong("Stormbringer", 4.6);
+	album.addSong("Soldier of Fortune", 3.13);
+	album.addSong("Space Truckin'", 5.2);
+	albums.add(album);
+
+	album = new Album("For those about to rock", "AC/DC");
+	album.addSong("For those about to rock", 5.44);
+	album.addSong("Highway to Hell", 3.25);
+
+
     }
 }
