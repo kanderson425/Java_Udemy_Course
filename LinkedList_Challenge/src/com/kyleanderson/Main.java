3ba89855d82fbef0de7409264dc79ca5f966678e
kanderson425 <kyleanderson.dev@gmail.com>
2019-09-12 22:38:01 -0500

    Adds the majority of functionality for the playlistAction method in the Playlist class

--- LinkedList_Challenge/src/com/kyleanderson/Main.java
+++ LinkedList_Challenge/src/com/kyleanderson/Main.java
@@ -6,14 +6,8 @@ public class Main {
 
     public static void main(String[] args) {
         LinkedList<String> playList = new LinkedList<String>();
+
     }
 
-    public static void printMenu() {
-        System.out.println("Available actions:\npresss");
-        System.out.println("0 - to quit\n");
-        System.out.println("1 - to skip forward a song\n");
-        System.out.println("2 - to skip backward a song\n");
-        System.out.printf("3 - replay current song\n");
-        System.out.println("4 - list all songs in the playlist\n");
-    }
+
 }
--- LinkedList_Challenge/src/com/kyleanderson/Playlist.java
+++ LinkedList_Challenge/src/com/kyleanderson/Playlist.java
@@ -1,8 +1,6 @@
 package com.kyleanderson;
 
-import java.util.ArrayList;
-import java.util.Iterator;
-import java.util.LinkedList;
+import java.util.*;
 
 public class Playlist {
     private ArrayList<Album> playlist;
@@ -37,4 +35,71 @@ public class Playlist {
         return null;
     }
 
+    private static void playlistAction(LinkedList playlist) {
+        Scanner scanner = new Scanner(System.in);
+        boolean quit = false;
+        boolean goingForward = true;
+        ListIterator<String> listIterator = playlist.listIterator();
+
+        if(playlist.isEmpty()) {
+            System.out.println("No songs in the playlist");
+            return;
+        } else {
+            System.out.println("Now playing " + listIterator.next());
+            printMenu();
+        }
+        while(!quit) {
+            int action = scanner.nextInt();
+            scanner.nextLine();
+            switch(action) {
+                case 0:
+                    System.out.println("Music has now stopped");
+                    quit = true;
+                    break;
+                case 1:
+                    if(!goingForward) {
+                        if(listIterator.hasNext()) {
+                            listIterator.next();
+                        }
+                        goingForward = true;
+                    }
+                    if(listIterator.hasNext()) {
+                        System.out.println("Now playing " + listIterator.next());
+                    } else {
+                        System.out.println("Reached the end of the playlist");
+                        goingForward = false;
+                    }
+                    break;
+                case 2:
+                    if(goingForward) {
+                        if(listIterator.hasPrevious()) {
+                            listIterator.previous();
+                        }
+                        goingForward = false;
+                    }
+                    if(listIterator.hasPrevious()) {
+                        System.out.println("Now playing " + listIterator.previous());
+                    } else {
+                        System.out.println("We are at the first song of the playlist");
+                        goingForward = true;
+                    }
+                    break;
+                case 3:
+                    System.out.println("Song is attempting to replay");
+                    break;
+                case 4:
+                    System.out.println("Attempting to list all songs in playlist");
+                    break;
+            }
+        }
+    }
+
+    public static void printMenu() {
+        System.out.println("Available actions:\npresss");
+        System.out.println("0 - to quit\n");
+        System.out.println("1 - to skip forward a song\n");
+        System.out.println("2 - to skip backward a song\n");
+        System.out.println("3 - replay current song\n");
+        System.out.println("4 - list all songs in the playlist\n");
+    }
 }
