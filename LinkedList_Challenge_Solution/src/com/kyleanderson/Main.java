9a973b929a7989204dd0580690e68ccb62a4f07b
kanderson425 <kyleanderson.dev@gmail.com>
2019-09-15 12:08:19 -0500

    Adds the play method to the Main class

--- LinkedList_Challenge_Solution/src/com/kyleanderson/Main.java
+++ LinkedList_Challenge_Solution/src/com/kyleanderson/Main.java
@@ -2,6 +2,7 @@ package com.kyleanderson;
 
 import java.util.ArrayList;
 import java.util.LinkedList;
+import java.util.ListIterator;
 
 public class Main {
     private static ArrayList<Album> albums = new ArrayList<Album>();
@@ -21,9 +22,26 @@ public class Main {
 
 	LinkedList<Song> playList = new LinkedList<Song>();
 	albums.get(0).addToPlaylist("Stormbringer", playList);
+	albums.get(0).addToPlaylist("Soldier of Fortune", playList);
+	albums.get(0).addToPlaylist("Speed King", playList); // DOES NOT EXIST
 
+	albums.get(0).addToPlaylist(5, playList);
+	albums.get(1).addToPlaylist(4, playList);
+	albums.get(1).addToPlaylist(3, playList);
+	albums.get(1).addToPlaylist(2, playList);
+	albums.get(1).addToPlaylist(24, playList);	 //There is no track 24
 
-
+	play(playList);
 
     }
+
+    private static void play(LinkedList<Song> playList) {
+		ListIterator<Song> listeIterator = playList.listIterator();
+		if(playList.size() ==0) {
+			System.out.println("No songs in playlist");
+			return;
+		} else {
+			System.out.println("Now playing " + listeIterator.next().toString());
+		}
+	}
 }
