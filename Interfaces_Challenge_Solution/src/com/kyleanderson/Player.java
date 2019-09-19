c4f517b46df4264c1b9e1621ab1690d195cbd464
kanderson425 <kyleanderson.dev@gmail.com>
2019-09-19 08:40:32 -0500

    Adds the getters and setters to the Player class along with the toString override

---
+++ Interfaces_Challenge_Solution/src/com/kyleanderson/Player.java
@@ -0,0 +1,57 @@
+package com.kyleanderson;
+
+public class Player implements ISaveable {
+    private String name;
+    private int hitPoints;
+    private int strength;
+    private String weapon;
+
+    public Player(String name, int hitPoints, int strength) {
+        this.name = name;
+        this.hitPoints = hitPoints;
+        this.strength = strength;
+        this.weapon = "Sword";
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getHitPoints() {
+        return hitPoints;
+    }
+
+    public int getStrength() {
+        return strength;
+    }
+
+    public String getWeapon() {
+        return weapon;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setHitPoints(int hitPoints) {
+        this.hitPoints = hitPoints;
+    }
+
+    public void setStrength(int strength) {
+        this.strength = strength;
+    }
+
+    public void setWeapon(String weapon) {
+        this.weapon = weapon;
+    }
+
+    @Override
+    public String toString() {
+        return "Player{" +
+                "name='" + name + '\'' +
+                ", hitPoints=" + hitPoints +
+                ", strength=" + strength +
+                ", weapon='" + weapon + '\'' +
+                '}';
+    }
+}
