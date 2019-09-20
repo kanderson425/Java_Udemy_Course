e8adbc8cc4edafd9b2705c5355b004c8028257e7
kanderson425 <kyleanderson.dev@gmail.com>
2019-09-20 16:24:53 -0500

    Adds the underlying code of the read method

--- Interfaces_Challenge_Solution/src/com/kyleanderson/Monster.java
+++ Interfaces_Challenge_Solution/src/com/kyleanderson/Monster.java
@@ -46,6 +46,11 @@ public class Monster implements ISaveable {
 
     @Override
     public void read(List<String> savedValues) {
+        if(savedValues != null && savedValues.size() > 0) {
+            this.name = savedValues.get(0);
+            this.hitPoints = Integer.parseInt(savedValues.get(1));
+            this.strength = Integer.parseInt(savedValues.get(2));
+        }
 
     }
 }
