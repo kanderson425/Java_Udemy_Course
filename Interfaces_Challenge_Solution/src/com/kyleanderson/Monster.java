357aa5e0fe19c704b73d1a234d586776d73d8e63
kanderson425 <kyleanderson.dev@gmail.com>
2019-09-20 16:19:20 -0500

    Adds the Monster class including the Monster constructor and property getters

---
+++ Interfaces_Challenge_Solution/src/com/kyleanderson/Monster.java
@@ -0,0 +1,29 @@
+package com.kyleanderson;
+
+public class Monster implements ISaveable {
+    private String name;
+    private int hitPoints;
+    private int strength;
+
+    public Monster(String name, int hitPoints, int strength) {
+        this.name = name;
+        this.hitPoints = hitPoints;
+        this.strength = strength;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getHitPoints() {
+        return hitPoints;
+    }
+
+    public int getStrength() {
+        return strength;
+    }
+}
+
+
+
+
