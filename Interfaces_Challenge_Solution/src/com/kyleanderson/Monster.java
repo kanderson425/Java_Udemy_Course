54ad1e89e7c7b83c891900ad6b4c0253510c99e4
kanderson425 <kyleanderson.dev@gmail.com>
2019-09-20 16:21:24 -0500

    Adds the Read and Write methods to the Monster class

--- Interfaces_Challenge_Solution/src/com/kyleanderson/Monster.java
+++ Interfaces_Challenge_Solution/src/com/kyleanderson/Monster.java
@@ -1,5 +1,8 @@
 package com.kyleanderson;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Monster implements ISaveable {
     private String name;
     private int hitPoints;
@@ -21,6 +24,29 @@ public class Monster implements ISaveable {
 
     public int getStrength() {
         return strength;
+    }
+
+    @Override
+    public String toString() {
+        return "Monster{" +
+                "name='" + name + '\'' +
+                ", hitPoints=" + hitPoints +
+                ", strength=" + strength +
+                '}';
+    }
+
+    @Override
+    public List<String> write() {
+        ArrayList<String> values = new ArrayList<String>();
+        values.add(0, this.name);
+        values.add(1, "" + this.hitPoints);
+        values.add(2, "" + this.strength);
+        return values;
+    }
+
+    @Override
+    public void read(List<String> savedValues) {
+
     }
 }
 
