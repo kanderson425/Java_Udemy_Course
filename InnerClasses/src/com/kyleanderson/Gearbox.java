5f18ec521a9a2f0ae1bf716d3b5f33a1bba6474f
kanderson425 <kyleanderson.dev@gmail.com>
2019-09-21 15:37:24 -0500

    Adds the operateClutch, addGear, changeGear and the wheelSpeed methods to the Gear and GearBox method

--- InnerClasses/src/com/kyleanderson/Gearbox.java
+++ InnerClasses/src/com/kyleanderson/Gearbox.java
@@ -5,7 +5,8 @@ import java.util.ArrayList;
 public class Gearbox {
     private ArrayList<Gear> gears;
     private int maxGears;
-    private int gearNumber = 0;
+    private int currentGear = 0;
+    private boolean cluthIsIn;
 
     public Gearbox(int maxGears) {
         this.maxGears = maxGears;
@@ -14,7 +15,36 @@ public class Gearbox {
         this.gears.add(neutral);
     }
 
-    public class Gear {
+    public void operateClutch(boolean in) {
+        this.cluthIsIn = in;
+    }
+
+    public void addGear(int number, double ratio) {
+        if((number <0) && (number <= maxGears)) {
+            this.gears.add(new Gear(number, ratio));
+        }
+    }
+
+    public void changeGear(int newGear) {
+        if((newGear >= 0) && (newGear < this.gears.size()) && (this.cluthIsIn)) {
+            this.currentGear = newGear;
+            System.out.println("Gear " + newGear + " selected.");
+        } else {
+            System.out.println("Grind!");
+            this.currentGear = 0;
+        }
+    }
+
+    public double wheelSpeed(int revs) {
+        if(cluthIsIn) {
+            System.out.println("Scream!!!");
+            return 0.0;
+        }
+        return revs * gears.get(currentGear).getRatio();
+    }
+
+
+    private class Gear {
         private int gearNumber;
         private double ratio;
 
@@ -23,8 +53,14 @@ public class Gearbox {
             this.ratio = ratio;
         }
 
+        public double getRatio() {
+            return ratio;
+        }
+
         public double driveSpeed(int revs) {
             return revs * (this.ratio);
         }
     }
+
+
 }
--- InnerClasses/src/com/kyleanderson/Main.java
+++ InnerClasses/src/com/kyleanderson/Main.java
@@ -4,5 +4,12 @@ public class Main {
 
     public static void main(String[] args) {
 	//4 Types of inner classes: static nested, non-static class (inner class), inner class, anonymous class
+
+        Gearbox mcLaren = new Gearbox(6);
+        Gearbox.Gear first = mcLaren.new Gear(1,12.3);
+//        Gearbox.Gear second = new Gearbox.Gear(2,15.4); //you can't do this
+//        Gearbox.Gear third = new mcLaren.Gear(3,17.0); // you cannot do this either b/c you need to create a new instance of gear on the mclaren object
+        System.out.println(first.driveSpeed(1000));
+
     }
 }
