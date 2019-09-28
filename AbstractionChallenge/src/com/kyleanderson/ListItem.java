a137ec0fa85b22a8ae9ee60806b6b11fd6960c35
kanderson425 <kyleanderson.dev@gmail.com>
2019-09-28 08:50:17 -0500

    Adds the nextItem, previousItem, setNextItem and setPreviousItem methods within the ListItem abstract class

--- AbstractionChallenge/src/com/kyleanderson/ListItem.java
+++ AbstractionChallenge/src/com/kyleanderson/ListItem.java
@@ -3,4 +3,14 @@ package com.kyleanderson;
 public abstract class ListItem {
 
 
+
+    public abstract void nextItem();
+
+    public abstract void previousItem();
+
+    public abstract void setNextItem();
+
+    public abstract void setPreviousItem();
+
+
 }
