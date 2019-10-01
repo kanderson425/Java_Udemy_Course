f0e374f13fb0e48d6bc71af4f0afb34d39e9a7d7
kanderson425 <kyleanderson.dev@gmail.com>
2019-09-30 19:23:50 -0500

    Adds the constructor to the ListItem abstract class

---
+++ AbstractionChallenge_Solution/src/com/kyleanderson/ListItem.java
@@ -0,0 +1,12 @@
+package com.kyleanderson;
+
+public abstract class ListItem {
+    protected ListItem rightLink = null;
+    protected ListItem leftLink = null;
+
+    protected Object value;
+
+    public ListItem(Object value) {
+        this.value = value;
+    }
+}
