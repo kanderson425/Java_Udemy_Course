651381aada4a29139e0583b28c871becd547d01c
kanderson425 <kyleanderson.dev@gmail.com>
2019-10-02 18:10:42 -0500

    Adds the underlying code to the addItem method within the MyLinkedList class

--- AbstractionChallenge_Solution/src/com/kyleanderson/MyLinkedList.java
+++ AbstractionChallenge_Solution/src/com/kyleanderson/MyLinkedList.java
@@ -9,11 +9,50 @@ public class MyLinkedList implements NodeList {
 
     @Override
     public ListItem getRoot() {
-        return null;
+        return this.root;
     }
 
     @Override
-    public boolean addItem(ListItem item) {
+    public boolean addItem(ListItem newItem) {
+        if (this.root == null) {
+            // The list was empty, so the list item becomes the head of the list
+            this.root = newItem;
+            return true;
+        }
+
+        ListItem currentItem = this.root;
+        while (currentItem != null) {
+            int comparison = (currentItem.compareTo(newItem));
+            if (comparison < 0) {
+                // newItem is greater, move right if possible
+                if (currentItem.next() != null) {
+                    currentItem = currentItem.next();
+                } else {
+                    // there is no next, so insert at the end of the list
+                    currentItem.setNext(newItem);
+                    newItem.setPrevious(currentItem);
+                    return true;
+                }
+            } else if (comparison > 0) {
+                // newItem is less, insert before
+                if (currentItem.previous() != null) {
+                    currentItem.previous().setNext(newItem);
+                    newItem.setPrevious(currentItem.previous());
+                    newItem.setNext(currentItem);
+                    currentItem.setPrevious(newItem);
+                } else {
+                    // the node with a previous is the root
+                    newItem.setNext(this.root);
+                    this.root.setPrevious(newItem);
+                    this.root = newItem;
+                }
+                return true;
+            } else {
+                // equal
+                System.out.println(newItem.getValue() + " is already present, not added.");
+                return false;
+            }
+        }
         return false;
     }
 
