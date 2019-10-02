8f2a5e0c120cf2f7d3c2e27a5c4eaa1d64b0b222
kanderson425 <kyleanderson.dev@gmail.com>
2019-10-02 18:01:04 -0500

    Adds the NodeList interface

---
+++ AbstractionChallenge_Solution/src/com/kyleanderson/NodeList.java
@@ -0,0 +1,10 @@
+package com.kyleanderson;
+
+public interface NodeList {
+
+    ListItem getRoot();
+    boolean addItem(ListItem item);
+    boolean removeItem(ListItem item);
+    void tranverse(ListItem item);
+
+}
