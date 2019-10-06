3bee0ea14c45a7be9e071d9f0dcc678c3ccb37cd
kanderson425 <kyleanderson.dev@gmail.com>
2019-10-06 16:39:44 -0500

    Adds the underlying code  for the Traverse method of the SearchTree class, including recursion

--- AbstractionChallenge_Solution/src/com/kyleanderson/Main.java
+++ AbstractionChallenge_Solution/src/com/kyleanderson/Main.java
@@ -4,24 +4,24 @@ public class Main {
 
     public static void main(String[] args) {
 
-        MyLinkedList list = new MyLinkedList(null);
-        list.traverse(list.getRoot());
+        SearchTree tree = new SearchTree(null);
+        tree.traverse(tree.getRoot());
     // Create a string data array to avoid multiple loads of addItem instructions:
 	String stringData = "5 7 3 9 8 2 1 0 4 6";
 
 
 	String[] data = stringData.split(" ");
 	for(String s : data) {
-	    list.addItem(new Node(s));
+	    tree.addItem(new Node(s));
     }
 
 
-	list.traverse(list.getRoot());
-	list.removeItem(new Node("3"));
-	list.traverse(list.getRoot());
+	tree.traverse(tree.getRoot());
+	tree.removeItem(new Node("3"));
+	tree.traverse(tree.getRoot());
 
-	list.removeItem(new Node("5"));
-	list.traverse(list.getRoot());
+	tree.removeItem(new Node("5"));
+	tree.traverse(tree.getRoot());
 
 
     }
---
+++ AbstractionChallenge_Solution/src/com/kyleanderson/SearchTree.java
@@ -0,0 +1,37 @@
+package com.kyleanderson;
+
+import java.util.List;
+
+public class SearchTree implements NodeList{
+    private ListItem root = null;
+
+    public SearchTree(ListItem root) {
+        this.root = root;
+    }
+
+    @Override
+    public ListItem getRoot() {
+        return this.root;
+    }
+
+    @Override
+    public boolean addItem(ListItem item) {
+        return false;
+    }
+
+    @Override
+    public boolean removeItem(ListItem item) {
+        return false;
+    }
+
+    @Override
+    public void traverse(ListItem item) {
+        // recursive method
+        if (root != null){
+               traverse(root.previous());
+            System.out.println(root.getValue());
+            traverse(root.next());
+        }
+
+    }
+}
