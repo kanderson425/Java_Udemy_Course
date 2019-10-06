dd999f7724a60c6b7bc1529cb7935beb657caee7
kanderson425 <kyleanderson.dev@gmail.com>
2019-10-06 17:11:11 -0500

    Adds the inner code to the performRemoval method within the SearchTree class

--- AbstractionChallenge_Solution/src/com/kyleanderson/SearchTree.java
+++ AbstractionChallenge_Solution/src/com/kyleanderson/SearchTree.java
@@ -78,6 +78,56 @@ public class SearchTree implements NodeList{
         return false;
     }
 
+    private void performRemoval(ListItem item, ListItem parent) {
+        // remove item from the tree
+        if (item.next() == null) {
+            // no right tree, so make parent point to left tree (which may be null)
+            if (parent.next() == item) {
+                // item is right child of its parent
+                parent.setNext(item.previous());
+            } else if (parent.previous() == item) {
+                // item is left child of its parent
+                parent.setPrevious(item.previous());
+            } else {
+                // parent must be item, which means we were looking at the root of the tree
+                this.root = item.previous();
+            }
+        } else if(item.previous() == null) {
+            // no left tree, so make parent point to right tree (which may be null)
+            if (parent.next() == item) {
+                // item is right child of its parent
+                parent.setNext(item.next());
+            } else if(parent.previous() == item) {
+                // item is left child of its parent
+                parent.setPrevious(item.next());
+            } else {
+                // again, we are deleting the root
+                this.root = item.next();
+            }
+        } else {
+            // neither left nor right are null, deletion is now a lot trickier!
+            // From the right sub-tree, find the smallest value (i.e., the leftmost)
+            ListItem current = item.next();
+            ListItem leftmostParent = item;
+            while(current.previous() != null) {
+                leftmostParent = current;
+                current = current.previous();
+            }
+            // Now put the smallest value into our node to be deleted
+            item.setValue(current.getValue());
+            // and delete the smallest
+            if(leftmostParent == item) {
+                // there was no leftmost node, so 'current' points to the smallest
+                // node (the one that must now be deleted).
+                item.setNext(current.next());
+            } else {
+                // set the smallest node's parent to point to
+                // the smallest node's right child (which may be null).
+                leftmostParent.setPrevious(current.next());
+            }
+        }
+    }
+
     @Override
     public void traverse(ListItem root) {
         // recursive method
