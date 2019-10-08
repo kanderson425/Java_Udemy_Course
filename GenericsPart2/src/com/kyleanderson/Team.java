1846970fcff3991f520ac30e98d6d8a9efa11037
kanderson425 <kyleanderson.dev@gmail.com>
2019-10-07 21:04:44 -0500

    Parametizes the addPlayer method within the Team class to accept a paramet of type T, allowing us to pass in all of our types of players

--- GenericsPart2/src/com/kyleanderson/Team.java
+++ GenericsPart2/src/com/kyleanderson/Team.java
@@ -2,14 +2,14 @@ package com.kyleanderson;
 
 import java.util.ArrayList;
 
-public class Team {
+public class Team<T> {
     private String name;
     int played = 0;
     int won = 0;
     int lost = 0;
     int tied = 0;
 
-    private ArrayList<Player> members = new ArrayList<>(); // remember: you do NOT need to add "Player" into the second diamond b/c it inherits from the first diamond
+    private ArrayList<T> members = new ArrayList<>(); // remember: you do NOT need to add "Player" into the second diamond b/c it inherits from the first diamond
 
     public Team(String name) {
         this.name = name;
@@ -19,13 +19,13 @@ public class Team {
         return name;
     }
 
-    public boolean addPlayer(Player player) {
+    public boolean addPlayer(T player) {
         if(members.contains(player)) {
-            System.out.println(player.getName() + " is already on the team.");
+            System.out.println(((Player) player).getName() + " is already on the team.");
             return false;
         } else {
             members.add(player);
-            System.out.println(player.getName() + " picked for team " + this.name);
+            System.out.println(((Player) player).getName() + " picked for team " + this.name);
             return true;
         }
     }
