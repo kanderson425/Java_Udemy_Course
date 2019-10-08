f5170a729b3e28a77ba481db1fb66f2357bcd2c3
kanderson425 <kyleanderson.dev@gmail.com>
2019-10-07 20:53:09 -0500

    Adds the addPlayer method with degrees of validation into the Team class

--- GenericsPart2/src/com/kyleanderson/Team.java
+++ GenericsPart2/src/com/kyleanderson/Team.java
@@ -1,5 +1,7 @@
 package com.kyleanderson;
 
+import java.util.ArrayList;
+
 public class Team {
     private String name;
     int played = 0;
@@ -7,4 +9,24 @@ public class Team {
     int lost = 0;
     int tied = 0;
 
+    private ArrayList<Player> members = new ArrayList<>(); // remember: you do NOT need to add "Player" into the second diamond b/c it inherits from the first diamond
+
+    public Team(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public boolean addPlayer(Player player) {
+        if(members.contains(player)) {
+            System.out.println(player.getName() + " is already on the team.");
+            return false;
+        } else {
+            members.add(player);
+            System.out.println(player.getName() + " picked for team " + this.name);
+            return true;
+        }
+    }
 }
