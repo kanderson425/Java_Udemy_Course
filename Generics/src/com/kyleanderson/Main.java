3ccdf8a34bd41a0631509728baffc867414ea17a
kanderson425 <kyleanderson.dev@gmail.com>
2019-10-07 20:34:31 -0500

    Adds the parametized type to the ArrayList within our main class

--- Generics/src/com/kyleanderson/Main.java
+++ Generics/src/com/kyleanderson/Main.java
@@ -5,7 +5,7 @@ import java.util.ArrayList;
 public class Main {
 
     public static void main(String[] args) {
-        ArrayList items = new ArrayList();
+        ArrayList<Integer> items = new ArrayList<Integer>();
 
         items.add(1);
         items.add(2);
@@ -16,9 +16,9 @@ public class Main {
         printDoubled(items);
     }
 
-	private static void printDoubled(ArrayList n) {
+	private static void printDoubled(ArrayList<Integer> n) {
 	    for(Object i : n) {
-            System.out.println((Integer) i *2);
+            System.out.println((Integer) i * 2);
         }
     }
 }
