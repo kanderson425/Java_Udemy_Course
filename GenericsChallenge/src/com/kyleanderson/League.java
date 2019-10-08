1f1f7ea138530a4dd7a2cb851d7c01f2b0440fbf
kanderson425 <kyleanderson.dev@gmail.com>
2019-10-08 17:39:20 -0500

    Adds the compareTo and ranking method to the League class

--- GenericsChallenge/src/com/kyleanderson/League.java
+++ GenericsChallenge/src/com/kyleanderson/League.java
@@ -2,7 +2,7 @@ package com.kyleanderson;
 
 import java.util.ArrayList;
 
-public class League<T> {
+public class League<T> implements Comparable<League<T>>{
     private String name;
     int wins = 0;
 
@@ -18,6 +18,30 @@ public class League<T> {
     }
 
     public boolean addTeam(T team) {
-
+        if(teams.contains(team)) {
+            System.out.println(((Team) team).getName() + " is already in the league.");
+            return false;
+        } else {
+            teams.add(team);
+            System.out.println(((Team) team).getName() + " added to this league: " + this.name);
+            return true;
+        }
     }
+
+    public int ranking() {
+        return (wins * 2);
+    }
+
+    @Override
+    public int compareTo(League<T> team) {
+        if(this.ranking() > team.ranking()) {
+            return -1;
+        } else if(this.ranking() < team.ranking()) {
+            return 1;
+        } else {
+            return 0;
+        }
+    }
+
+
 }
