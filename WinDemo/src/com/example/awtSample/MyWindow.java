93a555f5f9d88c8576d4ca95b598621b0059e299
kanderson425 <kyleanderson.dev@gmail.com>
2019-10-14 11:09:01 -0500

    Adds the windowClosing and paint overrides

--- WinDemo/src/com/example/awtSample/MyWindow.java
+++ WinDemo/src/com/example/awtSample/MyWindow.java
@@ -6,14 +6,30 @@ import java.awt.event.WindowEvent;
 
 public class MyWindow extends Frame {
 
+
+
     public MyWindow(String title) {
         super(title);
         setSize(500, 140);
         addWindowListener(new WindowAdapter() {
             @Override
-            public void windowClosed(WindowEvent e) {
+            public void windowClosing(WindowEvent e) {
                 System.exit(0);
             }
         });
     }
+
+    @Override
+    public void paint(Graphics g) {
+        super.paint(g);
+        Font sansSerifLarge = new Font("SansSerif", Font.BOLD, 18);
+        Font sansSerifSmall = new Font("SansSerif",Font.BOLD, 12);
+        g.setFont(sansSerifLarge);
+        g.drawString("The Complete Java Developer Course", 60, 60);
+        g.setFont(sansSerifSmall);
+        g.drawString("by Kyle Anderson", 60, 100);
+
+    }
+
+
 }
