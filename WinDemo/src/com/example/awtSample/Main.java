698ee0f35c4858710df8b245974e285789f27e6d
kanderson425 <kyleanderson.dev@gmail.com>
2019-10-14 11:53:04 -0500

    Updates the PackageDemo with the inner code from the InterfacesChallengeSolution monster/player game

--- WinDemo/src/com/example/awtSample/Main.java
+++ WinDemo/src/com/example/awtSample/Main.java
@@ -3,6 +3,7 @@ package com.example.awtSample;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+	MyWindow myWindow = new MyWindow("Complete Java");
+	myWindow.setVisible(true);
     }
 }
