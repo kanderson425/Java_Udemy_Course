93f76ea4690508a84b5c97113fde790bf9849061
kanderson425 <kyleanderson.dev@gmail.com>
2019-10-15 20:35:01 -0500

    Adds the fibonnaci method to the Series class

--- PackageChallenge/src/com/kyleanderson/Main.java
+++ PackageChallenge/src/com/kyleanderson/Main.java
@@ -11,12 +11,23 @@ public class Main {
 
 
 
-        Series.factorial(0);
-        Series.factorial(1);
-        Series.factorial(2);
-        Series.factorial(3);
-        Series.factorial(4);
-        Series.factorial(5);
-        Series.factorial(6);
+//        Series.factorial(0);
+//        Series.factorial(1);
+//        Series.factorial(2);
+//        Series.factorial(3);
+//        Series.factorial(4);
+//        Series.factorial(5);
+//        Series.factorial(6);
+
+
+
+        Series.fibonnaci(0);
+        Series.fibonnaci(1);
+        Series.fibonnaci(2);
+        Series.fibonnaci(3);
+        Series.fibonnaci(4);
+        Series.fibonnaci(5);
+        Series.fibonnaci(6);
+        Series.fibonnaci(7);
     }
 }
--- PackageChallenge/src/com/kyleanderson/Series.java
+++ PackageChallenge/src/com/kyleanderson/Series.java
@@ -7,7 +7,7 @@ public class Series {
 
     public static int nSum(int n) {
         int sum = 0;
-        for(int i = 0;i <= n; i++) {
+        for (int i = 0; i <= n; i++) {
             sum += i;
         }
         System.out.println(sum);
@@ -20,11 +20,26 @@ public class Series {
         if (n == 0) {
             product = 0;
         } else {
-        for (int i = 1; i <= n; i++) {
-            product *= i;
+            for (int i = 1; i <= n; i++) {
+                product *= i;
             }
         }
         System.out.println(product);
         return product;
+    }
+
+    public static int fibonnaci(int n) {
+        int a = 0, b = 1, c;
+        if (n == 0) {
+            return a;
+        } else {
+            for (int i = 2; i <= n; i++) {
+                c = a + b;
+                a = b;
+                b = c;
+            }
+        }
+        System.out.println(b);
+        return b;
     }
 }
