ccb6fa3e5d4cb811c8060d4a5589aefbb5892f41
kanderson425 <kyleanderson.dev@gmail.com>
2019-10-15 20:49:21 -0500

    Creates the PackageChallenge_2 project and imports the Series class from the PackageChallenge package

---
+++ PackageChallenge_2/src/com/kyleanderson/Main.java
@@ -0,0 +1,15 @@
+package com.kyleanderson;
+
+import com.example.packageChallenge.Series;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Series.nSum(5);
+
+        Series.factorial(5);
+
+        Series.fibonnaci(5);
+    }
+}
