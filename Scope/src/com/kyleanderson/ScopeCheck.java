85f93232e9a606e65800ab460eb137c154bbab75
kanderson425 <kyleanderson.dev@gmail.com>
2019-10-16 22:44:23 -0500

    Adds the InnerClass inner class to the ScopeCheck class

--- Scope/src/com/kyleanderson/ScopeCheck.java
+++ Scope/src/com/kyleanderson/ScopeCheck.java
@@ -18,5 +18,22 @@ public class ScopeCheck {
         for(int i=0; i<10; i++) {
             System.out.println(i + " times two is " + i * privateVar);
         }
+//        System.out.println("Value of i is now " + i);
+    }
+
+    public class InnerClass {
+        public int privateVar = 3;
+
+        public InnerClass() {
+            System.out.println("InnerClass created, privateVar is " + privateVar);
+        }
+
+        public void timesTwo() {
+            int privateVar = 2;
+            for(int i=0; i<10; i++) {
+                System.out.println(i + " times two is " + i * privateVar);
+            }
+//        System.out.println("Value of i is now " + i);
+        }
     }
 }
