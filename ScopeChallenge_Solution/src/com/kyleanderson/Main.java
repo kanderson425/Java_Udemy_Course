b8be8f4ba14ae009532f2d3378d5ccb1cbe93fe7
kanderson425 <kyleanderson.dev@gmail.com>
2019-10-21 23:18:45 -0500

    Adds the solution code of the ScopeChallenge including the bonus

--- ScopeChallenge_Solution/src/com/kyleanderson/Main.java
+++ ScopeChallenge_Solution/src/com/kyleanderson/Main.java
@@ -1,8 +1,11 @@
 package com.kyleanderson;
+import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+
+        X x = new X(new Scanner(System.in));
+        x.x();
     }
 }
--- ScopeChallenge_Solution/src/com/kyleanderson/X.java
+++ ScopeChallenge_Solution/src/com/kyleanderson/X.java
@@ -1,11 +1,19 @@
 package com.kyleanderson;
 
+import java.util.Scanner;
+
 public class X {
     private int x;
 
-    public X(int x) {
-        this.x = x;
+    public X(Scanner x) {
+        System.out.println("Please enter a number: ");
+        this.x = x.nextInt();
     }
 
+    public void x() {
+        for(int x = 1; x< 13; x++) {
+            System.out.println(x + " times " + this.x + " equals " + x * this.x);
 
+        }
+    }
 }
