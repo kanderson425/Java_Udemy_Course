6bf905b6555cce6a7cadd68aa75aedea021258bd
kanderson425 <kyleanderson.dev@gmail.com>
2019-10-23 18:38:53 -0500

    Adds a filler method to the static lesson

---
+++ Static/src/com/kyleanderson/Main.java
@@ -0,0 +1,16 @@
+package com.kyleanderson;
+
+public class Main {
+
+    public static void main(String[] args) {
+        doubleInt(5);
+    }
+
+    public static int doubleInt(int num) {
+        int doubleInt;
+        doubleInt = num * 2;
+        System.out.println(doubleInt);
+        return doubleInt;
+
+    }
+}
