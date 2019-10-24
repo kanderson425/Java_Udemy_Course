32c0b4f9fd017e02bebbfd648e8c1897ecbcf1de
kanderson425 <kyleanderson.dev@gmail.com>
2019-10-24 18:00:20 -0500

    Adds the multiply method and the multiplier variable to the Main class, demonstrating that non-static methods and variables cannot be referenced from a static context.

--- Static/src/com/kyleanderson/Main.java
+++ Static/src/com/kyleanderson/Main.java
@@ -1,17 +1,27 @@
 package com.kyleanderson;
 
 public class Main {
+    public static int multiplier = 7;
 
     public static void main(String[] args) {
-        StaticTest firstInstance = new StaticTest("1st Instance");
-        System.out.println(firstInstance.getName() + " is instance number " + firstInstance.getNumInstances());
+//        StaticTest firstInstance = new StaticTest("1st Instance");
+//        System.out.println(firstInstance.getName() + " is instance number " + StaticTest.getNumInstances());
+//
+//        StaticTest secondInstance = new StaticTest("2nd Instance");
+//        System.out.println(secondInstance.getName() + " is instance number " + StaticTest.getNumInstances());
+//
+//        StaticTest thirdInstance = new StaticTest("3rd Instance");
+//        System.out.println(thirdInstance.getName() + " is instance number " + StaticTest.getNumInstances());
 
-        StaticTest secondInstance = new StaticTest("2nd Instance");
-        System.out.println(secondInstance.getName() + " is instance number " + secondInstance.getNumInstances());
 
-        StaticTest thirdInstance = new StaticTest("3rd Instance");
-        System.out.println(thirdInstance.getName() + " is instance number " + thirdInstance.getNumInstances());
+        // Here is what you can do to run a project through the command line: java com.company._static.Main
+
+        int answer = multiply(6);
+        System.out.println("The answer is " + answer);
+        System.out.println("Multiplier is " + multiplier);
+        }
+
+    public static int multiply(int number) {
+        return number * multiplier;
     }
-
-
 }
--- Static/src/com/kyleanderson/StaticTest.java
+++ Static/src/com/kyleanderson/StaticTest.java
@@ -9,7 +9,7 @@ public class StaticTest {
         numInstances ++;
     }
 
-    public int getNumInstances() {
+    public static int getNumInstances() {
         return numInstances;
     }
 
