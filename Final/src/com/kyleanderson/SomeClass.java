0ab25d3638f33b87e7b7eda4e123369e859f7c73
kanderson425 <kyleanderson.dev@gmail.com>
2019-10-26 14:32:20 -0500

    Adds the SomeClass constructor and the getInstanceNumber getter

--- Final/src/com/kyleanderson/SomeClass.java
+++ Final/src/com/kyleanderson/SomeClass.java
@@ -2,6 +2,19 @@ package com.kyleanderson;
 
 public class SomeClass {
 
-    private final int instanceNumber = 1;
+    private static int classCounter = 0;
+    public final int instanceNumber;
+    private final String name;
 
+
+    public SomeClass(String name) {
+        this.name = name;
+        classCounter ++;
+        instanceNumber = classCounter;
+        System.out.println(name + " created, instance is " + instanceNumber);
+    }
+
+    public int getInstanceNumber() {
+        return instanceNumber;
+    }
 }
