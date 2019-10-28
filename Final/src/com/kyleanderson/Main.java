cee7c13cb0b0ed52ca674cdf915704120a75e13f
kanderson425 <kyleanderson.dev@gmail.com>
2019-10-27 20:17:00 -0500

    Adds the testing code in the Main class to test the SIBTest static initialization blocks

--- Final/src/com/kyleanderson/Main.java
+++ Final/src/com/kyleanderson/Main.java
@@ -3,30 +3,35 @@ package com.kyleanderson;
 public class Main {
 
     public static void main(String[] args) {
-		SomeClass one = new SomeClass("one");
-		SomeClass two = new SomeClass("two");
-		SomeClass three = new SomeClass("three");
+//		SomeClass one = new SomeClass("one");
+//		SomeClass two = new SomeClass("two");
+//		SomeClass three = new SomeClass("three");
+//
+//		System.out.println(one.getInstanceNumber());
+//		System.out.println(two.getInstanceNumber());
+//		System.out.println(three.getInstanceNumber());
+//
+////		one.instanceNumber = 4;
+//		// The reason that the above does not work is b/c the instanceNumber variable is a final variable, meaning
+//		// that this variable is getting treated as a constant and cannot be modified directy/after the variable has been set.
+//
+////		System.out.println(Math.PI);
+////		Math m = new Math();
+//
+//		int pw = 674312;
+//		Password password = new ExtendedPassword(pw);
+//		password.storePassword();
+//
+//		password.letMeIn(1);
+//		password.letMeIn(523266);
+//		password.letMeIn(9773);
+//		password.letMeIn(0);
+//		password.letMeIn(-1);
+//		password.letMeIn(674312);
 
-		System.out.println(one.getInstanceNumber());
-		System.out.println(two.getInstanceNumber());
-		System.out.println(three.getInstanceNumber());
-
-//		one.instanceNumber = 4;
-		// The reason that the above does not work is b/c the instanceNumber variable is a final variable, meaning
-		// that this variable is getting treated as a constant and cannot be modified directy/after the variable has been set.
-
-//		System.out.println(Math.PI);
-//		Math m = new Math();
-
-		int pw = 674312;
-		Password password = new ExtendedPassword(pw);
-		password.storePassword();
-
-		password.letMeIn(1);
-		password.letMeIn(523266);
-		password.letMeIn(9773);
-		password.letMeIn(0);
-		password.letMeIn(-1);
-		password.letMeIn(674312);
+		System.out.println("Main method called");
+		SIBTest test = new SIBTest();
+		test.someMethod();
+		System.out.println("Owner is " + SIBTest.owner);
     }
 }
