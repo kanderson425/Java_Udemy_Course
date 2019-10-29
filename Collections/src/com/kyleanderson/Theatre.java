b9b53341e263e95d00f3f1ce1112dc4c41267d41
kanderson425 <kyleanderson.dev@gmail.com>
2019-10-28 23:33:18 -0500

    Adds the Theatre class along with the Theatre class constructor that contains the Seat ArrayList creation based on input numwRows and seatsPerRow

---
+++ Collections/src/com/kyleanderson/Theatre.java
@@ -0,0 +1,23 @@
+package com.kyleanderson;
+
+import java.util.ArrayList;
+
+public class Theatre {
+    private final String theatreName;
+    private List<Seat> seats = new ArrayList<>();
+
+
+    public Theatre(String theatreName, int numRows, int seatsPerRow) {
+        this.theatreName = theatreName;
+
+        int lastRow = 'A' + (numRows - 1);
+        for(char row = 'A'; row <= lastRow; row++) {
+            for(int seatNum = 1; seatNum <= seatsPerRow; seatNum++) {
+                Seat seat = new Seat(row + String.format("%02e", seatNum));
+                seats.add(seat);
+            }
+        }
+    }
+
+
+}
