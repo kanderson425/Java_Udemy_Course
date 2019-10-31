e421818b1f17d3d3de54ef0c47160d56d3f53ccc
kanderson425 <kyleanderson.dev@gmail.com>
2019-10-30 22:50:37 -0500

    Adds the MapProgram class, along with the Map object called 'languages'

---
+++ Collections/src/com/kyleanderson/MapProgram.java
@@ -0,0 +1,18 @@
+package com.kyleanderson;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class MapProgram {
+
+    public static void main(String[] args) {
+        Map<String, String> languages = new HashMap<>();
+        languages.put("Java", "a compiled high level, object-oriented, platform independent language");
+        languages.put("Python", "an interpreted, object-oriented, high-level programming language with dynamic semantics");
+        languages.put("Algol", "an algorithmic language");
+        languages.put("BASIC", "Beginners All Purpose Symbolic Instruction Code");
+        languages.put("Lisp","Therein lies madness");
+
+        System.out.println(languages.get("Java"));
+    }
+}
