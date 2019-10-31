038a60e74f24b537d1dc1a103cf9382c446bbeb1
kanderson425 <kyleanderson.dev@gmail.com>
2019-10-30 23:01:15 -0500

    Adds the put(), containsKey() and the keySet() method to the MapProgram class

--- Collections/src/com/kyleanderson/MapProgram.java
+++ Collections/src/com/kyleanderson/MapProgram.java
@@ -7,12 +7,26 @@ public class MapProgram {
 
     public static void main(String[] args) {
         Map<String, String> languages = new HashMap<>();
-        languages.put("Java", "a compiled high level, object-oriented, platform independent language");
+        if(languages.containsKey("Java")) {
+            System.out.println("Java already exists");
+        } else {
+            languages.put("Java", "a compiled high level, object-oriented, platform independent language");
+            System.out.println("Java added successfully");
+        }
         languages.put("Python", "an interpreted, object-oriented, high-level programming language with dynamic semantics");
         languages.put("Algol", "an algorithmic language");
-        languages.put("BASIC", "Beginners All Purpose Symbolic Instruction Code");
-        languages.put("Lisp","Therein lies madness");
+        System.out.println(languages.put("BASIC", "Beginners All Purpose Symbolic Instruction Code"));
+        System.out.println(languages.put("Lisp","Therein lies madness"));
 
-        System.out.println(languages.get("Java"));
+        if(languages.containsKey("Java")) {
+            System.out.println("Java is already in the map");
+        } else {
+            languages.put("Java","this course is about java");
+        }
+
+        System.out.println("===============================");
+        for(String key: languages.keySet()) {
+            System.out.println(key + " : " + languages.get(key));
+        }
     }
 }
