fd9927dcffc51f6c4e28c72cc40058a02dd10141
kanderson425 <kyleanderson.dev@gmail.com>
2019-11-03 14:04:52 -0600

    Adds the locationID, description and exits fields to the Location class within the Adventure project. Includes the constructor and the respective getters

---
+++ Adventure/src/com/kyleanderson/Location.java
@@ -0,0 +1,30 @@
+package com.kyleanderson;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Location {
+    private final int locationID;
+    private final String description;
+    private final Map<String, Integer> exits;
+
+    public Location(int locationID, String description) {
+        this.locationID = locationID;
+        this.description = description;
+        this.exits = new HashMap<String, Integer>();
+    }
+
+    pu
+
+    public int getLocationID() {
+        return locationID;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public Map<String, Integer> getExits() {
+        return exits;
+    }
+}
--- Collections/src/com/kyleanderson/MapProgram.java
+++ Collections/src/com/kyleanderson/MapProgram.java
@@ -33,7 +33,11 @@ public class MapProgram {
             System.out.println("Algol not removed, key/value pair not found");
         }
 
-        System.out.println(languages.replace("Lisp", "a functional programming language with imperative features"));
+        if(languages.replace("Lisp", "Therein lies madness", "a functional programming language with imperative features")) {
+            System.out.println("Lisp replaced");
+        } else {
+            System.out.println("List was not replaced");
+        }
         System.out.println(languages.replace("Scala", "this will not be added"));
         for(String key: languages.keySet()) {
             System.out.println(key + " : " + languages.get(key));
