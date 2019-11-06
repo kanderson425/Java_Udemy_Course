add8552ffc29122f15e678e1ef9f0854c04a7d5f
kanderson425 <kyleanderson.dev@gmail.com>
2019-11-05 18:25:15 -0600

    Adds the addMoon method and the satelites getter to the HeavenlyBody class

--- SetAndHashSet/src/com/kyleanderson/HeavenlyBody.java
+++ SetAndHashSet/src/com/kyleanderson/HeavenlyBody.java
@@ -1,5 +1,6 @@
 package com.kyleanderson;
 
+import java.util.HashSet;
 import java.util.Set;
 
 public final class HeavenlyBody {
@@ -10,5 +11,22 @@ public final class HeavenlyBody {
     public HeavenlyBody(String name, double orbitalPeriod) {
         this.name = name;
         this.orbitalPeriod = orbitalPeriod;
+        this.satelites = new HashSet<>();
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public double getOrbitalPeriod() {
+        return orbitalPeriod;
+    }
+
+    public boolean addMoon(HeavenlyBody moon) {
+        return this.satelites.add(moon);
+    }
+
+    public Set<HeavenlyBody> getSatelites() {
+        return satelites;
     }
 }
