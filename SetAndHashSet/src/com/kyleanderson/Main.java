58e8248bbe88dfade29c4a4f2d5f5b4517fa8469
kanderson425 <kyleanderson.dev@gmail.com>
2019-11-06 22:16:54 -0600

    Adds all of the planets of the solar system to the Planets set along with certain moons of each planet

--- SetAndHashSet/src/com/kyleanderson/Main.java
+++ SetAndHashSet/src/com/kyleanderson/Main.java
@@ -21,5 +21,63 @@ public class Main {
         temp = new HeavenlyBody("Earth", 365);
         solarSystem.put(temp.getName(), temp);
         planets.add(temp);
+
+        HeavenlyBody tempMoon = new HeavenlyBody("Moon", 27);
+        solarSystem.put(tempMoon.getName(), tempMoon);
+        temp.addMoon(tempMoon);
+
+        temp = new HeavenlyBody("Mars", 687);
+        solarSystem.put(temp.getName(), temp);
+        planets.add(temp);
+
+        tempMoon = new HeavenlyBody("Deimos", 1.3);
+        solarSystem.put(tempMoon.getName(), tempMoon);
+        temp.addMoon(tempMoon); //temp is still Mars
+
+        tempMoon = new HeavenlyBody("Phobos", 0.3);
+        solarSystem.put(tempMoon.getName(), tempMoon);
+        temp.addMoon(tempMoon); // temp is still Mars
+
+        temp = new HeavenlyBody("Jupiter", 4332);
+        solarSystem.put(temp.getName(), temp);
+        planets.add(temp);
+
+        tempMoon = new HeavenlyBody("Io", 1.8);
+        solarSystem.put(tempMoon.getName(), tempMoon);
+        temp.addMoon(tempMoon); // temp is still Jupiter
+
+        tempMoon = new HeavenlyBody("Europa", 3.5);
+        solarSystem.put(tempMoon.getName(), tempMoon);
+        temp.addMoon(tempMoon); // temp is still Jupiter
+
+        tempMoon = new HeavenlyBody("Ganymede", 7.1);
+        solarSystem.put(tempMoon.getName(), tempMoon);
+        temp.addMoon(tempMoon); // temp is still Jupiter
+
+        tempMoon = new HeavenlyBody("Callisto", 16.7);
+        solarSystem.put(tempMoon.getName(), tempMoon);
+        temp.addMoon(tempMoon); // temp is still Jupiter
+
+        temp = new HeavenlyBody("Saturn", 10759);
+        solarSystem.put(tempMoon.getName(), tempMoon);
+        planets.add(temp);
+
+        temp = new HeavenlyBody("Uranus", 30660);
+        solarSystem.put(temp.getName(), temp);
+        planets.add(temp);
+
+        temp = new HeavenlyBody("Neptune", 165);
+        solarSystem.put(temp.getName(), temp);
+        planets.add(temp);
+
+        temp = new HeavenlyBody("Pluto", 248);
+        solarSystem.put(temp.getName(), temp);
+        planets.add(temp);
+
+
+        System.out.println("Planets");
+        for(HeavenlyBody planet: planets) {
+            System.out.println("\t" + planet.getName());
+        }
     }
 }
