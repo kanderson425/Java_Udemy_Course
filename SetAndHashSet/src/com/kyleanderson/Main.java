8d8775e075e89550abaf7575ebeadbd501210652
kanderson425 <kyleanderson.dev@gmail.com>
2019-11-07 22:27:23 -0600

    Adds code to reflect the issues with adding a duplicate key within the HeavenlyBody hashset

--- SetAndHashSet/src/com/kyleanderson/Main.java
+++ SetAndHashSet/src/com/kyleanderson/Main.java
@@ -94,6 +94,14 @@ public class Main {
         for(HeavenlyBody moon: moons) {
             System.out.println("\t" + moon.getName());
         }
+
+        HeavenlyBody pluto = new HeavenlyBody("Pluto", 842);
+        planets.add(pluto);
+
+        for(HeavenlyBody planet: planets) {
+            System.out.println(planet.getName() + ": " + planet.getOrbitalPeriod());
+        }
+
     }
 
 
