9549737b645802ca6f4095b010fbd43f7be44ce9
kanderson425 <kyleanderson.dev@gmail.com>
2019-11-08 19:10:39 -0600

    Removes the override in the Labrador subclass

--- SetAndHashSet/src/com/kyleanderson/Labrador.java
+++ SetAndHashSet/src/com/kyleanderson/Labrador.java
@@ -7,17 +7,17 @@ public class Labrador extends Dog {
         super(name);
     }
 
-    @Override
-    public boolean equals(Object obj) {
-        if(this == obj) {
-            return true;
-        }
-
-        if(obj instanceof Labrador) {
-            String objName = ((Labrador) obj).getName();
-            return this.getName().equals(objName);
-        }
-
-        return false;
-    }
+//    @Override
+//    public boolean equals(Object obj) {
+//        if(this == obj) {
+//            return true;
+//        }
+//
+//        if(obj instanceof Labrador) {
+//            String objName = ((Labrador) obj).getName();
+//            return this.getName().equals(objName);
+//        }
+//
+//        return false;
+//    }
 }
