0d5df1f2449cc89d46d936bdc69a10eb9c737c1e
kanderson425 <kyleanderson.dev@gmail.com>
2019-11-11 21:19:00 -0600

    Adds the Key inner class within thte HeavenlyBody class in addition to adding the equals and hashcode overrides within the Key inner class

--- SetAndHashSet/src/com/kyleanderson/HeavenlyBody.java
+++ SetAndHashSet/src/com/kyleanderson/HeavenlyBody.java
@@ -71,4 +71,36 @@ public abstract class HeavenlyBody {
     public String toString() {
         return this.name + ": " + this.bodyType + ", " + this.orbitalPeriod;
     }
+
+    public static final class Key {
+        private String name;
+        private BodyTypes bodyType;
+
+        private Key(String name, BodyTypes bodyType) {
+            this.name = name;
+            this.bodyType = bodyType;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public BodyTypes getBodyType() {
+            return bodyType;
+        }
+
+        @Override
+        public int hashCode() {
+            return this.name.hashCode() + 57 + this.bodyType.hashCode();
+        }
+
+        @Override
+        public boolean equals(Object obj) {
+            Key key = (Key) obj;
+            if(this.name.equals(key.getName())) {
+                return (this.bodyType == key.getBodyType());
+            }
+            return false;
+        }
+    }
 }
