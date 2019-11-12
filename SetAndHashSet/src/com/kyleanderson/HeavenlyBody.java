7649178815d421bacb78f72fc28a8441cdd5de9f
kanderson425 <kyleanderson.dev@gmail.com>
2019-11-11 21:24:26 -0600

    Adds the MakeKey method within the HeavenlyBody class'

--- SetAndHashSet/src/com/kyleanderson/HeavenlyBody.java
+++ SetAndHashSet/src/com/kyleanderson/HeavenlyBody.java
@@ -4,10 +4,10 @@ import java.util.HashSet;
 import java.util.Set;
 
 public abstract class HeavenlyBody {
-    private final String name;
+    private final Key key;
     private final double orbitalPeriod;
     private final Set<HeavenlyBody> satelites;
-    private final BodyTypes bodyType;
+
 
     public enum BodyTypes {
         STAR,
@@ -20,22 +20,14 @@ public abstract class HeavenlyBody {
 
 
     public HeavenlyBody(String name, double orbitalPeriod, BodyTypes bodyType) {
-        this.name = name;
+        this.key = new Key(name, bodyType);
         this.orbitalPeriod = orbitalPeriod;
         this.satelites = new HashSet<>();
-        this.bodyType = bodyType;
-    }
 
-    public String getName() {
-        return name;
     }
 
     public double getOrbitalPeriod() {
         return orbitalPeriod;
-    }
-
-    public BodyTypes getBodyType() {
-        return bodyType;
     }
 
     public boolean addSatelite(HeavenlyBody moon) {
@@ -46,6 +38,9 @@ public abstract class HeavenlyBody {
         return new HashSet<>(this.satelites);
     }
 
+    public Key getKey() {
+        return key;
+    }
 
     @Override
     public final boolean equals(Object obj) {
@@ -55,21 +50,23 @@ public abstract class HeavenlyBody {
 
         if(obj instanceof HeavenlyBody) {
             HeavenlyBody theObject = (HeavenlyBody) obj;
-            if(this.name.equals(theObject.getName())) {
-                return this.bodyType == theObject.getBodyType();
-            }
+            return this.key.equals(theObject.getKey());
         }
         return false;
     }
 
     @Override
     public final int hashCode() {
-        return this.name.hashCode() + 57 + this.bodyType.hashCode();
+        return this.key.hashCode();
+    }
+
+    public static Key makeKey(String name, BodyTypes bodyType) {
+        return new Key(name, bodyType);
     }
 
     @Override
     public String toString() {
-        return this.name + ": " + this.bodyType + ", " + this.orbitalPeriod;
+        return this.key.name + ": " + this.key.bodyType + ", " + this.orbitalPeriod;
     }
 
     public static final class Key {
