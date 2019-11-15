a1706aba727f6b91a6e989d796b9d86d7258f32d
kanderson425 <kyleanderson.dev@gmail.com>
2019-11-14 23:19:15 -0600

    Adds the toString override method to the Basket class

--- sortedCollections/src/com/kyleanderson/Basket.java
+++ sortedCollections/src/com/kyleanderson/Basket.java
@@ -1,5 +1,6 @@
 package com.kyleanderson;
 
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -21,5 +22,19 @@ public class Basket {
         return 0;
     }
 
+    public Map<StockItem, Integer> Items() {
+        return Collections.unmodifiableMap(list);
+    }
 
+
+    @Override
+    public String toString() {
+        String s = "\nShopping basket " + name + " contains " + list.size() + " items\n";
+        double totalCost = 0.0;
+        for(Map.Entry<StockItem, Integer> item: list.entrySet()) {
+            s = s + item.getKey() + ", " + item.getValue() + " purchased\n";
+            totalCost = item.getKey().getPrice() * item.getValue();
+        }
+        return s + "Total Cost " + totalCost;
+    }
 }
