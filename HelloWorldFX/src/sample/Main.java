33bea75fa9e21f31c46affe7d04caa00b6e76981
kanderson425 <kyleanderson.dev@gmail.com>
2019-11-23 15:11:41 -0600

    Adds the greeting label variable and utilizes thee setTextFill method to adjust the label color to green

--- HelloWorldFX/src/sample/Main.java
+++ HelloWorldFX/src/sample/Main.java
@@ -2,16 +2,31 @@ package sample;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.geometry.Pos;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.layout.GridPane;
+import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 
 public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
+//        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+
+        GridPane root = new GridPane();
+        root.setAlignment(Pos.CENTER);
+        root.setVgap(10);
+        root.setHgap(10);
+
+        Label greeting = new Label("Welcome to JavaFX!");
+        greeting.setTextFill(Color.GREEN);
+        root.getChildren().add(greeting);
+
+
+        primaryStage.setTitle("Hello JavaFX");
         primaryStage.setScene(new Scene(root, 300, 275));
         primaryStage.show();
     }
