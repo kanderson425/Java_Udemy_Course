00a0446b2c02297fccace12bf44fd5af17f6c9b5
kanderson425 <kyleanderson.dev@gmail.com>
2019-11-23 15:26:10 -0600

    Utilizes the sample.fxml file to adjust the label text and font attributes

--- HelloWorldFX/src/sample/Main.java
+++ HelloWorldFX/src/sample/Main.java
@@ -16,21 +16,21 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-//        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
 
-        GridPane root = new GridPane();
-        root.setAlignment(Pos.CENTER);
-        root.setVgap(10);
-        root.setHgap(10);
-
-        Label greeting = new Label("Welcome to JavaFX!");
-        greeting.setTextFill(Color.GREEN);
-        greeting.setFont(Font.font("Times New Roman", FontWeight.BOLD, 70));
-        root.getChildren().add(greeting);
+//        GridPane root = new GridPane();
+//        root.setAlignment(Pos.CENTER);
+//        root.setVgap(10);
+//        root.setHgap(10);
+//
+//        Label greeting = new Label("Welcome to JavaFX!");
+//        greeting.setTextFill(Color.GREEN);
+//        greeting.setFont(Font.font("Times New Roman", FontWeight.BOLD, 70));
+//        root.getChildren().add(greeting);
 
 
         primaryStage.setTitle("Hello JavaFX");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.setScene(new Scene(root, 700, 275));
         primaryStage.show();
     }
 
