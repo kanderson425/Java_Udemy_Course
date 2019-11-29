044a59fb79055ce5c2c0473987c5e30471089204
kanderson425 <kyleanderson.dev@gmail.com>
2019-11-29 07:02:25 -0700

    Adds the ByeButtton and repurposes the onButtonClicked handler to handle both the helloButton controla and the byeButton control, reducing code duplication

--- Events/src/sample/Controller.java
+++ Events/src/sample/Controller.java
@@ -1,7 +1,24 @@
 package sample;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
 public class Controller {
-    public void onButtonClicked() {
-        System.out.println("Hello");
+    @FXML
+    private TextField nameField;
+    @FXML
+    private Button helloButton;
+    @FXML
+    private Button byeButton;
+
+    @FXML
+    public void onButtonClicked(ActionEvent e) {
+        if(e.getSource().equals(helloButton)) {
+            System.out.println("Hello, " + nameField.getText());
+        } else if(e.getSource().equals(byeButton)) {
+            System.out.println("Bye, " + nameField.getText());
+        }
     }
 }
