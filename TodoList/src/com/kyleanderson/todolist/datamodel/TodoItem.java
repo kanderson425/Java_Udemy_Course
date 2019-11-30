c3f169b3f3040cc86caf4edddacac6dae3dcd347
kanderson425 <kyleanderson.dev@gmail.com>
2019-11-30 10:13:46 -0700

    Adds the shortDescription, details and deadline fields to the TodoItem class. Adds the TodoItem constructor along with respective getters and setters

--- TodoList/src/com/kyleanderson/todolist/datamodel/TodoItem.java
+++ TodoList/src/com/kyleanderson/todolist/datamodel/TodoItem.java
@@ -1,4 +1,39 @@
 package com.kyleanderson.todolist.datamodel;
 
+import java.time.LocalDate;
+
 public class TodoItem {
+    private String shortDescription;
+    private String details;
+    private LocalDate deadline;
+
+    public TodoItem(String shortDescription, String details, LocalDate deadline) {
+        this.shortDescription = shortDescription;
+        this.details = details;
+        this.deadline = deadline;
+    }
+
+    public String getShortDescription() {
+        return shortDescription;
+    }
+
+    public String getDetails() {
+        return details;
+    }
+
+    public LocalDate getDeadline() {
+        return deadline;
+    }
+
+    public void setShortDescription(String shortDescription) {
+        this.shortDescription = shortDescription;
+    }
+
+    public void setDetails(String details) {
+        this.details = details;
+    }
+
+    public void setDeadline(LocalDate deadline) {
+        this.deadline = deadline;
+    }
 }
