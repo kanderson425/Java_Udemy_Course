e2828aa451e833033a74f5dc3153dc27f3b398a4
kanderson425 <kyleanderson.dev@gmail.com>
2019-11-30 20:20:32 -0600

    Adds the toString override to the TodoItem class in order to populate the shortDescription within the left BorderPane

--- TodoList/src/com/kyleanderson/todolist/datamodel/TodoItem.java
+++ TodoList/src/com/kyleanderson/todolist/datamodel/TodoItem.java
@@ -36,4 +36,9 @@ public class TodoItem {
     public void setDeadline(LocalDate deadline) {
         this.deadline = deadline;
     }
+
+    @Override
+    public String toString() {
+        return shortDescription;
+    }
 }
