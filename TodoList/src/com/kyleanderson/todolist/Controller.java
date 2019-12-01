0b48866aa822b4065b3ce02cb6fc0924bbc7ecc6
kanderson425 <kyleanderson.dev@gmail.com>
2019-11-30 20:04:54 -0600

    Adds the initialize method within the Controller class along with adds mock todo list items

--- TodoList/src/com/kyleanderson/todolist/Controller.java
+++ TodoList/src/com/kyleanderson/todolist/Controller.java
@@ -1,4 +1,32 @@
 package com.kyleanderson.todolist;
+import com.kyleanderson.todolist.datamodel.TodoItem;
+
+import java.time.LocalDate;
+import java.time.Month;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Controller {
+    private List<TodoItem> todoItems;
+
+    public void initialize() {
+        TodoItem item1 = new TodoItem("Mail birthday card", "Buy a 38th birthday card for John",
+                LocalDate.of(2016, Month.APRIL, 25));
+        TodoItem item2 = new TodoItem("Doctor's Appointment", "See Dr. Smith at 123 Main Street. Bring paperwork",
+                LocalDate.of(2016, Month.MAY, 23));
+        TodoItem item3 = new TodoItem("Finish design proposal for client", "I promised Mike I'd email website mockups by Friday 22nd April",
+                LocalDate.of(2016, Month.APRIL, 22));
+        TodoItem item4 = new TodoItem("Pickup Doug at the train station", "Doug is arrive on March 23 on the 5:00 train",
+                LocalDate.of(2016, Month.MARCH, 23));
+        TodoItem item5 = new TodoItem("Pickup dry cleaning", "The clothes should be ready by wednesday",
+                LocalDate.of(2016, Month.APRIL, 20));
+
+        todoItems = new ArrayList<TodoItem>();
+        todoItems.add(item1);
+        todoItems.add(item2);
+        todoItems.add(item3);
+        todoItems.add(item4);
+        todoItems.add(item5);
+
+    }
 }
