fded063709dcd6275a720154244ebf15a936c3ff
kanderson425 <kyleanderson.dev@gmail.com>
2019-12-02 13:05:00 -0600

    Adds the processResults method to the DialogController

--- TodoList/src/com/kyleanderson/todolist/DialogController.java
+++ TodoList/src/com/kyleanderson/todolist/DialogController.java
@@ -1,9 +1,13 @@
 package com.kyleanderson.todolist;
 
+import com.kyleanderson.todolist.datamodel.TodoData;
+import com.kyleanderson.todolist.datamodel.TodoItem;
 import javafx.fxml.FXML;
 import javafx.scene.control.DatePicker;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
+
+import java.time.LocalDate;
 
 public class DialogController {
 
@@ -15,4 +19,12 @@ public class DialogController {
 
     @FXML
     private DatePicker deadlinePicker;
+
+    public void processResults() {
+        String shortDescription = shortDescriptionField.getText().trim();
+        String details = detailsArea.getText().trim();
+        LocalDate deadlineValue = deadlinePicker.getValue();
+
+        TodoData.getInstance().addTodoItem(new TodoItem(shortDescription, details, deadlineValue));
+    }
 }
