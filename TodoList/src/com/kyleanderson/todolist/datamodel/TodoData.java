b28c2aca6666db511e42acc34d4e72fc77dd9168
kanderson425 <kyleanderson.dev@gmail.com>
2019-12-01 20:36:39 -0600

    Adds the loadTodoItems method within the TodoData class

--- TodoList/src/com/kyleanderson/todolist/datamodel/TodoData.java
+++ TodoList/src/com/kyleanderson/todolist/datamodel/TodoData.java
@@ -1,5 +1,14 @@
 package com.kyleanderson.todolist.datamodel;
 
+import javafx.collections.FXCollections;
+
+import java.io.BufferedReader;
+import java.io.IOError;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 import java.util.List;
 
@@ -17,4 +26,42 @@ public class TodoData {
     private TodoData() {
         formatter = DateTimeFormatter.ofPattern("dd-MM-yyyy");
     }
+
+    public List<TodoItem> getTodoItems() {
+        return todoItems;
+    }
+
+    public void setTodoItems(List<TodoItem> todoItems) {
+        this.todoItems = todoItems;
+    }
+
+    public void loadTodoItems() throws IOException {
+        todoItems = FXCollections.observableArrayList();
+        Path path = Paths.get(filename);
+        BufferedReader br = Files.newBufferedReader(path);
+
+        String input;
+
+        try {
+            while ((input = br.readLine()) != null) {
+                String[] itemPieces = input.split("\t");
+
+                String shortDescription = itemPieces[0];
+                String details = itemPieces[1];
+                String dateString = itemPieces[2];
+
+                LocalDate date = LocalDate.parse(dateString, formatter);
+                TodoItem todoItem = new TodoItem(shortDescription, details, date);
+                todoItems.add(todoItem);
+            }
+
+        } finally {
+            if(br != null) {
+                br.close();
+            }
+        }
+    }
+
+
+
 }
