43bf3e56a03b6b43b96d7c0e246f48ac9b9e7fc4
kanderson425 <kyleanderson.dev@gmail.com>
2019-12-08 16:37:25 -0600

    Updates the default theme of Medina and replaces this theme with CASPIAN

--- JavaFX Application/src/sample/Main.java
+++ JavaFX Application/src/sample/Main.java
@@ -11,6 +11,7 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+        setUserAgentStylesheet(STYLESHEET_CASPIAN);
         primaryStage.setTitle("Hello World");
         primaryStage.setScene(new Scene(root, 600, 275));
         primaryStage.show();
