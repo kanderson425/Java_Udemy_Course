f3da4938e8ab453024084334cd9dda3897e53805
kanderson425 <kyleanderson.dev@gmail.com>
2019-12-09 15:48:04 -0600

    Adds the handleClick method by instantiating a new FileChooser class. Adds the event to buttonOne

--- JavaFX Application/src/sample/Controller.java
+++ JavaFX Application/src/sample/Controller.java
@@ -1,13 +1,24 @@
 package sample;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.effect.DropShadow;
+import javafx.scene.layout.GridPane;
+import javafx.stage.FileChooser;
 
 public class Controller {
     @FXML
     private Label label;
 
+    @FXML
+    private Button button4;
+
+    @FXML
+    private GridPane gridPane;
+
     public void initialize() {
+        button4.setEffect(new DropShadow());
 
     }
 
@@ -21,5 +32,11 @@ public class Controller {
     public void handleMouseExit() {
         label.setScaleX(1.0);
         label.setScaleY(1.0);
+    }
+
+    @FXML
+    public void handleClick() {
+        FileChooser chooser = new FileChooser();
+        chooser.showOpenDialog(null);
     }
 }
