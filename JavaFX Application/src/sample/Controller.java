0c99b3acf5c1bab613f9e8b4516e26e72f43c7f9
kanderson425 <kyleanderson.dev@gmail.com>
2019-12-09 15:17:11 -0600

    Adds handleMouseEnter and handleMouseExit methods which scale the 'JavaFX Effects' label from 1.0 to 2.0 when mouse enters/exits the label.

--- JavaFX Application/src/sample/Controller.java
+++ JavaFX Application/src/sample/Controller.java
@@ -1,4 +1,25 @@
 package sample;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
 public class Controller {
+    @FXML
+    private Label label;
+
+    public void initialize() {
+
+    }
+
+    @FXML
+    public void handlMouseEnter() {
+        label.setScaleX(2.0);
+        label.setScaleY(2.0);
+    }
+
+    @FXML
+    public void handleMouseExit() {
+        label.setScaleX(1.0);
+        label.setScaleY(1.0);
+    }
 }
