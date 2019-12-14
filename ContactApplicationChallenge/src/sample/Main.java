794350638de17bbe4c31cc52bdce9147105b2c37
kanderson425 <kyleanderson.dev@gmail.com>
2019-12-14 16:08:13 -0600

    Adds the Contacts Menu along with the menu items.

--- ContactApplicationChallenge/src/sample/Main.java
+++ ContactApplicationChallenge/src/sample/Main.java
@@ -12,7 +12,7 @@ public class Main extends Application {
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
         primaryStage.setTitle("My Contacts");
-        primaryStage.setScene(new Scene(root, 400, 275));
+        primaryStage.setScene(new Scene(root, 750, 275));
         primaryStage.show();
     }
 
