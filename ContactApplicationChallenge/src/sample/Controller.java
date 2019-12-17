e18598268faea7b7281ccbdb7915d2ff64c2aba8
kanderson425 <kyleanderson.dev@gmail.com>
2019-12-16 22:50:43 -0600

    Adds the showAddContactDialog method within the Controller

--- ContactApplicationChallenge/src/sample/Controller.java
+++ ContactApplicationChallenge/src/sample/Controller.java
@@ -1,10 +1,43 @@
 package sample;
 
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.control.Button;
+import javafx.scene.control.ButtonType;
+import javafx.scene.control.Dialog;
+import javafx.scene.layout.BorderPane;
+
+import java.io.IOException;
+import java.util.Optional;
 
 public class Controller{
+
+    @FXML
+    private BorderPane mainPanel;
+
     @FXML
     public void showAddContactDialog() {
+        Dialog<ButtonType> dialog = new Dialog<>();
+        dialog.initOwner(mainPanel.getScene().getWindow());
+        dialog.setTitle("Add New Contact");
+        FXMLLoader fxmlLoader = new FXMLLoader();
+        fxmlLoader.setLocation(getClass().getResource("contactDialog.fxml"));
+        try{
+            dialog.getDialogPane().setContent(fxmlLoader.load());
+
+        } catch(IOException e) {
+            System.out.println("Couldn't load the dialog");
+            e.printStackTrace();
+            return;
+        }
+
+        dialog.getDialogPane().getButtonTypes().add(ButtonType.OK);
+        dialog.getDialogPane().getButtonTypes().add(ButtonType.CANCEL);
+
+        Optional<ButtonType> result = dialog.showAndWait();
+        if(result.isPresent() && result.get() == ButtonType.OK) {
+
+        }
 
     }
 
