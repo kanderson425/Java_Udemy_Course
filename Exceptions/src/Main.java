a598644de8d5606b4b7818cf331507c3e2f14179
kanderson425 <kyleanderson.dev@gmail.com>
2019-12-18 22:21:04 -0600

    Adds anoother test case of examples between the LBYL and EAFP method.:

--- Exceptions/src/Main.java
+++ Exceptions/src/Main.java
@@ -1,15 +1,53 @@
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-       int x = 98;
-       int y = 0;
-        System.out.println(divideLBYL(x, y));
-        System.out.println(divideEAFP(x,y));
+//       int x = 98;
+//       int y = 0;
+//        System.out.println(divideLBYL(x, y));
+//        System.out.println(divideEAFP(x,y));
+//        System.out.println(divide(x,y));
+        int x = getIntLBYL();
+        System.out.println("x is " + x);
 
     }
 
     //LBYL - "Look Before You Leap"
     //EAFP - "Easier to ask for permission"
+
+    private static int getInt() {
+        Scanner s = new Scanner(System.in);
+        return s.nextInt();
+    }
+
+    private static int getIntLBYL() {
+        Scanner s = new Scanner(System.in);
+        boolean isValid = true;
+        System.out.println("Please enter an integer ");
+        String input = s.next();
+        for(int i= 0; i< input.length(); i++) {
+            if(!Character.isDigit(input.charAt(i))) {
+                isValid = false;
+                break;
+            }
+        }
+        if(isValid) {
+            return Integer.parseInt(input);
+        }
+        return 0;
+    }
+
+    private static int getIntEAFP() {
+        Scanner s = new Scanner(System.in);
+        System.out.println("Please enter an integer ");
+        try {
+            return s.nextInt();
+        } catch(InputMismatchException e) {
+            return 0;
+        }
+    }
 
     private static int divideLBYL(int x, int y) {
         if(y != 0) {
@@ -25,5 +63,10 @@ public class Main {
         } catch(ArithmeticException e) {
             return 0;
         }
+    }
+
+    //This method crashes automatically b/c it doesn't test for anything
+    private static int divide(int x, int y) {
+        return x / y;
     }
 }
