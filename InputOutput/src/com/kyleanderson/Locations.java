3b70474b93a3e72edc1d3d34202f63efd1b72964
kanderson425 <kyleanderson.dev@gmail.com>
2019-12-23 17:03:45 -0600

    Updates the Main method of the Locations class to exclude the 'finally' statement in order to properly catch the initial exception error on immediate run of the program.

--- InputOutput/src/com/kyleanderson/Locations.java
+++ InputOutput/src/com/kyleanderson/Locations.java
@@ -15,27 +15,25 @@ import java.util.Set;
 public class Locations implements Map<Integer, Location> {
     private static Map<Integer, Location> locations = new HashMap<Integer, Location>();
 
-    public static void main(String[] args) {
-        FileWriter locFile = null;
-        try {
-            locFile = new FileWriter("locations.txt");
-            for(Location location: locations.values()) {
+    public static void main(String[] args) throws IOException{
+        try(FileWriter locFile = new FileWriter("locations.txt")) {
+            for(Location location : locations.values()) {
                 locFile.write(location.getLocationID() + "," + location.getDescription() + "\n");
             }
-        } catch(IOException e) {
-            System.out.println("In catch block");
-            e.printStackTrace();
-        } finally {
-            System.out.println("in finally block");
-            try {
-                if(locFile != null) {
-                    System.out.println("Attempting to close the locfile");
-                    locFile.close();
-                }
-            } catch(IOException e) {
-                e.printStackTrace();
-            }
         }
+//        FileWriter locFile = null;
+//        try {
+//            locFile = new FileWriter("locations.txt");
+//            for(Location location: locations.values()) {
+//                locFile.write(location.getLocationID() + "," + location.getDescription() + "\n");
+//            }
+//        } finally {
+//            System.out.println("in finally block");
+//                if(locFile != null) {
+//                    System.out.println("Attempting to close the locfile");
+//                    locFile.close();
+//                }
+//        }
 
     }
 
