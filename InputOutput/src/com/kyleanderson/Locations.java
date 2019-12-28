6ff3172c495ead82450bb08bbd95cbd794794d7f
kanderson425 <kyleanderson.dev@gmail.com>
2019-12-27 22:23:22 -0600

    Reorganizes the static initialization block within the Locations class to account for an EOFException.

--- InputOutput/src/com/kyleanderson/Locations.java
+++ InputOutput/src/com/kyleanderson/Locations.java
@@ -32,24 +32,30 @@ public class Locations implements Map<Integer, Location> {
     static {
 
         try(DataInputStream locFile = new DataInputStream(new BufferedInputStream(new FileInputStream("locations.dat")))) {
-            while(true) {
-                Map<String, Integer> exits = new LinkedHashMap<>();
-                int locID = locFile.readInt();
-                String description = locFile.readUTF();
-                int numExits = locFile.readInt();
-                System.out.println("Read location " + locID + " : " + description);
-                System.out.println("Found " + numExits + " exits");
-                for(int i=0; i<numExits; i++) {
-                    String direction = locFile.readUTF();
-                    int destination = locFile.readInt();
-                    exits.put(direction, destination);
-                    System.out.println("\t\t" + direction + "," + destination);
+            boolean eof = false;
+            while(!eof) {
+                try {
+                    Map<String, Integer> exits = new LinkedHashMap<>();
+                    int locID = locFile.readInt();
+                    String description = locFile.readUTF();
+                    int numExits = locFile.readInt();
+                    System.out.println("Read location " + locID + " : " + description);
+                    System.out.println("Found " + numExits + " exits");
+                    for(int i=0; i<numExits; i++) {
+                        String direction = locFile.readUTF();
+                        int destination = locFile.readInt();
+                        exits.put(direction, destination);
+                        System.out.println("\t\t" + direction + "," + destination);
+                    }
+                    locations.put(locID, new Location(locID, description, exits));
+
+                } catch(EOFException e) {
+                    eof = true;
                 }
-                locations.put(locID, new Location(locID, description, exits));
 
             }
         } catch(IOException io) {
-            System.out.println("Io exception");
+            System.out.println("IO Exception");
         }
 
 //        try(Scanner scanner = new Scanner(new BufferedReader(new FileReader("locations_big.txt")))) {
