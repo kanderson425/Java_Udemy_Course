fb67f6d59520e0d06df8132e2dcedca32f4a8b13
kanderson425 <kyleanderson.dev@gmail.com>
2019-12-30 09:36:39 -0600

    Resets the starting currentLocation to location 1.

--- AdventureRandom/src/com/kyleanderson/Main.java
+++ AdventureRandom/src/com/kyleanderson/Main.java
@@ -27,7 +27,7 @@ public class Main {
         vocabulary.put("EAST", "E");
 
 
-        Location currentLocation = locations.getLocation(64);
+        Location currentLocation = locations.getLocation(1);
         while(true) {
             System.out.println(currentLocation.getDescription());
 
@@ -60,6 +60,8 @@ public class Main {
                 System.out.println("You cannot go in that direction");
             }
         }
+
+        locations.close();
 
     }
 }
