a9aa795723a378b1218f7bce1942fc1ce55ddd6f
kanderson425 <kyleanderson.dev@gmail.com>
2020-01-02 17:35:30 -0600

    Creates an instance of a ByteBuffer to wrap the 'Hello World!' message.

--- JavaNIO/src/com/kyleanderson/Main.java
+++ JavaNIO/src/com/kyleanderson/Main.java
@@ -1,24 +1,36 @@
 package com.kyleanderson;
 
 import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.nio.ByteBuffer;
 import java.nio.channels.FileChannel;
 import java.nio.file.FileSystems;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.nio.file.StandardOpenOption;
 import java.util.List;
 
 public class Main {
 
     public static void main(String[] args) {
-        try {
+
+        try(FileOutputStream binFile = new FileOutputStream("data.dat");
+            FileChannel binChannel = binFile.getChannel()) {
+
+            byte[] outputBytes = "Hello World!".getBytes();
+            ByteBuffer buffer = ByteBuffer.wrap(outputBytes);
+            int numBytes = binChannel.write(buffer);
+            System.out.println("numBytes written was: " + numBytes);
+
 //            FileInputStream file = new FileInputStream("data.txt");
 //            FileChannel channel = file.getChannel();
-            Path dataPath = FileSystems.getDefault().getPath("data.txt");
-            List<String> lines = Files.readAllLines(dataPath);
-            for(String line : lines) {
-                System.out.println(line);
-            }
+//            Path dataPath = FileSystems.getDefault().getPath("data.txt");
+//            Files.write(dataPath, "\nLine 5".getBytes("UTF-8"), StandardOpenOption.APPEND);
+//            List<String> lines = Files.readAllLines(dataPath);
+//            for(String line : lines) {
+//                System.out.println(line);
+//            }
 
         } catch (IOException e) {
             e.printStackTrace();
