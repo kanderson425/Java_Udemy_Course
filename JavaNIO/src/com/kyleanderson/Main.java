5f94bebf5d6015e42344753dbc3fe19669cbf872
kanderson425 <kyleanderson.dev@gmail.com>
2020-01-04 13:01:34 -0600

    Updates Main method by adding the readBuffer.get(inputString) to properly read the first string.

--- JavaNIO/src/com/kyleanderson/Main.java
+++ JavaNIO/src/com/kyleanderson/Main.java
@@ -22,73 +22,25 @@ public class Main {
             buffer.put(outputBytes2);
             buffer.putInt(1000);
             buffer.flip();
+
             binChannel.write(buffer);
 
+            RandomAccessFile ra = new RandomAccessFile("data.dat", "rwd");
+            FileChannel channel = ra.getChannel();
 
-//            ByteBuffer buffer = ByteBuffer.allocate(outputBytes.length);
-//            buffer.put(outputBytes);
-//
-//            buffer.flip();
-//            int numBytes = binChannel.write(buffer);
-//            System.out.println("numBytes written was: " + numBytes);
-//
-//            ByteBuffer intBuffer = ByteBuffer.allocate(Integer.BYTES);
-//            intBuffer.putInt(245);
-//            intBuffer.flip();
-//            numBytes = binChannel.write(intBuffer);
-//            System.out.println("numBytes written was: " + numBytes);
-//
-//            intBuffer.flip();
-//            intBuffer.putInt(-98765);
-//            intBuffer.flip();
-//            numBytes = binChannel.write(intBuffer);
-//            System.out.println("numBytes written was: " + numBytes);
-//
-//            RandomAccessFile ra = new RandomAccessFile("data.dat", "rwd");
-//            FileChannel channel = ra.getChannel();
-//            outputBytes[0] = 'a';
-//            outputBytes[1] = 'b';
-//            buffer.flip();
-//            long numBytesRead = channel.read(buffer);
-//            if(buffer.hasArray()) {
-//                System.out.println("byte buffer = " + new String(buffer.array()));
-////                System.out.println("byte buffer = " + new String(outputBytes));
-//            }
-//
-//            // Absolute Read
-//            intBuffer.flip();
-//            numBytesRead = channel.read(intBuffer);
-//            System.out.println(intBuffer.getInt(0));
-//            intBuffer.flip();
-//            numBytesRead = channel.read(intBuffer);
-//            System.out.println(intBuffer.getInt(0));
-//            intBuffer.flip();
-//            System.out.println(intBuffer.getInt());
-//
-//            // Relative Read
-//            //            intBuffer.flip();
-//            //            numBytesRead = channel.read(intBuffer);
-//            //            intBuffer.flip();
-//            //            System.out.println(intBuffer.getInt());
-//            //            intBuffer.flip();
-//            //            numBytesRead = channel.read(intBuffer);
-//            //            intBuffer.flip();
-//            //            System.out.println(intBuffer.getInt());
-//            channel.close();
-//            ra.close();
-//
-//
-////            System.out.println("outputBytes = " + new String(outputBytes));
-//
-////            RandomAccessFile ra = new RandomAccessFile("data.dat", "rwd");
-////            byte[] b = new byte[outputBytes.length];
-////            ra.read(b);
-////            System.out.println(new String(b));
-////
-////            long int1 = ra.readInt();
-////            long int2 = ra.readInt();
-////            System.out.println(int1);
-////            System.out.println(int2);
+            ByteBuffer readBuffer = ByteBuffer.allocate(100);
+            channel.read(readBuffer);
+            readBuffer.flip();
+            byte[] inputString = new byte[outputBytes.length];
+            readBuffer.get(inputString);
+            System.out.println("inputString = " + new String(inputString));
+            System.out.println("int1 = " + readBuffer.getInt());
+            System.out.println("int2 = " + readBuffer.getInt());
+            byte[] inputString2 = new byte[outputBytes2.length];
+            readBuffer.get(inputString2);
+            System.out.println("inputString2 = " + new String(inputString2));
+            System.out.println("int3 = " + readBuffer.getInt());
+
 
         } catch (IOException e) {
             e.printStackTrace();
