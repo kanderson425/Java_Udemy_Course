9db26c7b82e451ff28786c2997e40984fe662173
kanderson425 <kyleanderson.dev@gmail.com>
2020-01-04 13:17:59 -0600

    Adds the definitions of each of the 3 integers starting positions in order to read the integers back in reverse order.

--- JavaNIO/src/com/kyleanderson/Main.java
+++ JavaNIO/src/com/kyleanderson/Main.java
@@ -13,20 +13,38 @@ public class Main {
         try(FileOutputStream binFile = new FileOutputStream("data.dat");
             FileChannel binChannel = binFile.getChannel()) {
 
+            // Uses put chains
             ByteBuffer buffer = ByteBuffer.allocate(100);
-            byte[] outputBytes = "Hello World!".getBytes();
-            byte[] outputBytes2 = "Nice to meet you".getBytes();
-            buffer.put(outputBytes).putInt(245).putInt(-98765).put(outputBytes2).putInt(1000);
-            buffer.flip();
-
 //            byte[] outputBytes = "Hello World!".getBytes();
-//            buffer.put(outputBytes);
-//            buffer.putInt(245);
-//            buffer.putInt(-98765);
 //            byte[] outputBytes2 = "Nice to meet you".getBytes();
-//            buffer.put(outputBytes2);
-//            buffer.putInt(1000);
+//            buffer.put(outputBytes).putInt(245).putInt(-98765).put(outputBytes2).putInt(1000);
 //            buffer.flip();
+
+            //read(ByteBuffer) - reads bytes beginning at the channel's current position, and after the read,
+//                               updates the position accordingly.
+//                               Note that now we're talking about the channel's position, not the byte buffer's position.
+//                               Of course, the bytes will be placed into the buffer starting at its current position.
+//            write(ByteBuffer) - the same as read, except it writes. There's one exception.
+//                              If a datasource is opened in APPEND mode, then the first write will take place starting
+//                              at the end of the datasource, rather than at position 0. After the write, the position
+//                              will be updated accordingly.
+//            position() - returns the channel's position.
+//            position(long) - sets the channel's position to the passed value.
+//            truncate(long) - truncates the size of the attached datasource to the passed value.
+//            size() - returns the size of the attached datasource
+
+            // Does not use chains
+            byte[] outputBytes = "Hello World!".getBytes();
+            buffer.put(outputBytes);
+            long int1Pos = outputBytes.length;
+            buffer.putInt(245);
+            long int2Pos = int1Pos + Integer.BYTES;
+            buffer.putInt(-98765);
+            byte[] outputBytes2 = "Nice to meet you".getBytes();
+            buffer.put(outputBytes2);
+            long int3Pos = int2Pos + Integer.BYTES + outputBytes2.length;
+            buffer.putInt(1000);
+            buffer.flip();
 
             binChannel.write(buffer);
 
