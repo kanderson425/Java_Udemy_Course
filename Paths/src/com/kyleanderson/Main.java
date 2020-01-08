2567434c65acccc685b59eea27b6e943d37d2679
kanderson425 <kyleanderson.dev@gmail.com>
2020-01-07 20:39:48 -0600

    Adds the printFile method.

---
+++ Paths/src/com/kyleanderson/Main.java
@@ -0,0 +1,30 @@
+package com.kyleanderson;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.nio.file.FileSystems;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Path path = FileSystems.getDefault().getPath("workingDirectoryFile.txt");
+        printFile(path);
+
+    }
+
+    private static void printFile(Path path) {
+        try(BufferedReader fileReader = Files.newBufferedReader(path)) {
+            String line;
+            while((line = fileReader.readLine()) != null) {
+                System.out.println(line);
+            }
+        } catch(IOException e) {
+            System.out.println(e.getMessage());
+            e.printStackTrace();
+        }
+    }
+}
+
+
