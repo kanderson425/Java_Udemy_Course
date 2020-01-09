33c2c176a0825714d64310214fb9dbe3d7bc25c3
kanderson425 <kyleanderson.dev@gmail.com>
2020-01-08 21:27:55 -0600

    Adds examples of using the .exists() methods to check for validity of file path.

--- Paths/src/com/kyleanderson/Main.java
+++ Paths/src/com/kyleanderson/Main.java
@@ -21,6 +21,22 @@ public class Main {
 
         filePath = Paths.get(".");
         System.out.println(filePath.toAbsolutePath());
+
+        Path path2 = FileSystems.getDefault().getPath(".","files","..","files","SubdirectoryFile.txt");
+        System.out.println(path2.normalize().toAbsolutePath());
+        printFile(path2.normalize());
+
+        Path path3 = FileSystems.getDefault().getPath("thisfiledoesntexist.txt");
+        System.out.println(path3.toAbsolutePath());
+
+        Path path4 = Paths.get("/Volumes/Test/ING","abcdef","whatever.txt");
+        System.out.println(path4.toAbsolutePath());
+
+        filePath = FileSystems.getDefault().getPath("files");
+        System.out.println("Exists = " + Files.exists(filePath));
+
+        System.out.println("Exists = " + Files.exists(path4));
+
     }
 
     private static void printFile(Path path) {
