71552b7612bf8a7da0a349872ee9d13386a9a08e
kanderson425 <kyleanderson.dev@gmail.com>
2020-01-09 22:48:21 -0600

    Adds example of way to create an instance of the BasicFileAttributes class in order to more efficiently pull the attributes of a file.

--- Paths/src/com/kyleanderson/Main.java
+++ Paths/src/com/kyleanderson/Main.java
@@ -4,16 +4,24 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.*;
+import java.nio.file.attribute.BasicFileAttributes;
 
 public class Main {
 
     public static void main(String[] args) {
         try {
-            //Size
+            //Size & Modified Date
             Path filePath = FileSystems.getDefault().getPath("Examples","Dir1/file1.txt");
             long size = Files.size(filePath);
             System.out.println("Size = "+ size);
             System.out.println("Last modified = "+ Files.getLastModifiedTime(filePath));
+
+            BasicFileAttributes attrs = Files.readAttributes(filePath, BasicFileAttributes.class);
+            System.out.println("Size = " + attrs.size());
+            System.out.println("Last modified = " + attrs.lastModifiedTime());
+            System.out.println("Created = " + attrs.creationTime());
+            System.out.println("Is directory = " + attrs.isDirectory());
+            System.out.println("Is regular file = " + attrs.isRegularFile());
 
 
             //Creates a File
