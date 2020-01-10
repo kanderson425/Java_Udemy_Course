b867deb3cf6d91c0abac24657a6494a9b20f505b
kanderson425 <kyleanderson.dev@gmail.com>
2020-01-09 23:49:51 -0600

    Adds an instance of  DirectoryStream.Filter that filters for all entities that are only files, not directories within Dir2.

--- Directories/src/com/kyleanderson/Main.java
+++ Directories/src/com/kyleanderson/Main.java
@@ -6,8 +6,16 @@ import java.nio.file.*;
 public class Main {
 
     public static void main(String[] args) {
+
+        DirectoryStream.Filter<Path> filter =
+                new DirectoryStream.Filter<Path>() {
+                    public boolean accept(Path path) throws IOException {
+                        return (Files.isRegularFile(path));
+                    }
+                };
+
         Path directory = FileSystems.getDefault().getPath("FileTree/Dir2");
-        try (DirectoryStream<Path> contents = Files.newDirectoryStream(directory, "*.dat")) {
+        try (DirectoryStream<Path> contents = Files.newDirectoryStream(directory, filter)) {
             for(Path file: contents) {
                 System.out.println(file.getFileName());
             }
