e2479509f7f9872d084fed481fc782d57b768f2d
kanderson425 <kyleanderson.dev@gmail.com>
2020-01-09 23:54:53 -0600

    Adds the DirectoryStream.Filter instance using a lambda expression.

--- Directories/src/com/kyleanderson/Main.java
+++ Directories/src/com/kyleanderson/Main.java
@@ -7,13 +7,15 @@ public class Main {
 
     public static void main(String[] args) {
 
-        DirectoryStream.Filter<Path> filter =
-                new DirectoryStream.Filter<Path>() {
-                    public boolean accept(Path path) throws IOException {
-                        return (Files.isRegularFile(path));
-                    }
-                };
+//        DirectoryStream.Filter<Path> filter =
+//                new DirectoryStream.Filter<Path>() {
+//                    public boolean accept(Path path) throws IOException {
+//                        return (Files.isRegularFile(path));
+//                    }
+//                };
 
+        //This utilizes a lambda expression to achieve the same filter
+        DirectoryStream.Filter<Path> filter = p -> Files.isRegularFile(p);
         Path directory = FileSystems.getDefault().getPath("FileTree/Dir2");
         try (DirectoryStream<Path> contents = Files.newDirectoryStream(directory, filter)) {
             for(Path file: contents) {
