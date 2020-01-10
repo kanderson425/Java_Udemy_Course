4a9ffb77a2259c77c362c78a1f50db0d9d298a66
kanderson425 <kyleanderson.dev@gmail.com>
2020-01-09 23:44:10 -0600

    Adds the DirectoryIteratorException catch andd a glob parameter to the DirectoryStream instance.

--- Directories/src/com/kyleanderson/Main.java
+++ Directories/src/com/kyleanderson/Main.java
@@ -1,21 +1,18 @@
 package com.kyleanderson;
 
 import java.io.IOException;
-import java.nio.file.DirectoryStream;
-import java.nio.file.FileSystems;
-import java.nio.file.Files;
-import java.nio.file.Path;
+import java.nio.file.*;
 
 public class Main {
 
     public static void main(String[] args) {
         Path directory = FileSystems.getDefault().getPath("FileTree/Dir2");
-        try (DirectoryStream<Path> contents = Files.newDirectoryStream(directory)) {
+        try (DirectoryStream<Path> contents = Files.newDirectoryStream(directory, "*.dat")) {
             for(Path file: contents) {
                 System.out.println(file.getFileName());
             }
 
-        }catch(IOException e) {
+        }catch(IOException | DirectoryIteratorException e) {
             System.out.println(e.getMessage());
         }
     }
