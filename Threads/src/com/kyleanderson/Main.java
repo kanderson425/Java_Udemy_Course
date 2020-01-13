f5e60d87dc1689edb5dd56336dd9b780d78b6d9a
kanderson425 <kyleanderson.dev@gmail.com>
2020-01-13 15:23:58 -0600

    Adds an anonymous Thread class and runs it.

--- Threads/src/com/kyleanderson/Main.java
+++ Threads/src/com/kyleanderson/Main.java
@@ -8,5 +8,17 @@ public class Main {
         Thread anotherThread = new AnotherThread();
         anotherThread.start();
 
+        new Thread() {
+            public void run() {
+                System.out.println("Hello from the anonymous class thread");
+            }
+        }.start();
+
+        System.out.println("Hello again from the main thread.");
+
+//        anotherThread.start(); We cannot re-use the same instance here. We'd need to create a new instance
+
+
+
     }
 }
