68550edac1b99e5eef143f50db3340c213b2ccea
kanderson425 <kyleanderson.dev@gmail.com>
2020-01-14 12:54:45 -0600

    Adds the counter project and the color-coding conditional that handles the switching between one of two threads.

--- Threads/src/com/kyleanderson/Main.java
+++ Threads/src/com/kyleanderson/Main.java
@@ -23,16 +23,17 @@ public class Main {
             @Override
             public void run() {
                 System.out.println(ANSI_RED + "Hello from the anonymous class' implementation of run()");
+                try {
+                    anotherThread.join();
+                    System.out.println(ANSI_RED+"AnotherThread terminated or timed out, so I'm running again.");
+                } catch (InterruptedException e) {
+                    System.out.println(ANSI_RED+"I couldn't wait after all. I was interrupted.");
+                }
             }
         });
 
         myRunnableThread.start();
 
         System.out.println(ANSI_PURPLE+"Hello again from the main thread.");
-
-//        anotherThread.start(); We cannot re-use the same instance here. We'd need to create a new instance
-
-
-
     }
 }
--- Threads/src/com/kyleanderson/MyRunnable.java
+++ Threads/src/com/kyleanderson/MyRunnable.java
@@ -6,6 +6,6 @@ public class MyRunnable implements Runnable {
 
     @Override
     public void run() {
-        System.out.println(ANSI_RED + "Hello from MyRunnable's implementatin of run()");
+        System.out.println(ANSI_RED + "Hello from MyRunnable's implementation of run()");
     }
 }
