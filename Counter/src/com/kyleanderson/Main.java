7b40a0a3707ea8f1e3df560b168e19f7bcff8235
kanderson425 <kyleanderson.dev@gmail.com>
2020-01-15 13:46:07 -0600

    Adds the 'sychronized' keyword to the doCountdown method which allows for one thread to execute the doCountdown for loop in its entirety before switching to other thread.

--- Counter/src/com/kyleanderson/Main.java
+++ Counter/src/com/kyleanderson/Main.java
@@ -18,7 +18,7 @@ public class Main {
     class Countdown {
         private int i;
 
-        public void doCountdown() {
+        public synchronized void doCountdown() {
             String color;
 
             switch(Thread.currentThread().getName()) {
