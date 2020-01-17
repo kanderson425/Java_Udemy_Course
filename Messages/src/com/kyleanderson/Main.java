ff4251139bf5736fa99fb8a017a96c48f8dec113
kanderson425 <kyleanderson.dev@gmail.com>
2020-01-16 18:46:39 -0600

    Adds the Reader class along with the Reader's respecrtive constructor and run method.

--- Messages/src/com/kyleanderson/Main.java
+++ Messages/src/com/kyleanderson/Main.java
@@ -60,8 +60,27 @@ class Writer implements Runnable {
         message.write("Finished");
 
     }
+}
 
+class Reader implements Runnable {
+    private Message message;
 
+    public Reader(Message message) {
+        this.message = message;
+    }
+
+    public void run() {
+        Random random = new Random();
+        for(String latestMessage = message.read(); !latestMessage.equals("Finished");
+            latestMessage = message.read()) {
+            System.out.println(latestMessage);
+            try {
+                Thread.sleep(random.nextInt(2000));
+            } catch(InterruptedException e) {
+
+            }
+        }
+    }
 }
 
 
