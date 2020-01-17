e3a6313ae02cb5186dcf7b966f45e5a7f68d051c
kanderson425 <kyleanderson.dev@gmail.com>
2020-01-16 18:39:14 -0600

    Adds the Writer class in addition to the run method.

--- Messages/src/com/kyleanderson/Main.java
+++ Messages/src/com/kyleanderson/Main.java
@@ -1,5 +1,7 @@
 package com.kyleanderson;
 
+import java.util.Random;
+
 public class Main {
 
     public static void main(String[] args) {
@@ -28,6 +30,38 @@ class Message {
         empty = false;
         this.message = message;
     }
+}
+
+class Writer implements Runnable {
+    private Message message;
+
+    public Writer(Message message) {
+        this.message = message;
+    }
+
+    public void run() {
+        String messages[] = {
+                "Humpty Dumpty sat on a wall",
+                "Humpty Dumpty had a great fall",
+                "All the king's horses and al the king's men",
+                "Couldn't put Humpty together again"
+        };
+
+        Random random = new Random();
+
+        for(int i=0; i<messages.length; i++) {
+            message.write(messages[i]);
+            try {
+                Thread.sleep(random.nextInt(2000));
+            } catch(InterruptedException e) {
+
+            }
+        }
+        message.write("Finished");
+
+    }
+
+
 }
 
 
