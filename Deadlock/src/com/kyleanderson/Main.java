0f7697e94c7b5fbd968441bdedcee21c42684292
kanderson425 <kyleanderson.dev@gmail.com>
2020-01-21 20:34:13 -0600

    Fixes the deadlock error by forcing the threads to obtaining the two designated locks in the same order.

--- Deadlock/src/com/kyleanderson/Main.java
+++ Deadlock/src/com/kyleanderson/Main.java
@@ -32,20 +32,20 @@ public class Main {
 
     private static class Thread2 extends Thread {
         public void run() {
-            synchronized (lock2) {
-                System.out.println("Thread 2: Has lock2");
+            synchronized (lock1) {
+                System.out.println("Thread 2: Has lock1");
                 try {
                     Thread.sleep(100);
                 } catch (InterruptedException e) {
 
                 }
-                System.out.println("Thread 2: Waiting for lock1");
-                synchronized (lock1) {
-                    System.out.println("Thread 2: Has lock2 and lock1");
+                System.out.println("Thread 2: Waiting for lock2");
+                synchronized (lock2) {
+                    System.out.println("Thread 2: Has lock1 and lock2");
                 }
-                System.out.println("Thread 2: Released lock1");
+                System.out.println("Thread 2: Released lock2");
             }
-            System.out.println("Thread 2: released lock2. Exiting...");
+            System.out.println("Thread 2: released lock1. Exiting...");
         }
     }
 }
