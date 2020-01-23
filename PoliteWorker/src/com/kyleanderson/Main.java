deae9ac8bf41fd0c47168e36fad80a72299c577b
kanderson425 <kyleanderson.dev@gmail.com>
2020-01-22 21:33:01 -0600

    Creates the two worker objects.

---
+++ PoliteWorker/src/com/kyleanderson/Main.java
@@ -0,0 +1,12 @@
+package com.kyleanderson;
+
+public class Main {
+
+    public static void main(String[] args) {
+	    final Worker worker1 = new Worker("Worker 1", true);
+	    final Worker worker2 = new Worker("Worker 2", true);
+
+
+
+    }
+}
