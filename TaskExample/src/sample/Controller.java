6c16b47771d04104fadb326994f5e38f8de5dc2d
kanderson425 <kyleanderson.dev@gmail.com>
2020-01-25 10:35:31 -0600

    Adds two event handlers to handle the visibility of the progressBar and progressLabel upon running of the service and the success of the service.

--- TaskExample/src/sample/Controller.java
+++ TaskExample/src/sample/Controller.java
@@ -5,6 +5,9 @@ import javafx.collections.ObservableList;
 import javafx.concurrent.Service;
 import javafx.concurrent.Task;
 import javafx.concurrent.Worker;
+import javafx.concurrent.WorkerStateEvent;
+import javafx.event.Event;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 import javafx.scene.control.ListView;
@@ -51,6 +54,23 @@ public class Controller {
         progressBar.progressProperty().bind(service.progressProperty());
         progressLabel.textProperty().bind(service.messageProperty());
         listView.itemsProperty().bind(service.valueProperty());
+
+        service.setOnRunning(new EventHandler<WorkerStateEvent>() {
+            @Override
+            public void handle(WorkerStateEvent workerStateEvent) {
+                progressBar.setVisible(true);
+                progressLabel.setVisible(true);
+            }
+        });
+
+        service.setOnSucceeded(new EventHandler<WorkerStateEvent>() {
+            @Override
+            public void handle(WorkerStateEvent workerStateEvent) {
+                progressBar.setVisible(false);
+                progressLabel.setVisible(false);
+            }
+        });
+
 
     }
 
