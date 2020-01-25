2ac18e3fd146d26892a376bbd2aaf45855201034
kanderson425 <kyleanderson.dev@gmail.com>
2020-01-25 10:19:32 -0600

    Binds the progressLabel textProperty to the task's messageProperty.

--- TaskExample/src/sample/Controller.java
+++ TaskExample/src/sample/Controller.java
@@ -4,6 +4,7 @@ import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.concurrent.Task;
 import javafx.fxml.FXML;
+import javafx.scene.control.Label;
 import javafx.scene.control.ListView;
 import javafx.scene.control.ProgressBar;
 
@@ -16,6 +17,9 @@ public class Controller {
 
     @FXML
     private ProgressBar progressBar;
+
+    @FXML
+    private Label progressLabel;
 
     public void initialize() {
         task = new Task<ObservableList<String>>() {
@@ -33,6 +37,7 @@ public class Controller {
 
                 for(int i =0; i<6; i++) {
                     employees.add(names[i]);
+                    updateMessage("Added " + names[i] + " to the list");
                     updateProgress(i + 1, 6);
                     Thread.sleep(200);
                 }
@@ -41,6 +46,7 @@ public class Controller {
         };
 
         progressBar.progressProperty().bind(task.progressProperty());
+        progressLabel.textProperty().bind(task.messageProperty());
         listView.itemsProperty().bind(task.valueProperty());
 
     }
