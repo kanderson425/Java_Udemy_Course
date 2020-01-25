f52283ca01dfb86e91c592ff1fc65ccd72c81b9d
kanderson425 <kyleanderson.dev@gmail.com>
2020-01-24 18:13:50 -0600

    Utilizes the Platform.runLater method to draw the listItems into the scene.

--- TaskExample/src/sample/Controller.java
+++ TaskExample/src/sample/Controller.java
@@ -1,26 +1,41 @@
 package sample;
+import javafx.application.Platform;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.concurrent.Task;
 import javafx.fxml.FXML;
+import javafx.scene.control.ListView;
 
 public class Controller {
 
     private Task<ObservableList<String>> task;
+
+    @FXML
+    private ListView listView;
 
     public void initialize() {
         task = new Task<ObservableList<String>>() {
             @Override
             protected ObservableList<String> call() throws Exception {
                 Thread.sleep(1000);
-                return FXCollections.observableArrayList(
+
+                ObservableList<String> employees = FXCollections.observableArrayList(
                         "Tim Buchalka",
                         "Bill Rogers",
                         "Jack Jill",
                         "Jane Andrews",
                         "Mary Johnson",
                         "Bob McDonald");
-            }
+
+                Platform.runLater(new Runnable() {
+                    @Override
+                    public void run() {
+                        listView.setItems(employees);
+                    }
+                });
+
+                return employees;
+            };
         };
     }
 
