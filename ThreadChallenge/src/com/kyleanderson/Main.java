06878c08f305aed483b3b32973578794de993ff5
kanderson425 <kyleanderson.dev@gmail.com>
2020-01-27 15:46:17 -0600

    Nests the deposit and withdraw methods in a synchronization block.

--- ThreadChallenge/src/com/kyleanderson/Main.java
+++ ThreadChallenge/src/com/kyleanderson/Main.java
@@ -6,41 +6,46 @@ public class Main {
 	final BankAccount account = new BankAccount(1000.00, "12345-678");
 
 	// My Solution
-//    new Thread(new Runnable() {
+//    Thread trThread1 = new Thread(new Runnable() {
 //        @Override
 //        public void run() {
-//            account.deposit(300.00);
-//            account.withdraw(50.00);
-//
+//            synchronized (account) {
+//                account.deposit(300.00);
+//                account.withdraw(50.00);
+//            }
 //        }
-//    }).start();
+//    });
 //
-//    new Thread(new Runnable() {
+//    Thread trThread2 = new Thread(new Runnable() {
 //        @Override
 //        public void run() {
-//            account.deposit(203.75);
-//            account.withdraw(100.00);
+//            synchronized (account) {
+//                account.deposit(203.75);
+//                account.withdraw(100.00);
+//            }
 //        }
-//    }).start();
+//    });
 
         // Tim's Solution
         Thread trThread1 = new Thread() {
             public void run() {
-                account.deposit(300.00);
-                account.withdraw(50.00);
-
+                synchronized (account) {
+                    account.deposit(300.00);
+                    account.withdraw(50.00);
+                }
             }
         };
 
         Thread trThread2 = new Thread() {
             public void run() {
-                account.deposit(203.75);
-                account.withdraw(10.00);
+                synchronized (account) {
+                    account.deposit(203.75);
+                    account.withdraw(10.00);
+                }
             }
         };
 
         trThread1.start();
         trThread2.start();
-
     }
 }
