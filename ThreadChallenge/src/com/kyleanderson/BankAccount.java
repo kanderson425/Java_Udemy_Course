8929684281c6e61ff85a78ac7b2c665e7adf1be4
kanderson425 <kyleanderson.dev@gmail.com>
2020-01-28 10:01:13 -0600

    Updates the BankAccount class by using a ReentrantLock object to make the class threadsafe.

--- ThreadChallenge/src/com/kyleanderson/BankAccount.java
+++ ThreadChallenge/src/com/kyleanderson/BankAccount.java
@@ -1,36 +1,48 @@
 package com.kyleanderson;
 
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
 public class BankAccount {
 
     private double balance;
     private String accountNumber;
 
+    private Lock lock;
+
     public BankAccount(double balance, String accountNumber) {
         this.balance = balance;
         this.accountNumber = accountNumber;
+        this.lock = new ReentrantLock();
     }
 
     public void deposit(double amount) {
-        synchronized (this) {
+        lock.lock();
+        try {
             balance += amount;
             System.out.println("The new balance after deposit is " + balance);
+        } finally {
+            lock.unlock();
         }
     }
 
     public void withdraw(double amount) {
-        synchronized (this) {
+        lock.lock();
+        try {
             balance -= amount;
             System.out.println("The new balance after withdrawal is " + balance);
+        } finally {
+            lock.unlock();
         }
     }
 
     public String getAccountNumber() {
-            return accountNumber;
-
+        return accountNumber;
     }
 
     public void printAccountNumber() {
-            System.out.println("Account Number = " + accountNumber);
+        System.out.println("Account Number = " + accountNumber);
+
 
     }
 }
