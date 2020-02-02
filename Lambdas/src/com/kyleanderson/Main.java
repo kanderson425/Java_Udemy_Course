07603194df8bb7fed98141bc37912b54a16d531a
kanderson425 <kyleanderson.dev@gmail.com>
2020-02-02 13:01:46 -0600

    Adds the new Runnable CodeTORun to Lambdas project.

--- Lambdas/src/com/kyleanderson/Main.java
+++ Lambdas/src/com/kyleanderson/Main.java
@@ -3,6 +3,13 @@ package com.kyleanderson;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+	    new Thread(new CodeToRun()).start();
+    }
+}
+
+class CodeToRun implements Runnable {
+    @Override
+    public void run() {
+        System.out.println("Printing from the Runnable");
     }
 }
