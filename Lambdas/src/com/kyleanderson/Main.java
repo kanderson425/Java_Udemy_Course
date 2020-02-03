27567fb6e206330444b517505abf6732d432e03e
kanderson425 <kyleanderson.dev@gmail.com>
2020-02-03 12:57:49 -0600

    Adds the Employee class and creates example employees that are added to the Employees ArrayList.

--- Lambdas/src/com/kyleanderson/Main.java
+++ Lambdas/src/com/kyleanderson/Main.java
@@ -1,15 +1,58 @@
 package com.kyleanderson;
 
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
 
     public static void main(String[] args) {
-	    new Thread(new CodeToRun()).start();
+	    new Thread(()-> {
+	        System.out.println("Printing from the Runnable");
+            System.out.println("Line 2");
+            System.out.format("This is line %d\n",3);
+        }).start();
+
+	    Employee john = new Employee("John Doe", 30);
+	    Employee tim = new Employee("Tim Bulchalk", 21);
+	    Employee jack = new Employee("Jack Hill", 40);
+	    Employee snow = new Employee("Snow White", 22);
+
+        List<Employee> employees = new ArrayList<>();
+        employees.add(john);
+        employees.add(tim);
+        employees.add(jack);
+        employees.add(snow);
+    }
+
+}
+
+class Employee {
+    private String name;
+    private int age;
+
+    public Employee(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
     }
 }
 
-class CodeToRun implements Runnable {
-    @Override
-    public void run() {
-        System.out.println("Printing from the Runnable");
-    }
-}
+
+
