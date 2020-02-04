83f49d7294cbf5db7ed5a02820bf4c0f154daaa8
kanderson425 <kyleanderson.dev@gmail.com>
2020-02-04 13:21:22 -0600

    Updates the UpperConcat concatenation block to utilize a lambda expression.

--- Lambdas/src/com/kyleanderson/Main.java
+++ Lambdas/src/com/kyleanderson/Main.java
@@ -40,6 +40,23 @@ public class Main {
         for(Employee employee : employees) {
             System.out.println(employee.getName());
         }
+
+//        String sillyString = doStringStuff(new UpperConcat() {
+//            @Override
+//            public String upperAndConcat(String s1, String s2) {
+//                return s1.toUpperCase() + s2.toUpperCase();
+//            }
+//        },
+//                employees.get(0).getName(), employees.get(1).getName());
+//                System.out.println(sillyString);
+
+        UpperConcat uc = (s1, s2) -> s1.toUpperCase() + s2.toUpperCase();
+        String sillyString = doStringStuff(uc, employees.get(0).getName(), employees.get(1).getName());
+        System.out.println(sillyString);
+    }
+
+    public final static String doStringStuff(UpperConcat uc, String s1, String s2) {
+        return uc.upperAndConcat(s1, s2);
     }
 
 }
@@ -68,6 +85,10 @@ class Employee {
     public void setAge(int age) {
         this.age = age;
     }
+}
+
+interface UpperConcat {
+    public String upperAndConcat(String s1, String s2);
 }
 
 
