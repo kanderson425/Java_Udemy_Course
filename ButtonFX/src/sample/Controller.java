b5d7ef32169aabd76945702db5b60eb6ad48f49c
kanderson425 <kyleanderson.dev@gmail.com>
2020-02-04 13:13:28 -0600

    Updates the clickMeButton action handler to utilize a lambda expression.

--- ButtonFX/src/sample/Controller.java
+++ ButtonFX/src/sample/Controller.java
@@ -12,12 +12,6 @@ public class Controller {
     private Button clickMeButton;
 
         public void initialize() {
-            clickMeButton.setOnAction(new EventHandler<ActionEvent>() {
-                @Override
-                public void handle(ActionEvent actionEvent) {
-                    System.out.println("You clicked me!");
-
-                }
-            });
+            clickMeButton.setOnAction(event -> System.out.println("You Clicked me!"));
         }
 }
