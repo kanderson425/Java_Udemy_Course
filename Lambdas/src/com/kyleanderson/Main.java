8a46ceb807a9e8ce3ca914a4ff77eecc56ac3fc8
kanderson425 <kyleanderson.dev@gmail.com>
2020-02-05 20:15:32 -0600

    Adds a printValue method for testing purposes that showcases a local variables scope affected by a thread sleep time.

--- Lambdas/src/com/kyleanderson/Main.java
+++ Lambdas/src/com/kyleanderson/Main.java
@@ -98,14 +98,31 @@ class AnotherClass {
 
     public String doSomething() {
         int i = 0;
+
         UpperConcat uc = (s1, s2) -> {
             System.out.println("The Lambda expression's class is " + getClass().getSimpleName());
+            System.out.println("i in the lambda expression = " + i);
             String result = s1.toUpperCase() + s2.toUpperCase();
             return result;
         };
+
             System.out.println("The AnotherClass class' name is " + getClass().getSimpleName());
             return Main.doStringStuff(uc, "String1", "String2");
+    }
 
+    public void printValue() {
+        int number = 25;
+
+        Runnable r = () -> {
+            try {
+                Thread.sleep(5000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            System.out.println("The value is " + number);
+        };
+
+        new Thread(r).start();
     }
 }
 
