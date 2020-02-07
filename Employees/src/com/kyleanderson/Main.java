2224827c17fd7e423602fc4bdd8173213a280e25
kanderson425 <kyleanderson.dev@gmail.com>
2020-02-07 13:13:13 -0600

    Adds a lambda expression equivalent for printing the names and ages of employees younger than 30.

--- Employees/src/com/kyleanderson/Main.java
+++ Employees/src/com/kyleanderson/Main.java
@@ -31,6 +31,14 @@ public class Main {
             }
         });
 
+        System.out.println("\nEmployees 30 and younger:");
+        System.out.println("===========================");
+        employees.forEach(employee -> {
+            if(employee.getAge() <= 30) {
+                System.out.println(employee.getName());
+            }
+        });
+
 //        for(Employee employee : employees) {
 //            if(employee.getAge() > 30) {
 //                System.out.println(employee.getName());
