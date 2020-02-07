9b4f9cdb74e106e9c5cf8b0c4fe1cb0b34f4f6b0
kanderson425 <kyleanderson.dev@gmail.com>
2020-02-07 12:50:00 -0600

    Updates the Main class in the Employees project to print out employee names for employees over the age of 30.

---
+++ Employees/src/com/kyleanderson/Employee.java
@@ -0,0 +1,33 @@
+package com.kyleanderson;
+
+
+public class Employee {
+    private String name;
+    private int age;
+
+    public Employee(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+}
+
+
+
+
+
---
+++ Employees/src/com/kyleanderson/Main.java
@@ -0,0 +1,35 @@
+package com.kyleanderson;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Employee john = new Employee("John Doe", 30);
+        Employee tim = new Employee("Tim Bulchalka", 21);
+        Employee jack = new Employee("Jack Hill", 40);
+        Employee snow = new Employee("Snow White", 22);
+        Employee red = new Employee("Red Riding Hood", 35);
+        Employee charming = new Employee("Prince Charming", 31);
+
+
+        List<Employee> employees = new ArrayList<>();
+        employees.add(john);
+        employees.add(tim);
+        employees.add(jack);
+        employees.add(snow);
+        employees.add(red);
+        employees.add(charming);
+
+        System.out.println("Employees over 30:");
+        System.out.println("======================");
+        for(Employee employee : employees) {
+            if(employee.getAge() > 30) {
+                System.out.println(employee.getName());
+            }
+        }
+
+    }
+}
