f513f01e1d3707c2f1aa0b1962eb8e0ab0b04d56
kanderson425 <kyleanderson.dev@gmail.com>
2020-02-10 15:27:36 -0600

    Adds the Employee and the Department class

--- Streams/src/com/kyleanderson/Main.java
+++ Streams/src/com/kyleanderson/Main.java
@@ -37,8 +37,12 @@ public class Main {
 
         Stream<String> ioNumberStream = Stream.of("I26", "I17", "I29", "O71");
         Stream<String> inNumberStream = Stream.of("N40", "N36", "I26", "I17", "I29", "O71");
-        Stream<String> concatSteam = Stream.concat(ioNumberStream, inNumberStream);
-        System.out.println(concatSteam.count());
+        Stream<String> concatStream = Stream.concat(ioNumberStream, inNumberStream);
+        System.out.println("-------------------------------------");
+        System.out.println(concatStream
+                .distinct()
+                .peek(System.out::println)
+                .count());
 
     }
 }
