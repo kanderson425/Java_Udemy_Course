6de7b5fd2e6ea07bbcde679293dbbf5e2d5a3694
kanderson425 <kyleanderson.dev@gmail.com>
2020-02-10 15:13:15 -0600

    Creates the ioNumberStream and the inNumberStream and concatenates them both in the contacStream.

--- Streams/src/com/kyleanderson/Main.java
+++ Streams/src/com/kyleanderson/Main.java
@@ -3,6 +3,7 @@ package com.kyleanderson;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Stream;
 
 public class Main {
 
@@ -16,14 +17,28 @@ public class Main {
 
         List<String> gNumbers = new ArrayList<>();
 
-        someBingoNumbers.forEach(number -> {
-            if(number.toUpperCase().startsWith("G")) {
-                gNumbers.add(number);
-            }
-        });
+//        someBingoNumbers.forEach(number -> {
+//            if(number.toUpperCase().startsWith("G")) {
+//                gNumbers.add(number);
+//            }
+//        });
+//
+//        gNumbers.sort((String s1, String s2) -> s1.compareTo(s2));
+//        gNumbers.forEach((String s) -> System.out.println(s));
 
-        gNumbers.sort((String s1, String s2) -> s1.compareTo(s2));
-        gNumbers.forEach((String s) -> System.out.println(s));
+        //Replace code above with one line: stream
+
+        someBingoNumbers
+                .stream()
+                .map(String::toUpperCase)  //String::toUpperCase = s->s.toUpperCase()
+                .filter(s->s.startsWith("G"))
+                .sorted()
+                .forEach(System.out::println);
+
+        Stream<String> ioNumberStream = Stream.of("I26", "I17", "I29", "O71");
+        Stream<String> inNumberStream = Stream.of("N40", "N36", "I26", "I17", "I29", "O71");
+        Stream<String> concatSteam = Stream.concat(ioNumberStream, inNumberStream);
+        System.out.println(concatSteam.count());
 
     }
 }
