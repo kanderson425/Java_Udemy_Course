3b7d1b9848ee44320bdb2591044fcd77950a1e50
kanderson425 <kyleanderson.dev@gmail.com>
2020-02-13 12:42:26 -0600

    Adds the solution to challenge 4

--- LambdaChallenges/src/com/kyleanderson/Main.java
+++ LambdaChallenges/src/com/kyleanderson/Main.java
@@ -7,57 +7,77 @@ public class Main {
     public static void main(String[] args) {
 
 	//Challenge 1
-        Runnable runnable = new Runnable() {
-            @Override
-            public void run() {
-                String myString = "Let's split this up into an array";
-                String[] parts = myString.split(" " );
-                for(String part: parts) {
-                    System.out.println(part);
-                }
-            }
-        };
-
-        //Solution
-        Runnable lambdaRunnable = () -> {
-            String myString = "Let's split this up into an array";
-            String[] parts = myString.split(" " );
-            for(String part: parts) {
-                System.out.println(part);
-            }
-        };
+//        Runnable runnable = new Runnable() {
+//            @Override
+//            public void run() {
+//                String myString = "Let's split this up into an array";
+//                String[] parts = myString.split(" " );
+//                for(String part: parts) {
+//                    System.out.println(part);
+//                }
+//            }
+//        };
+//
+//        //Solution
+//        Runnable lambdaRunnable = () -> {
+//            String myString = "Let's split this up into an array";
+//            String[] parts = myString.split(" " );
+//            for(String part: parts) {
+//                System.out.println(part);
+//            }
+//        };
 
 
         //Challenge 2 Solution
-        Function<String, String> lambdaFunction =  s -> {
+//        Function<String, String> lambdaFunction =  s -> {
+//            StringBuilder returnVal = new StringBuilder();
+//            for(int i = 0; i < s.length(); i++) {
+//                if(i % 2 == 1) {
+//                    returnVal.append(s.charAt(i));
+//                }
+//            }
+//            return returnVal.toString();
+//        };
+
+        //Challenge 3 Solution
+//        System.out.println(lambdaFunction.apply("1234567890"));
+
+
+        //Challenge 4 Solution
+        Function<String, String> lambdaFunction =  (s) -> {
             StringBuilder returnVal = new StringBuilder();
             for(int i = 0; i < s.length(); i++) {
                 if(i % 2 == 1) {
                     returnVal.append(s.charAt(i));
                 }
             }
+            System.out.println(returnVal.toString());
             return returnVal.toString();
         };
 
-        //Challenge 3 Solution
-        System.out.println(lambdaFunction.apply("1234567890"));
-
+        everySecondChar("1234567890", lambdaFunction);
     }
-
 
 
 
     //Challenge 2
-    public static String everySecondChar(String source) {
-        StringBuilder returnVal = new StringBuilder();
-        for(int i = 0; i < source.length(); i++) {
-            if(i % 2 == 1) {
-                returnVal.append(source.charAt(i));
-            }
-        }
-        return returnVal.toString();
+//    public static String everySecondChar(String source) {
+//        StringBuilder returnVal = new StringBuilder();
+//        for(int i = 0; i < source.length(); i++) {
+//            if(i % 2 == 1) {
+//                returnVal.append(source.charAt(i));
+//            }
+//        }
+//        return returnVal.toString();
+//    }
+
+
+
+    public static String everySecondChar(String source, Function<String,String> lambdaFunction) {
+        return lambdaFunction.apply(source);
     }
 
-
 }
+
+
 
