10aed9e0d276512a1896b9a542394c20a4f61b88
kanderson425 <kyleanderson.dev@gmail.com>
2020-02-18 13:23:45 -0600

    Adds some small examples of the String object replaceAll method.

---
+++ Regular Expressions/src/com/kyleanderson/Main.java
@@ -0,0 +1,14 @@
+package com.kyleanderson;
+
+public class Main {
+
+    public static void main(String[] args) {
+	    String string = "I am a string. Yes, I am";
+        System.out.println(string);
+        String yourString = string.replaceAll("I", "You");
+        System.out.println(yourString);
+
+        String alphanumeric = "abcDeeeF12Ghhiiiijkl99z";
+        System.out.println(alphanumeric.replaceAll(".","Y"));
+    }
+}
