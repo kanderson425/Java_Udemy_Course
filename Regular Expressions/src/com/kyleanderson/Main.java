dba14436f558494c53da75e49138c45c9bfbf1c1
kanderson425 <kyleanderson.dev@gmail.com>
2020-02-19 12:47:15 -0600

    Adds character matching for chacters followed by other character classes strung together.

--- Regular Expressions/src/com/kyleanderson/Main.java
+++ Regular Expressions/src/com/kyleanderson/Main.java
@@ -10,5 +10,20 @@ public class Main {
 
         String alphanumeric = "abcDeeeF12Ghhiiiijkl99z";
         System.out.println(alphanumeric.replaceAll(".","Y"));
+
+        System.out.println(alphanumeric.replaceAll("^abcDeee", "YYY"));
+
+        String secondString = "abcDeeeF12GhhabcDeeeiiiijkl99z";
+        System.out.println(secondString.replaceAll("^abcDeee", "YYY"));
+
+        System.out.println(alphanumeric.matches("^hello"));
+        System.out.println(alphanumeric.matches("^abcDeee"));
+        System.out.println(alphanumeric.matches("^abcDeeeF12Ghhiiiijkl99z"));
+
+        System.out.println(alphanumeric.replaceAll("ijkl99z$", "THE END"));
+        System.out.println(alphanumeric.replaceAll("[aei]", "X"));
+        System.out.println(alphanumeric.replaceAll("[aei]", "I replaced a letter here"));
+        System.out.println(alphanumeric.replaceAll("[aei][Fj]", "X"));
+
     }
 }
