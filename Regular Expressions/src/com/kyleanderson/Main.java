e4c484c2ead9c2602948f4b8e8f5412304ea3d3b
kanderson425 <kyleanderson.dev@gmail.com>
2020-02-22 12:36:07 -0600

    Updates the greedy quantifier '*' with the lazy quantifier '?' to generate separate occurences of the '<h2></h2>' tags.

--- Regular Expressions/src/com/kyleanderson/Main.java
+++ Regular Expressions/src/com/kyleanderson/Main.java
@@ -74,10 +74,15 @@ public class Main {
             System.out.println("Occurrence " + count + " : " + matcher.start() + " to " + matcher.end());
         }
 
-        String h2GroupPattern = "(<h2>)";
+        String h2GroupPattern = "(<h2>.*?</h2>)";
         Pattern groupPattern = Pattern.compile(h2GroupPattern);
         Matcher groupMatcher = groupPattern.matcher(htmlText);
         System.out.println(matcher.matches());
+        groupMatcher.reset();
+
+        while(groupMatcher.find()) {
+            System.out.println("Ocurrences: " + groupMatcher.group(1));
+        }
 
 
 
