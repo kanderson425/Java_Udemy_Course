1488125ee5994af4eb72ade8698718da1384733c
kanderson425 <kyleanderson.dev@gmail.com>
2020-02-23 09:45:28 -0600

    Adds my solution to Challenge 3.

--- Regular Expressions Challenge/src/com/kyleanderson/Main.java
+++ Regular Expressions Challenge/src/com/kyleanderson/Main.java
@@ -1,5 +1,8 @@
 package com.kyleanderson;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class Main {
 
     public static void main(String[] args) {
@@ -10,8 +13,22 @@ public class Main {
 
         //Challenge2
         String challenge2 = "I want a ball.";
-        String challenge3 = "ball";
-        System.out.println(challenge2.matches("I want a \\bb.*"));
+//        System.out.println(challenge2.matches("I want a \\bb.*"));
+
+        //Solution
+        String regExp = "I want a \\w+.";
+//        System.out.println(challenge1.matches(regExp));
+//        System.out.println(challenge2.matches(regExp));
+
+        //Uses the logical | operator and a group
+        String regExp1 = "I want a (bike|ball).";
+//        System.out.println(challenge1.matches(regExp1));
+
+        //Challenge 3
+        String regExp3 = "I want a \\w+.";
+        Pattern pattern = Pattern.compile(regExp3);
+        Matcher matcher = pattern.matcher(challenge2);
+        System.out.println(matcher.matches());
 
     }
 }
