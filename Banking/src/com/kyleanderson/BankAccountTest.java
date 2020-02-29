dc19984953ec588b66986e74b546fef8f3d025b9
kanderson425 <kyleanderson.dev@gmail.com>
2020-02-29 07:34:14 -0600

    Adds the dummyTest method, which is manually failing intentionally.

--- Banking/src/com/kyleanderson/BankAccountTest.java
+++ Banking/src/com/kyleanderson/BankAccountTest.java
@@ -6,13 +6,24 @@ public class BankAccountTest {
 
     @org.junit.Test
     public void deposit() {
+        fail("This test is yet to be implemented");
     }
 
     @org.junit.Test
     public void withdraw() {
+        fail("This test is yet to be implemented");
+
     }
 
     @org.junit.Test
     public void getBalance() {
+        fail("This test is yet to be implemented");
+
+    }
+
+    @org.junit.Test
+    public void dummyTest() {
+        assertEquals(20, 21);
     }
 }
+
--- Banking/src/com/kyleanderson/Main.java
+++ Banking/src/com/kyleanderson/Main.java
@@ -3,6 +3,6 @@ package com.kyleanderson;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        System.out.println("The application is running");
     }
 }
