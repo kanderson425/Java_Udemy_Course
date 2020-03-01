5fd2234950c850cc62e3b3a24f69f0a2b993b6b6
kanderson425 <kyleanderson.dev@gmail.com>
2020-03-01 16:22:26 -0600

    Adds the Setup method to the BankAccountTest class with the Before annotation to instantiate a BankAccount class prior to running our tests.

--- Banking/src/com/kyleanderson/BankAccountTest.java
+++ Banking/src/com/kyleanderson/BankAccountTest.java
@@ -4,9 +4,16 @@ import static org.junit.Assert.*;
 
 public class BankAccountTest {
 
+    private BankAccount account;
+
+    @org.junit.Before
+    public void setup() {
+        BankAccount account = new BankAccount("Kyle", "Anderson", 1000.00, BankAccount.CHECKING);
+        System.out.println("Running a test...");
+    }
+
     @org.junit.Test
     public void deposit() {
-        BankAccount account = new BankAccount("Kyle", "Anderson", 1000.00, BankAccount.CHECKING);
         double balance = account.deposit(200.00, true);
         assertEquals(1200.00, balance, 0);
 
@@ -19,24 +26,20 @@ public class BankAccountTest {
     }
 
     @org.junit.Test
-    public void getBalance_deposit() {
-        BankAccount account = new BankAccount("Kyle", "Anderson", 1000.00, BankAccount.CHECKING);
-        account.deposit(200.00, true);
+    public void getBalance_deposit() { account.deposit(200.00, true);
         assertEquals(1200.00, account.getBalance(), 0);
 
     }
 
     @org.junit.Test
     public void getBalance_withdraw() {
-        BankAccount account = new BankAccount("Kyle", "Anderson", 1000.00, BankAccount.CHECKING);
         account.withdraw(200.00, true);
         assertEquals(800.00, account.getBalance(), 0);
     }
 
     @org.junit.Test
     public void isChecking_true() {
-        BankAccount account = new BankAccount("Kyle", "Anderson", 1000.00, BankAccount.CHECKING);
-        assertTrue(account.isChecking());
+        assertTrue("The account is NOT a checking account", account.isChecking());
 
     }
 
