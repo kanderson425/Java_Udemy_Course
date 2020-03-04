d68329872ffb42fab97119d5014d104ec4591344
kanderson425 <kyleanderson.dev@gmail.com>
2020-03-04 14:06:16 -0600

    Adds the BankAccountTestParameterized class.

--- Banking/src/com/kyleanderson/BankAccountTest.java
+++ Banking/src/com/kyleanderson/BankAccountTest.java
@@ -31,17 +31,17 @@ public class BankAccountTest {
         assertEquals(400.00, balance, 0);
     }
 
-    @org.junit.Test(expected = IllegalArgumentException.class)
+    @org.junit.Test (expected = IllegalArgumentException.class)
     public void withdraw_notBranch() throws Exception {
-        double balance = account.withdraw(600.00, false);
-        assertEquals(400.00, balance,0);
+            account.withdraw(600.00, false);
+            fail("Should have thrown an IllegalArgumentException");
+
     }
 
     @org.junit.Test
     public void getBalance_deposit() throws Exception {
         account.deposit(200.00, true);
         assertEquals(1200.00, account.getBalance(), 0);
-
     }
 
     @org.junit.Test
