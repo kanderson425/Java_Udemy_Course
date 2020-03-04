0764dce748ff6fe88dc59bfd79e4d9c55dce1b32
kanderson425 <kyleanderson.dev@gmail.com>
2020-03-04 13:51:48 -0600

    Adds the IllegalArgumentException qualifier to the Junit annotation on the withdraw_notBranch method.

--- Banking/src/com/kyleanderson/BankAccountTest.java
+++ Banking/src/com/kyleanderson/BankAccountTest.java
@@ -26,20 +26,26 @@ public class BankAccountTest {
     }
 
     @org.junit.Test
-    public void withdraw() {
+    public void withdraw_branch() throws Exception {
         double balance = account.withdraw(600.00, true);
         assertEquals(400.00, balance, 0);
+    }
 
+    @org.junit.Test(expected = IllegalArgumentException.class)
+    public void withdraw_notBranch() throws Exception {
+        double balance = account.withdraw(600.00, false);
+        assertEquals(400.00, balance,0);
     }
 
     @org.junit.Test
-    public void getBalance_deposit() { account.deposit(200.00, true);
+    public void getBalance_deposit() throws Exception {
+        account.deposit(200.00, true);
         assertEquals(1200.00, account.getBalance(), 0);
 
     }
 
     @org.junit.Test
-    public void getBalance_withdraw() {
+    public void getBalance_withdraw() throws Exception {
         account.withdraw(200.00, true);
         assertEquals(800.00, account.getBalance(), 0);
     }
