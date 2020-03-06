feb43e66abe7b8dbd5cf9973f59711b927c4bcc3
kanderson425 <kyleanderson.dev@gmail.com>
2020-03-05 18:09:00 -0600

    Adds the setup before with the '@Before' annotation in order to create a universal instance of the Utilities class.

--- Junit Challenge/src/com/kyleanderson/UtilitiesTest.java
+++ Junit Challenge/src/com/kyleanderson/UtilitiesTest.java
@@ -5,11 +5,17 @@ import jdk.jshell.execution.Util;
 import static org.junit.Assert.*;
 
 public class UtilitiesTest {
+    private Utilities util;
+
+    @org.junit.Before
+    public void setup() {
+        util = new Utilities();
+        System.out.println("Running some tests...");
+    }
 
 
     @org.junit.Test
     public void everyNthChar() throws Exception {
-        Utilities util = new Utilities();
         char[] source = {'h','e','l','l','o'};
         char[] output = util.everyNthChar(new char[] {'h','e','l','l','o'}, 2);
         assertArrayEquals(output, util.everyNthChar(source, 2));
@@ -21,7 +27,6 @@ public class UtilitiesTest {
 
     @org.junit.Test
     public void removePairs() throws Exception  {
-        Utilities util = new Utilities();
         assertEquals("ABCDEF", util.removePairs("AABCDDEFF"));
         assertEquals("ABCABDEF", util.removePairs("ABCCABDEEF"));
         assertNull("Did not get null returned. Argument passed is null", util.removePairs(null));
@@ -32,19 +37,16 @@ public class UtilitiesTest {
 
     @org.junit.Test
     public void converter() throws Exception {
-        Utilities util = new Utilities();
         assertEquals(300,util.converter(10, 5));
     }
 
     @org.junit.Test(expected = ArithmeticException.class)
     public void converter_arithmeticException() throws Exception {
-        Utilities util = new Utilities();
         util.converter(10, 0);
     }
 
     @org.junit.Test
     public void nullIfOddLength() throws Exception {
-        Utilities util = new Utilities();
         assertNotNull(util.nullIfOddLength("Test"));
 
         assertNull(util.nullIfOddLength("Testing"));
