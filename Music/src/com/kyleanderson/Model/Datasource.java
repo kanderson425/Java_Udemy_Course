c50d66bbc28ecff270c510552efb56c2fed4705e
kanderson425 <kyleanderson.dev@gmail.com>
2020-03-15 12:56:32 -0500

    Update the queryArtists method witin the Datasource class to utilize a try-with-resources block.

--- Music/src/com/kyleanderson/Model/Datasource.java
+++ Music/src/com/kyleanderson/Model/Datasource.java
@@ -49,13 +49,8 @@ public class Datasource {
 
     public List<Artist> queryArtists() {
 
-        Statement statement = null;
-        ResultSet results = null;
-
-        try {
-
-            statement = conn.createStatement();
-            results = statement.executeQuery("SELECT * FROM " + TABLE_ARTISTS);
+        try(Statement statement = conn.createStatement();
+            ResultSet results = statement.executeQuery("SELECT * FROM " + TABLE_ARTISTS)) {
 
             List<Artist> artists = new ArrayList<>();
             while(results.next()) {
@@ -70,21 +65,6 @@ public class Datasource {
         } catch(SQLException e) {
             System.out.println("Query failed: " + e.getMessage());
             return null;
-        } finally {
-            try {
-                if(results != null) {
-                    results.close();
-                }
-            } catch (SQLException e) {
-                System.out.println("Error closing ResultSet: " + e.getMessage());
-            }
-            try {
-                if(statement != null) {
-                    statement.close();
-                }
-            } catch (SQLException e) {
-                System.out.println("Error closing Statement: " + e.getMessage());
-            }
         }
     }
 
