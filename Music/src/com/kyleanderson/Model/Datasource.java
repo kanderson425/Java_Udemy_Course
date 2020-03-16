cb1fbbdc43013668f016331964f0afc4615fda9b
kanderson425 <kyleanderson.dev@gmail.com>
2020-03-15 21:49:05 -0500

    Adds the column indices to the Datasource class so that the appropriate getter methods can call ont eh index int rather than the column name.

--- Music/src/com/kyleanderson/Model/Datasource.java
+++ Music/src/com/kyleanderson/Model/Datasource.java
@@ -16,14 +16,24 @@ public class Datasource {
     public static final String COLUMN_ALBUM_ID = "_id";
     public static final String COLUMN_ALBUM_NAME = "name";
     public static final String COLUMN_ALBUM_ARTIST = "artist";
+    public static final int INDEX_ALBUM_ID = 1;
+    public static final int INDEX_ALBUM_NAME = 2;
+    public static final int INDEX_ALBUM_ARTIST = 3;
 
     public static final String TABLE_ARTISTS = "artists";
     public static final String COLUMN_ARTIST_ID = "_id";
     public static final String COLUMN_ARTIST_NAME = "name";
+    public static final int INDEX_ARTIST_ID = 1;
+    public static final int INDEX_ARTIST_NAME = 2;
 
     public static final String TABLE_SONGS = "songs";
+    public static final String COLUMN_SONG_ID = "_id";
     public static final String COLUMN_SONG_TRACK = "track";
     public static final String COLUMN_SONG_ALBUM = "album";
+    public static final int INDEX_SONG_ID = 1;
+    public static final int INDEX_SONG_TRACK = 2;
+    public static final int INDEX_SONG_TITLE = 3;
+    public static final int INDEX_SONG_ALBUM = 4;
 
     private Connection conn;
 
@@ -55,8 +65,8 @@ public class Datasource {
             List<Artist> artists = new ArrayList<>();
             while(results.next()) {
                 Artist artist = new Artist();
-                artist.setId(results.getInt(COLUMN_ARTIST_ID));
-                artist.setName(results.getString(COLUMN_ARTIST_NAME));
+                artist.setId(results.getInt(INDEX_ARTIST_ID));
+                artist.setName(results.getString(INDEX_ARTIST_NAME));
                 artists.add(artist);
             }
 
