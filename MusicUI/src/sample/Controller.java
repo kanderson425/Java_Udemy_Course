641590e3fa63f3fac23dcaf5824341bcb7b4bb47
kanderson425 <kyleanderson.dev@gmail.com>
2020-03-21 16:13:40 -0500

    Adds the GetAllArtistsTask class that extends Task. Adds the call override to the GetAllArtistsTask class.

--- MusicUI/src/sample/Controller.java
+++ MusicUI/src/sample/Controller.java
@@ -1,4 +1,21 @@
 package sample;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.concurrent.Task;
+import sample.Model.Artist;
+import sample.Model.Datasource;
+
 public class Controller {
+
+}
+
+class GetAllArtistsTask extends Task {
+
+    @Override
+    public ObservableList<Artist> call() {
+        return FXCollections.observableArrayList
+                (Datasource.getInstance().queryArtists(Datasource.ORDER_BY_ASC));
+    }
+
 }
