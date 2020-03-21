3e779926d65578ef1eeb80aed0262ca9e5740e17
kanderson425 <kyleanderson.dev@gmail.com>
2020-03-21 18:08:23 -0500

    Adds the progressBar and the thread sleeping for 20 milliseconds in the queryArtists method.

--- MusicUI/src/sample/Controller.java
+++ MusicUI/src/sample/Controller.java
@@ -4,6 +4,7 @@ import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.concurrent.Task;
 import javafx.fxml.FXML;
+import javafx.scene.control.ProgressBar;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
@@ -13,11 +14,21 @@ import sample.Model.Datasource;
 
 public class Controller {
     @FXML
-    private TableView<Artist> artistTable;
+    private TableView artistTable;
 
+    @FXML
+    private ProgressBar progressBar;
+
+    @FXML
     public void listArtists() {
         Task<ObservableList<Artist>> task = new GetAllArtistsTask();
         artistTable.itemsProperty().bind(task.valueProperty());
+        progressBar.progressProperty().bind(task.progressProperty());
+
+        progressBar.setVisible(true);
+
+        task.setOnSucceeded(e -> progressBar.setVisible(false));
+        task.setOnFailed(e -> progressBar.setVisible(false));
 
         new Thread(task).start();
     }
--- MusicUI/src/sample/Model/Datasource.java
+++ MusicUI/src/sample/Model/Datasource.java
@@ -207,6 +207,11 @@ public class Datasource {
 
             List<Artist> artists = new ArrayList<>();
             while(results.next()) {
+                try {
+                    Thread.sleep(20);
+                } catch(InterruptedException e) {
+                    System.out.println("Interrupted: " + e.getMessage());
+                }
                 Artist artist = new Artist();
                 artist.setId(results.getInt(INDEX_ARTIST_ID));
                 artist.setName(results.getString(INDEX_ARTIST_NAME));
