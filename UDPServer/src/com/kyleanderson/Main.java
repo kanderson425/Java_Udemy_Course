cc5ac2769dff8b9ef8e56d15792f019f52f6db23
kanderson425 <kyleanderson.dev@gmail.com>
2020-03-24 17:23:44 -0500

    Adds the server-side code to the UDPServer project.

---
+++ UDPServer/src/com/kyleanderson/Main.java
@@ -0,0 +1,28 @@
+package com.kyleanderson;
+
+import javax.xml.crypto.Data;
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.SocketException;
+
+public class Main {
+
+    public static void main(String[] args) {
+        try {
+            DatagramSocket socket = new DatagramSocket(5000);
+
+            while(true) {
+                byte[] buffer = new byte[50];
+                DatagramPacket packet = new DatagramPacket(buffer, buffer.length);
+                socket.receive(packet);
+                System.out.println("Text received is: " + new String(buffer));
+            }
+
+        } catch(SocketException e) {
+            System.out.println("SocketException: " + e.getMessage());
+        } catch(IOException e) {
+            System.out.println("IOException: "  + e.getMessage());
+        }
+    }
+}
