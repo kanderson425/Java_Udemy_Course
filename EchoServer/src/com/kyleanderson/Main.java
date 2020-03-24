e7aa416f2874c3bbcd8ada640a1a9bbd37abb9c3
kanderson425 <kyleanderson.dev@gmail.com>
2020-03-23 21:01:27 -0500

    Updates the code in the Main class in order to instantiate a new thread upon new connection.

--- EchoServer/src/com/kyleanderson/Main.java
+++ EchoServer/src/com/kyleanderson/Main.java
@@ -1,9 +1,6 @@
 package com.kyleanderson;
 
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
 
@@ -13,24 +10,11 @@ public class Main {
         try (ServerSocket serverSocket = new ServerSocket(5000)) {
 
             while(true) {
-                Socket socket = serverSocket.accept();
-                System.out.println("Client Connected");
+                new Echoer(serverSocket.accept()).start();
+//                Socket socket = serverSocket.accept();
+//                Echoer echoer = new Echoer(socket);
+//                echoer.start();
 
-                BufferedReader input = new BufferedReader(
-                        new InputStreamReader(socket.getInputStream()));
-                PrintWriter output = new PrintWriter(socket.getOutputStream(), true);
-
-                String echoString = input.readLine();
-                try {
-                    Thread.sleep(15000);
-                }catch(InterruptedException e) {
-                    System.out.println("Thread interrupted");
-                }
-
-                if(echoString.equals("exit")) {
-                    break;
-                }
-                output.println("Echo from server: " + echoString);
             }
 
         } catch(IOException e) {
