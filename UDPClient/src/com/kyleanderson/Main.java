b4f18a26ba0af8bc27e574b6407bc08fc9493786
kanderson425 <kyleanderson.dev@gmail.com>
2020-03-24 17:28:40 -0500

    Adds the client-side code to the UDPClient project.

--- UDPClient/src/com/kyleanderson/Main.java
+++ UDPClient/src/com/kyleanderson/Main.java
@@ -1,8 +1,36 @@
 package com.kyleanderson;
 
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+import java.net.SocketTimeoutException;
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+	    try {
+            InetAddress address = InetAddress.getLocalHost(); //getByName()
+            DatagramSocket datagramSocket = new DatagramSocket();
+
+            Scanner scanner = new Scanner(System.in);
+            String echoString;
+
+            do {
+                System.out.println("Enter string to be echoed: " );
+                echoString = scanner.nextLine();
+
+                byte[] buffer = echoString.getBytes();
+
+                DatagramPacket packet = new DatagramPacket(buffer, buffer.length, address, 5000);
+                datagramSocket.send(packet);
+            } while(!echoString.equals("Exit"));
+
+        } catch(SocketTimeoutException e) {
+            System.out.println("The socket timed out");
+        } catch(IOException e) {
+            System.out.println("Client error");
+        }
     }
 }
