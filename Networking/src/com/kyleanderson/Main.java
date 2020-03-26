495cec6881ca36e73e0aecce71b3456885d3243a
kanderson425 <kyleanderson.dev@gmail.com>
2020-03-26 17:20:11 -0500

    Uses a BufferedReader and InputStreamReader to console out the underlying html of www.example.org.

--- Networking/src/com/kyleanderson/Main.java
+++ Networking/src/com/kyleanderson/Main.java
@@ -1,40 +1,27 @@
 package com.kyleanderson;
 
-import java.net.MalformedURLException;
-import java.net.URI;
-import java.net.URISyntaxException;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.net.URL;
 
 public class Main {
 
     public static void main(String[] args) {
         try {
-//            URI uri = new URI("http://username:password@myserver.com:5000/catalog/phones?os=android#samsung");
-            URI baseUri = new URI("http://username:password@myserver.com:5000");
-            URI uri1 = new URI("/catalog/phones?os=android#samsung");
-            URI uri2 = new URI("/catalog/tvs?manufacturer=samsung");
-            URI uri3 = new URI("/stores/locations?zip=12345");
+            URL url = new URL("http://example.org");
+            BufferedReader inputStream = new BufferedReader(
+                    new InputStreamReader(url.openStream()));
 
-            URI resolvedUri1 = baseUri.resolve(uri1);
-            URI resolvedUri2 = baseUri.resolve(uri2);
-            URI resolvedUri3 = baseUri.resolve(uri3);
+            String line = "";
+            while(line != null) {
+                line = inputStream.readLine();
+                System.out.println(line);
+            }
+            inputStream.close();
 
-            URL url1  = resolvedUri1.toURL();
-            System.out.println("URL 1 = " + url1);
-            URL url2  = resolvedUri2.toURL();
-            System.out.println("URL 2 = " + url2);
-            URL url3  = resolvedUri3.toURL();
-            System.out.println("URL 3 = " + url3);
-
-            URI relavitizedURI = baseUri.relativize(resolvedUri2);
-            System.out.println("Relative URI = " + relavitizedURI);
-
-
-
-        } catch(URISyntaxException e) {
-            System.out.println("URI Bad Syntax: " + e.getMessage());
-        } catch(MalformedURLException e) {
-            System.out.println("URL Malformed: " + e.getMessage());
+        } catch(IOException e) {
+            System.out.println("IOException: " + e.getMessage());
         }
     }
 }
