18da76599cebeda1f86dbb397593aaa13fa3f467
kanderson425 <kyleanderson.dev@gmail.com>
2020-03-25 22:32:08 -0500

    Adds an example of using the relavitizedURI method.

--- Networking/src/com/kyleanderson/Main.java
+++ Networking/src/com/kyleanderson/Main.java
@@ -14,19 +14,20 @@ public class Main {
             URI uri1 = new URI("/catalog/phones?os=android#samsung");
             URI uri2 = new URI("/catalog/tvs?manufacturer=samsung");
             URI uri3 = new URI("/stores/locations?zip=12345");
-            URI resolvedUri1 = baseUri.resolve(uri1);
 
-            URL url  = resolvedUri.toURL();
-            System.out.println("URL = " + url);
-//            System.out.println("Scheme = " + uri.getScheme());
-//            System.out.println("Scheme-specific part = " + uri.getSchemeSpecificPart());
-//            System.out.println("Authority = " + uri.getAuthority());
-//            System.out.println("User info = " + uri.getUserInfo());
-//            System.out.println("Host = " + uri.getHost());
-//            System.out.println("Port = " + uri.getPort());
-//            System.out.println("Path = " + uri.getPath());
-//            System.out.println("Query = " + uri.getQuery());
-//            System.out.println("Fragment = " + uri.getFragment());
+            URI resolvedUri1 = baseUri.resolve(uri1);
+            URI resolvedUri2 = baseUri.resolve(uri2);
+            URI resolvedUri3 = baseUri.resolve(uri3);
+
+            URL url1  = resolvedUri1.toURL();
+            System.out.println("URL 1 = " + url1);
+            URL url2  = resolvedUri2.toURL();
+            System.out.println("URL 2 = " + url2);
+            URL url3  = resolvedUri3.toURL();
+            System.out.println("URL 3 = " + url3);
+
+            URI relavitizedURI = baseUri.relativize(resolvedUri2);
+            System.out.println("Relative URI = " + relavitizedURI);
 
 
 
