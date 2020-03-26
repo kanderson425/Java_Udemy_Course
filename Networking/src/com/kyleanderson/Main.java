772c0e1300f462d13548426942916277f562c7bc
kanderson425 <kyleanderson.dev@gmail.com>
2020-03-26 17:24:11 -0500

    Reads the exmple.com page using a URLConnection instance.

--- Networking/src/com/kyleanderson/Main.java
+++ Networking/src/com/kyleanderson/Main.java
@@ -3,15 +3,23 @@ package com.kyleanderson;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.net.MalformedURLException;
 import java.net.URL;
+import java.net.URLClassLoader;
+import java.net.URLConnection;
 
 public class Main {
 
     public static void main(String[] args) {
         try {
             URL url = new URL("http://example.org");
+            URLConnection urlConnection = url.openConnection();
+            urlConnection.setDoOutput(true);
+            urlConnection.connect();
+
+
             BufferedReader inputStream = new BufferedReader(
-                    new InputStreamReader(url.openStream()));
+                    new InputStreamReader(urlConnection.getInputStream()));
 
             String line = "";
             while(line != null) {
@@ -20,6 +28,8 @@ public class Main {
             }
             inputStream.close();
 
+        } catch(MalformedURLException e) {
+            System.out.println("Malformed URL: " + e.getMessage());
         } catch(IOException e) {
             System.out.println("IOException: " + e.getMessage());
         }
