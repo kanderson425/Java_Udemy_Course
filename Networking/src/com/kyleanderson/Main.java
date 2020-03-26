40f408b35a454316401d39234d41084c06ecf932
kanderson425 <kyleanderson.dev@gmail.com>
2020-03-26 17:54:02 -0500

    Successfully reads the example.com page using the HttpURLConnection object.

--- Networking/src/com/kyleanderson/Main.java
+++ Networking/src/com/kyleanderson/Main.java
@@ -3,10 +3,7 @@ package com.kyleanderson;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.net.MalformedURLException;
-import java.net.URL;
-import java.net.URLClassLoader;
-import java.net.URLConnection;
+import java.net.*;
 import java.util.List;
 import java.util.Map;
 
@@ -15,29 +12,45 @@ public class Main {
     public static void main(String[] args) {
         try {
             URL url = new URL("http://example.org");
-            URLConnection urlConnection = url.openConnection();
-            urlConnection.setDoOutput(true);
-            urlConnection.connect();
+            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+            connection.setRequestMethod("GET");
+            connection.setRequestProperty("User-Agent", "Chrome");
+            connection.setReadTimeout(30000);
 
+            int responseCode = connection.getResponseCode();
+            System.out.println("Response code: " + responseCode);
 
-            BufferedReader inputStream = new BufferedReader(
-                    new InputStreamReader(urlConnection.getInputStream()));
-
-            Map<String, List<String>> headerFields = urlConnection.getHeaderFields();
-            for(Map.Entry<String, List<String>> entry: headerFields.entrySet()) {
-                String key = entry.getKey();
-                List<String> value = entry.getValue();
-                System.out.println("-------key = " + key);
-                for(String string: value) {
-                    System.out.println("value = " + value);
-                }
+            if(responseCode != 200) {
+                System.out.println("Error reading the web page");
+                return;
             }
 
-//            String line = "";
-//            while(line != null) {
-//                line = inputStream.readLine();
-//                System.out.println(line);
+            BufferedReader inputReader = new BufferedReader(
+                    new InputStreamReader(connection.getInputStream()));
+
+            String line;
+
+            while((line = inputReader.readLine()) != null) {
+                System.out.println(line);
+            }
+
+            inputReader.close();
+
+//            urlConnection.setDoOutput(true);
+//            urlConnection.connect();
+//
+//
+//
+//            Map<String, List<String>> headerFields = urlConnection.getHeaderFields();
+//            for(Map.Entry<String, List<String>> entry: headerFields.entrySet()) {
+//                String key = entry.getKey();
+//                List<String> value = entry.getValue();
+//                System.out.println("-------key = " + key);
+//                for(String string: value) {
+//                    System.out.println("value = " + value);
+//                }
 //            }
+
 //            inputStream.close();
 
         } catch(MalformedURLException e) {
