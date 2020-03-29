efb5f1fe0a9614b4e1080c25f49e72747cff9e25
kanderson425 <kyleanderson.dev@gmail.com>
2020-03-29 16:12:44 -0500

    Diagnosed application start on run error in Main.java.

---
+++ MusicUI/academy.learnProgramming.common/src/module-info.java
@@ -0,0 +1,12 @@
+module academy.learnProgramming.common {
+    requires javafx.controls;
+    requires sqlite.jdbc;
+    requires java.sql;
+    requires javafx.fxml;
+    requires javafx.base;
+
+    exports academy.learnProgramming.ui to javafx.graphics, javafx.fxml;
+    opens academy.learnProgramming.ui to javafx.fxml;
+    opens academy.learnProgramming.common to javafx.base;
+
+}
