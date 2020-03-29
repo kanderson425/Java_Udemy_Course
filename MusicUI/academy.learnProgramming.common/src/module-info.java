882ed01790e4f0a4048157492f84393d1cb53049
kanderson425 <kyleanderson.dev@gmail.com>
2020-03-29 16:53:20 -0500

    Adds the academy.learnProgramming.db module.

--- MusicUI/academy.learnProgramming.common/src/module-info.java
+++ MusicUI/academy.learnProgramming.common/src/module-info.java
@@ -5,8 +5,10 @@ module academy.learnProgramming.common {
     requires javafx.fxml;
     requires javafx.base;
 
+    exports academy.learnProgramming.common;
     exports academy.learnProgramming.ui to javafx.graphics, javafx.fxml;
     opens academy.learnProgramming.ui to javafx.fxml;
     opens academy.learnProgramming.common to javafx.base;
+
 
 }
---
+++ MusicUI/academy.learnProgramming.db/src/module-info.java
@@ -0,0 +1,8 @@
+module academy.learnProgramming.db {
+    requires java.sql;
+    requires academy.learnProgramming.common;
+    requires sqlite.jdbc;
+
+
+
+}
